9eb54860f3e79515dbd87bbf2527f00c321039e6
Oliver Obst <oliverobst@gmail.com>
2002-08-13 13:04:47 +0000

    Added initial stuff for a console with console variables 	(the variables will keep the server configuration). Needs still a 	lot of work.

---
+++ utility/console/console.cc
@@ -0,0 +1,220 @@
+/* -*- mode: c++ -*-
+   
+   this file is part of Soccer Server 3D
+   Copyright (C) 2002 Soccer Server Maintainance Group
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include <algorithm>
+
+#include "console.h"
+#include "consolebaseview.h"
+
+using namespace std;
+
+// the console is per default inactive
+Console::Console()
+    : mActive(false), mForwarder(), mSettings(&mForwarder)    
+{
+    // add the forwarder to the message multiplexer
+    gMux.AddTarget(&mForwarder);
+}
+
+Console& 
+Console::instance()
+{
+    static Console sInstance;
+    return sInstance;
+}
+
+void 
+Console::setActive(bool active)
+{
+    mActive = active;
+}
+
+bool 
+Console::isActive() const
+{
+    return mActive;
+}
+
+void 
+Console::processInput(const BaseInputDevice::Input &input)
+{
+    if (isActive())
+    {
+        // pipe the input through our inputline
+        mHistory.processInput(input);
+    } else {
+        // execute the binding
+        invokeBinding(input);
+    }
+}
+
+bool 
+Console::invokeBinding(const BaseInputDevice::Input &input)
+{
+    // if a strings is bound to the input, execute it
+    if (mBindings.exists(input.id))
+    {
+        execute(mBindings.get(input.id));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool 
+Console::execute(const string &input)
+{
+    pair <bool, ConsoleParser::tStatementList> scanResult;
+    
+    // try to scan the input
+    scanResult = mParser.scan(input);
+    if (!scanResult.first)
+    {
+        gDispatchError("Couldn't execute input: Scan error.\n");
+        return false;
+    }
+
+    // try to parse the input
+    if (!ConsoleParser::parse(scanResult.second))
+    {
+        ConsoleParser::deleteStatements(scanResult.second);
+        gDispatchError("Couldn't execute input: Syntax error.\n");
+        return false;
+    }
+    
+    // execute every parsed statement
+    bool success = true;
+    for (ConsoleParser::tStatementList::iterator iter = scanResult.second.begin(); 
+	 iter != scanResult.second.end(); 
+	 ++iter)
+    {
+        success = success && perform(*iter);
+    }
+    
+    // garbage collection
+    mParser.deleteStatements(scanResult.second);
+        
+    return success;
+}
+
+bool 
+Console::perform(ConVar::tConVars &conVars)
+{
+    ConVar*             commandVar;
+    const ConCommand    *command;
+    ConVar::tConVars    parameters;
+
+    // separate command and parameters
+    ConVar::tConVars::iterator iter = conVars.begin();
+    
+    commandVar = (*iter);
+    commandVar->getCommand(&command);
+    ++iter;
+    
+    copy(iter, conVars.end(), back_inserter(parameters));
+
+    // execute the command
+    ConCommand::eConExecResult execResult = command->execute(parameters);
+        
+    if (execResult == ConCommand::CER_INV_SIGNATURE)
+    {
+        gDispatchNormal("the command %s could not be executed: "
+			"wrong type of arguments.\n", 
+			commandVar->getAttributes().getName().c_str());
+        gDispatchNormal("%s\n", command->getUsage().c_str());
+
+        return false;
+    } 
+
+    return execResult == ConCommand::CER_OK;
+}
+
+ConsoleForwarder& 
+Console::getForwarder()
+{
+    return mForwarder;
+}
+
+ConsoleSettings& 
+Console::getSettings()
+{
+    return mSettings;
+}
+
+ConsoleHistory& 
+Console::getHistory()
+{
+    return mHistory;
+}
+
+ConsoleBindings& 
+Console::getBindings()
+{
+    return mBindings;
+}
+
+ConsoleAliases& 
+Console::getAliases()
+{
+    return mAliases;
+}
+
+ConsoleParser& 
+Console::getParser()
+{
+    return mParser;
+}
+
+void 
+Console::draw() const
+{
+    // only draw if active
+    if (isActive())
+    {
+        // tell every view to draw itself
+        for (tViews::const_iterator iter = mViews.begin(); 
+	     iter != mViews.end(); 
+	     ++iter)
+        {
+            (*iter)->draw();
+        }
+    }
+}
+
+void 
+Console::registerView(ConsoleBaseView *view)
+{
+    mViews.push_back(view);
+}
+
+void 
+Console::unregisterView(ConsoleBaseView *view)
+{
+    for (tViews::iterator iter = mViews.begin(); 
+	 iter != mViews.end(); )
+    {
+        if (*iter == view)
+        {
+            iter = mViews.erase(iter);
+            break;
+        } else {
+            ++iter;
+        }
+    }
+}
---
+++ utility/console/console.h
@@ -0,0 +1,159 @@
+/* -*- mode: c++ -*-
+   
+   this file is part of Soccer Server 3D
+   Copyright (C) 2002 Soccer Server Maintainance Group
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef _CONSOLE_H_
+#define _CONSOLE_H_
+
+// #include <macros.h> // there's a lot of evil stuff in there
+
+#include "consolesettings.h"
+#include "consolehistory.h"
+#include "consolebindings.h"
+#include "consolealiases.h"
+#include "consoleparser.h"
+#include "variables/convar.h"
+#include "commands/concommand.h"
+
+// #include "../forwarder/consoleforwarder.h"
+// #include "../inputserver/baseinputdevice.h"
+
+class ConsoleBaseView;
+
+/*! \class Console
+  $Id: console.h,v 1.1 2002/08/13 13:04:47 fruit Exp $
+
+    Console
+
+    The console is a in game console as known from many games (e.g quake).
+    Provided are :
+    - Variables (ConVarServer + ConVar)
+    - Commands (ConCommand)
+    - Aliases (ConAliases)
+    - Key Bindings (ConsoleBindings)
+
+    Two modi are discerned
+    - Active
+      the console displays itself (ConsoleBaseView) and provides 
+      an interactive input line for the user (ConsoleInputLine) 
+      which handles user input.
+    - Inactive
+      the console does not draw anything, but the users input is checked 
+      against the key bindings defined by the console (InputServer, 
+      BaseInputDevice).
+
+    The commands are classes inherited from ConCommand which are on
+    need created by the ClassServer.
+
+    HISTORY:
+    The console subsystem was taken from a student project at the AI
+    Research Group, Koblenz University. Original development by Marco
+    Koegler <koegler@uni-koblenz.de>, Marcus Rollmann
+    <rollmark@uni-koblenz.de>, Alexander Fuchs <alexf@uni-koblenz.de>,
+    et.al.
+
+    TODO:
+    - memory leaks ;-) (?)
+*/
+
+class Console
+{
+public:
+    //! get the singleton instance of the console
+    static Console& instance();
+
+    //! sets the consoles activity state
+    void setActive (bool active = true);
+    //! is the console active?
+    bool isActive () const;
+
+    /*! If the console is active the input is processed by the input
+        line (ConsoleInputLine). Otherwise the input bindings
+        (ConsoleBindings) are evaluated. 
+    */
+    void processInput (const BaseInputDevice::Input &input);
+
+    /*! execute a string as hacked into the console or 
+        read from a configuration file
+    */
+    bool execute (const std::string &command);
+    //! the forwarder associated with the console
+    ConsoleForwarder& getForwarder();
+    /*! the variable settings of this console which are 
+        accessible through the console input line
+    */
+    ConsoleSettings& getSettings();
+    /*! the history contains everything was has been displayed 
+        by the console plus the input line (ConsoleInputLine)
+    */
+    ConsoleHistory& getHistory();
+    //! the input bindings which are valid when the console is inactive
+    ConsoleBindings& getBindings();
+    //! the aliases
+    ConsoleAliases& getAliases();
+    //! the parser used to analyse the string given to execute
+    ConsoleParser& getParser();
+    /*! draw the console if active: 
+        this will pass the call to every registered view
+    */
+    void draw () const;
+    /*! Register a view to the console.  
+        The console will not delete the view, neither on
+        unregistration or in the destructor.
+    */
+    void registerView(ConsoleBaseView *view); 
+    //! unregister a view; the view will not be deleted 
+    void unregisterView(ConsoleBaseView *view);
+
+protected:
+    //! a container for the views
+    typedef std::list<ConsoleBaseView*> tViews;
+    //! this is a singleton: we don't want no copies
+    Console();
+    Console(const Console&);
+    Console& operator =(const Console&);
+    
+    //! execute the string bound to the id of the input event
+    bool invokeBinding(const BaseInputDevice::Input &input);
+    //! executes a statement returned by the parser and returns the success
+    bool perform(ConVar::tConVars &conVars);
+
+    //! is the console active?
+    bool mActive;
+    //! the registered views
+    tViews mViews;
+    /*! The console forwarder.
+        Should exist before the mSettings, as the settings will
+        reconfigure the forwarders message interests.
+    */
+    ConsoleForwarder mForwarder;
+    //! the variable settings accessable through the console input line
+    ConsoleSettings mSettings;
+    /*! the history contains everything what has been displayed by the
+        console and the input line (ConsoleInputLine)
+    */
+    ConsoleHistory mHistory;
+    //! the key bindings
+    ConsoleBindings mBindings;
+    //! the aliases
+    ConsoleAliases mAliases;
+    //! the parser used to analyse the strings given to execute
+    ConsoleParser mParser;
+};
+
+#endif // _CONSOLE_H_
