94129391b752532b84ab9f0376efdebec0b8b38a
Oliver Obst <oliverobst@gmail.com>
2003-05-19 21:18:20 +0000

    - added copyright text - using config.h file - fixed template method Cross in class TVector3 (at least for gcc 3.2, it was a bug)

--- lib/salt/tvector.h
+++ lib/salt/tvector.h
@@ -1,8 +1,28 @@
+/* -*- mode: c++ -*-
+   
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: tvector.h,v 1.3 2003/05/19 21:18:20 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef TVECTOR_H__
 #define TVECTOR_H__
 
 /*! \class TVector
-	$Id: tvector.h,v 1.2 2003/04/30 14:21:49 fruit Exp $
+	$Id: tvector.h,v 1.3 2003/05/19 21:18:20 fruit Exp $
 	
 	TVector
 	
@@ -17,6 +37,10 @@
 			- initial version
 
 */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include "defines.h"
 #include "gmath.h"
@@ -154,7 +178,7 @@ public:
 	f_inline const DATATYPE&	z() const			{ return El(2); }
 
 	// Cross product - returns a new vector
-	template <typename DATATYPE, class TYPE>
+        // template <typename DATATYPE, class TYPE>
 	const TYPE Cross(const TVector<DATATYPE, 3, TYPE> &v) const
 	{
 		// Create a new one
