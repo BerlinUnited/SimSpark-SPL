0efd89cf0a47a76bd9adcc94e9c18eff1cae22d7
Oliver Obst <oliverobst@gmail.com>
2003-08-29 07:02:30 +0000

    - added copyleft - replaced '#ifdef HAVE_HASH_MAP' by '#if HAVE_HASH_MAP' - including config.h - whitespace changes

--- lib/kerosin/openglserver/openglserver.h
+++ lib/kerosin/openglserver/openglserver.h
@@ -1,5 +1,30 @@
-#ifndef OPENGLSERVER_H__
-#define OPENGLSERVER_H__
+/* -*- mode: c++; c-basic-indent: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: openglserver.h,v 1.5 2003/08/29 07:02:30 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef KEROSIN_OPENGLSERVER_H
+#define KEROSIN_OPENGLSERVER_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <zeitgeist/class.h>
 #include <zeitgeist/leaf.h>
@@ -7,71 +32,72 @@
 
 namespace kerosin
 {
+#if 0
+}
+#endif
 
 class OpenGLServer : public zeitgeist::Leaf
 {
-        //
-        // types
-        //
+    //
+    // types
+    //
 private:
-        //! this structure will be used to map program names to OpenGL IDs
-#ifdef HAVE_HASH_MAP
-        typedef std::hash_map<std::string, unsigned int>        TProgramCache;
+    //! this structure will be used to map program names to OpenGL IDs
+#if HAVE_HASH_MAP
+    typedef std::hash_map<std::string, unsigned int> TProgramCache;
 #else
-        typedef std::map<std::string, unsigned int>     TProgramCache;
+    typedef std::map<std::string, unsigned int> TProgramCache;
 #endif
-
-
-        //
-        // functions
-        //
+    //
+    // functions
+    //
 public:
-        OpenGLServer();
-        ~OpenGLServer();
+    OpenGLServer();
+    ~OpenGLServer();
 
-        //! setup application window, initialize OpenGL, autoregister extensions
-        bool                                                            Init();
+    //! setup application window, initialize OpenGL, autoregister extensions
+    bool Init();
 
-        boost::shared_ptr<GLExtensionReg>       GetExtensionReg() const;
-        //! if this is called, the application will 'want to quit'
-        void                                                            Quit();
-        //! true if somebody called 'Quit'
-        bool                                                            WantsToQuit() const;
-        //! pump SDL event loop
-        void                                                            Update();
-        //! swap opengl buffer
-        void                                                            SwapBuffers() const;
+    boost::shared_ptr<GLExtensionReg> GetExtensionReg() const;
+    //! if this is called, the application will 'want to quit'
+    void Quit();
+    //! true if somebody called 'Quit'
+    bool WantsToQuit() const;
+    //! pump SDL event loop
+    void Update();
+    //! swap opengl buffer
+    void SwapBuffers() const;
 
-        //! vertex and fragment program loading
-        unsigned int LoadARBProgram(GLenum target, const char* fileName);
-        //! vertex and fragment program loading
-        unsigned int LoadARBVertexProgram(const char* fileName);
-        //! vertex and fragment program loading
-        unsigned int LoadARBFragmentProgram(const char* fileName);
+    //! vertex and fragment program loading
+    unsigned int LoadARBProgram(GLenum target, const char* fileName);
+    //! vertex and fragment program loading
+    unsigned int LoadARBVertexProgram(const char* fileName);
+    //! vertex and fragment program loading
+    unsigned int LoadARBFragmentProgram(const char* fileName);
 
-        bool    SupportsFancyLighting() const   {       return mSupportsFancyLighting;  }
+    bool SupportsFancyLighting() const { return mSupportsFancyLighting; }
+    void ToggleFancyLighting();
 
-        void    ToggleFancyLighting();
 protected:
-        //! set up opengl viewport
-        virtual bool ConstructInternal();
+    //! set up opengl viewport
+    virtual bool ConstructInternal();
 
-        //
-        // members
-        //
+    //
+    // members
+    //
 private:
-        //! this contains information on all available extensions
-        boost::shared_ptr<GLExtensionReg>       mExtensionReg;
-        //! a flag, which can be used to control the shutdown of the display window and the application
-        bool                                                            mWantsToQuit;
-        //! cache of loaded vertex and fragment programs (assumes that path names to the programs are unique)
-        TProgramCache                                           mPrograms;
-        //! flag whether the OpenGL-driver can do fancy lighting or not
-        bool                                                            mSupportsFancyLighting;
+    //! this contains information on all available extensions
+    boost::shared_ptr<GLExtensionReg> mExtensionReg;
+    //! a flag, which can be used to control the shutdown of the display window and the application
+    bool mWantsToQuit;
+    //! cache of loaded vertex and fragment programs (assumes that path names to the programs are unique)
+    TProgramCache mPrograms;
+    //! flag whether the OpenGL-driver can do fancy lighting or not
+    bool mSupportsFancyLighting;
 };
 
 DECLARE_CLASS(OpenGLServer);
 
 } //namespace kerosin
 
-#endif //OPENGLSERVER_H__
+#endif //KEROSIN_OPENGLSERVER_H
--- lib/kerosin/textureserver/textureserver.h
+++ lib/kerosin/textureserver/textureserver.h
@@ -1,10 +1,35 @@
-#ifndef TEXTURESERVER_H__
-#define TEXTURESERVER_H__
+/* -*- mode: c++; c-basic-indent: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: textureserver.h,v 1.4 2003/08/29 07:04:58 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef KEROSIN_TEXTURESERVER_H
+#define KEROSIN_TEXTURESERVER_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <zeitgeist/class.h>
 #include <zeitgeist/leaf.h>
 
-#ifdef HAVE_HASH_MAP
+#if HAVE_HASH_MAP
 #include <hash_map>
 #else
 #include <map>
@@ -12,47 +37,48 @@
 
 namespace kerosin
 {
+#if 0
+}
+#endif
 
 class OpenGLServer;
 class Texture;
 
 class TextureServer : public zeitgeist::Leaf
 {
-        //
-        // types
-        //
+    //
+    // types
+    //
 private:
-#ifdef HAVE_HASH_MAP
-        typedef std::hash_map<std::string, boost::shared_ptr<Texture> > TTextureCache;
+#if HAVE_HASH_MAP
+    typedef std::hash_map<std::string, boost::shared_ptr<Texture> > TTextureCache;
 #else
-        typedef std::map<std::string, boost::shared_ptr<Texture> > TTextureCache;
+    typedef std::map<std::string, boost::shared_ptr<Texture> > TTextureCache;
 #endif
-
-
-        //
-        // functions
-        //
+    //
+    // functions
+    //
 public:
-        TextureServer();
-        virtual ~TextureServer();
+    TextureServer();
+    virtual ~TextureServer();
 
-        //! initializes the TextureServer ... checks for the existance of the OpenGLServer
-        bool Init(const std::string &openglPath);
-        //! retrieve pointer to the OpenGL server ... used by Textures to check extensions
-        boost::shared_ptr<OpenGLServer> GetOpenGLServer() const;
-        //! load (or returned cached) texture
-        boost::shared_ptr<Texture>              GetTexture(const std::string &name);
+    //! initializes the TextureServer ... checks for the existance of the OpenGLServer
+    bool Init(const std::string &openglPath);
+    //! retrieve pointer to the OpenGL server ... used by Textures to check extensions
+    boost::shared_ptr<OpenGLServer> GetOpenGLServer() const;
+    //! load (or returned cached) texture
+    boost::shared_ptr<Texture> GetTexture(const std::string &name);
 
-        //
-        // members
-        //
+    //
+    // members
+    //
 private:
-        boost::shared_ptr<OpenGLServer> mOpenGLServer;
-        TTextureCache                                   mTextureCache;          // registry of cached textures
+    boost::shared_ptr<OpenGLServer> mOpenGLServer;
+    TTextureCache mTextureCache;          // registry of cached textures
 };
 
 DECLARE_CLASS(TextureServer);
 
 } //kerosin
 
-#endif //TEXTURESERVER_H__
+#endif //KEROSIN_TEXTURESERVER_H
