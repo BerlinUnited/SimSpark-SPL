d9879fb1654cfc2490e3dac3e112d4ee1942873a
Oliver Obst <oliverobst@gmail.com>
2003-08-29 11:43:35 +0000

    Added links on the main page generated by doxygen to the existing information for developers.

--- lib/salt/salt.h
+++ lib/salt/salt.h
@@ -1,19 +1,19 @@
 /* -*- mode: c++ -*-
-   
+
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2003 Koblenz University
-   $Id: salt.h,v 1.4 2003/08/21 08:56:49 rollmark Exp $
+   $Id: salt.h,v 1.5 2003/08/29 11:43:35 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; version 2 of the License.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
- 
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -34,5 +34,27 @@
 #include "bounds.h"
 #include "plane.h"
 #include "frustum.h"
+
+/*! \mainpage rcssserver3D Documentation
+ *
+ * \section welcome Welcome!
+ *
+ * Below are some links to pages containing useful information regarding
+ * the actual implementation of rcssserver3D.
+ *
+ * - <b><a href="developers/implementation.html">Implementation</a></b> This section
+ *   gives an overview of how the source code distribution
+ *   is structured. It also explains how the projects can be built.
+ *
+ * - <b><a href="developers/howtos.html">HowTos</a></b> This is probably the most useful
+ *   section as it describes how to go about using the framework developed
+ *   for the thesis to realize custom simulations. For example, how do you
+ *   implement a class which can live in the object hierarchy (and make it
+ *   loadable from the Ruby script side).
+ *
+ * - <b><a href="http://www.uni-koblenz.de/~fruit/ftp/teaching/koe03-diplomarbeit.pdf">Thesis PDF</a></b> The thesis (in PDF format) describing the
+ *   concepts of a framework for simulating and visualizing agents in
+ *   3D environments.
+ */
 
 #endif //SALT_H__
