613aec0f775563f8daa5879bf5e1da494c3fd389
Oliver Obst <oliverobst@gmail.com>
2003-09-05 12:07:05 +0000

    - added oxygen.h file to include all the oxygen headers at once

---
+++ lib/oxygen/oxygen.h
@@ -0,0 +1,50 @@
+/* -*- mode: c++ -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: oxygen.h,v 1.1 2003/09/05 12:07:05 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef OXYGEN_OXYGEN_H
+#define OXYGEN_OXYGEN_H
+
+#include "agentaspect/agentaspect.h"
+#include "agentaspect/collisionperceptor.h"
+#include "agentaspect/effector.h"
+#include "agentaspect/forceeffector.h"
+#include "agentaspect/perceptor.h"
+
+#include "controlaspect/controlaspect.h"
+
+#include "physicsserver/body.h"
+#include "physicsserver/boxcollider.h"
+#include "physicsserver/ccylindercollider.h"
+#include "physicsserver/collider.h"
+#include "physicsserver/odeobject.h"
+#include "physicsserver/physicsserver.h"
+#include "physicsserver/planecollider.h"
+#include "physicsserver/space.h"
+#include "physicsserver/spherecollider.h"
+#include "physicsserver/world.h"
+
+#include "sceneserver/basenode.h"
+#include "sceneserver/fpscontroller.h"
+// #include "sceneserver/indexbuffer.h"
+#include "sceneserver/scene.h"
+#include "sceneserver/sceneserver.h"
+#include "sceneserver/transform.h"
+
+#endif // OXYGEN_OXYGEN_H
