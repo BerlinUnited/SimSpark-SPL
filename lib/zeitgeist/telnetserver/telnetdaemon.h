927ca91e2000bd9e718cf838bb1a6bd5649ad360
Oliver Obst <oliverobst@gmail.com>
2003-09-05 12:22:50 +0000

    *** empty log message ***

--- lib/zeitgeist/telnetserver/telnetdaemon.h
+++ lib/zeitgeist/telnetserver/telnetdaemon.h
@@ -1,35 +1,36 @@
 /* -*- mode: c++ -*-
-   
+
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2003 Koblenz University
-   $Id: telnetdaemon.h,v 1.2 2003/08/21 12:53:31 rollmark Exp $
+   $Id: telnetdaemon.h,v 1.3 2003/09/05 12:22:51 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; version 2 of the License.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
- 
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
    TelnetDaemon
 
-	HISTORY:
-		19.06.2002 MK
-			- initial version
+        HISTORY:
+                19.06.2002 MK
+                        - initial version
 */
 
 #ifndef TELNETDAEMON_H__
 #define TELNETDAEMON_H__
 
-
+#ifdef _WIN32
 #include <winsock.h>
+#endif
 #include <list>
 
 namespace zeitgeist
@@ -44,71 +45,71 @@ class TelnetServer;
 */
 class TelnetDaemon
 {
-	//
-	// types
-	//
+        //
+        // types
+        //
 public:
 protected:
 private:
-	typedef std::list<TelnetSession*>	TSessionList;
+        typedef std::list<TelnetSession*>       TSessionList;
 
-	//
-	// functions
-	//
+        //
+        // functions
+        //
 public:
-	/** constructs the TelnetDaemon
-	    \param server is a reference to the server constructing this daemon
-	*/
-	TelnetDaemon(TelnetServer &server);
-	virtual ~TelnetDaemon();
+        /** constructs the TelnetDaemon
+            \param server is a reference to the server constructing this daemon
+        */
+        TelnetDaemon(TelnetServer &server);
+        virtual ~TelnetDaemon();
 
-	/** contains the code run inside the thread for this Daemon,
-	    called from the boost thread library */
-	void operator()();
+        /** contains the code run inside the thread for this Daemon,
+            called from the boost thread library */
+        void operator()();
 
-	/** called from the server to shutdown the daemon. This method
-	    destroys all session objects */
-	void Terminate();
+        /** called from the server to shutdown the daemon. This method
+            destroys all session objects */
+        void Terminate();
 
-	/** prints the status of the daemon to stdout */
-	void Status();
+        /** prints the status of the daemon to stdout */
+        void Status();
 
-	/** adds a session a object to the list of managed sessions */
-	void Attach(TelnetSession *session);
+        /** adds a session a object to the list of managed sessions */
+        void Attach(TelnetSession *session);
 
-	/** removes a session object from the list of managed sessions */
-	void Detach(TelnetSession *session);
-	
-	/** return a reference to the TelnetServer object, called by
-	    the TelnetSession objects */
-	const TelnetServer&	GetServer() const	{	return mServer;	}
-	
+        /** removes a session object from the list of managed sessions */
+        void Detach(TelnetSession *session);
+
+        /** return a reference to the TelnetServer object, called by
+            the TelnetSession objects */
+        const TelnetServer&     GetServer() const       {       return mServer; }
+
 protected:
-	/** create the network socket and start the daemon */
-	bool Init(int portNr);
+        /** create the network socket and start the daemon */
+        bool Init(int portNr);
 
 
-	/** accepts a pending connection request, creates a new socket
-	    and associates the TelnetSession cc with it*/
-	bool AcceptConnection(TelnetSession& cc);
+        /** accepts a pending connection request, creates a new socket
+            and associates the TelnetSession cc with it*/
+        bool AcceptConnection(TelnetSession& cc);
 
-	//
-	// members
-	//
+        //
+        // members
+        //
 public:
 protected:
 private:
-	/** the listen socket of the server */
-	SOCKET			mDaemonSocket;
+        /** the listen socket of the server */
+        SOCKET                  mDaemonSocket;
 
-	/** the local adress the daemon is bound to */
-	sockaddr_in		mDaemonAddr;
-	
-	/** the list of current active sessions */
-	TSessionList	mSessions;
+        /** the local adress the daemon is bound to */
+        sockaddr_in             mDaemonAddr;
 
-	/** a reference to the TelnetServer */
-	TelnetServer	&mServer;
+        /** the list of current active sessions */
+        TSessionList    mSessions;
+
+        /** a reference to the TelnetServer */
+        TelnetServer    &mServer;
 };
 
 }
