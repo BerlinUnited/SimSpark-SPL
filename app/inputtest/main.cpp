6ba8d25e10aeea6d507adb752069fc1b83337a21
Oliver Obst <oliverobst@gmail.com>
2003-11-10 12:55:13 +0000

    changed paths of the scripts to be loaded

--- app/inputtest/main.cpp
+++ app/inputtest/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 using namespace zeitgeist;
 
 //! the Zeitgeist context we are using
-shared_ptr<CoreContext>	gContext;
+shared_ptr<CoreContext> gContext;
 
 shared_ptr<LogServer> gLog;
 
@@ -20,87 +20,87 @@ const static int gCmdQuit = 1;
 
 static void update()
 {
-	// retrieve the input server
-	shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
+        // retrieve the input server
+        shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
 
-	if (!inputServer) return;
-	
+        if (!inputServer) return;
+
     // Process incoming input
-	InputServer::Input input;
-	while (inputServer->GetInput(input))
-	{
-		switch (input.id)
-		{
-			case gCmdQuit:
-				{
-					shared_ptr<OpenGLServer> openglServer = shared_static_cast<OpenGLServer>(gContext->Get("/sys/server/opengl"));
-					openglServer->Quit();
-				}
-				break;
-		}
-	}
+        InputServer::Input input;
+        while (inputServer->GetInput(input))
+        {
+                switch (input.id)
+                {
+                        case gCmdQuit:
+                                {
+                                        shared_ptr<OpenGLServer> openglServer = shared_static_cast<OpenGLServer>(gContext->Get("/sys/server/opengl"));
+                                        openglServer->Quit();
+                                }
+                                break;
+                }
+        }
 }
 
 static void shutdown()
 {
-	// we have to make sure, the inputServer is shut down before the opengl server,
-	// as the opengl server shuts down SDL ... this will conflict with the input
-	// server
-	shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
+        // we have to make sure, the inputServer is shut down before the opengl server,
+        // as the opengl server shuts down SDL ... this will conflict with the input
+        // server
+        shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
 
-	if (inputServer)
-		inputServer->Unlink();
+        if (inputServer)
+                inputServer->Unlink();
 }
 
 int main(int argc, char **argv)
 {
-	Zeitgeist	zg;
+        Zeitgeist       zg;
 
-	// create our global context (object hierarchy position container)
-	gContext = zg.CreateContext();
-	Kerosin					kCore(zg);
+        // create our global context (object hierarchy position container)
+        gContext = zg.CreateContext();
+        Kerosin                                 kCore(zg);
 
-	gLog = shared_static_cast<LogServer>(gContext->Get("/sys/server/log"));
+        gLog = shared_static_cast<LogServer>(gContext->Get("/sys/server/log"));
 
-	shared_ptr<ScriptServer> scriptServer = shared_static_cast<ScriptServer>(gContext->Get("/sys/server/script"));
-	scriptServer->Run("script/init.rb");
-	scriptServer->Run("script/inputtest.rb");
+        shared_ptr<ScriptServer> scriptServer = shared_static_cast<ScriptServer>(gContext->Get("/sys/server/script"));
+        scriptServer->Run("init.rb");
+        scriptServer->Run("inputtest.rb");
 
-	unsigned int prevTime = SDL_GetTicks();
-	int frames = 0;
-	shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
+        unsigned int prevTime = SDL_GetTicks();
+        int frames = 0;
+        shared_ptr<InputServer> inputServer = shared_static_cast<InputServer>(gContext->Get("/sys/server/input"));
 
-	inputServer->BindCommand("escape", gCmdQuit);
+        inputServer->BindCommand("escape", gCmdQuit);
 
-	// retrieve shared ptr to the OpenGL Server ... this represents the OpenGL
-	// context the application runs in, as well as the window
-	shared_ptr<OpenGLServer> openglServer = shared_static_cast<OpenGLServer>(gContext->Get("/sys/server/opengl"));
-	if (openglServer.get() == NULL)
-	{
-		gContext->GetCore()->GetLogServer()->Error() << "ERROR: Can't locate OpenGLServer ..." << endl;
-		shutdown();
-		return 1;
-	}
+        // retrieve shared ptr to the OpenGL Server ... this represents the OpenGL
+        // context the application runs in, as well as the window
+        shared_ptr<OpenGLServer> openglServer = shared_static_cast<OpenGLServer>(gContext->Get("/sys/server/opengl"));
+        if (openglServer.get() == NULL)
+        {
+                gContext->GetCore()->GetLogServer()->Error() << "ERROR: Can't locate OpenGLServer ..." << endl;
+                shutdown();
+                return 1;
+        }
 
-	// the runloop
-	while(!openglServer->WantsToQuit())
-	{
-		frames ++;
-		// update the window (pumps event loop, etc..)
-		openglServer->Update();
-		// update all the other components
-		update();
+        // the runloop
+        while(!openglServer->WantsToQuit())
+        {
+                frames ++;
+                // update the window (pumps event loop, etc..)
+                openglServer->Update();
+                // update all the other components
+                update();
 
-		openglServer->SwapBuffers();
-	}
+                openglServer->SwapBuffers();
+        }
 
-	unsigned int time = SDL_GetTicks() - prevTime;
+        unsigned int time = SDL_GetTicks() - prevTime;
 
-	gLog->Normal() << "Average FPS: " << 1000.0f*frames / (float)time << endl;
+        gLog->Normal() << "Average FPS: " << 1000.0f*frames / (float)time << endl;
 
-	gLog.reset();
+        gLog.reset();
 
-	shutdown();
+        shutdown();
 
-	return 0;
+        return 0;
 }
