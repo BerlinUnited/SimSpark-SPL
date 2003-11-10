e3883df7b4345f92a3bc2989d9e382dd2d33a231
Oliver Obst <oliverobst@gmail.com>
2003-11-10 16:24:27 +0000

    added copyright only

--- plugin/soundsystemfmod/soundmodulefmod.h
+++ plugin/soundsystemfmod/soundmodulefmod.h
@@ -1,3 +1,24 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: soundmodulefmod.h,v 1.2 2003/11/10 16:26:09 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef SOUNDMODULEFMOD_H__
 #define SOUNDMODULEFMOD_H__
 
@@ -7,14 +28,14 @@
 class SoundModuleFMOD : public kerosin::SoundModule
 {
 public:
-	SoundModuleFMOD(kerosin::SoundServer &inServer);
-	virtual ~SoundModuleFMOD();
+        SoundModuleFMOD(kerosin::SoundServer &inServer);
+        virtual ~SoundModuleFMOD();
 
-	void Load(const char *inName);
-	void Load(void *inBuffer, int inSize);
-	void Play();
+        void Load(const char *inName);
+        void Load(void *inBuffer, int inSize);
+        void Play();
 
 protected:
-	FMUSIC_MODULE* mHandle;
+        FMUSIC_MODULE* mHandle;
 };
 #endif //SOUNDMODULEFMOD_H__
--- plugin/soundsystemfmod/soundstreamfmod.h
+++ plugin/soundsystemfmod/soundstreamfmod.h
@@ -1,3 +1,24 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: soundstreamfmod.h,v 1.2 2003/11/10 16:27:02 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef SOUNDSTREAMFMOD_H__
 #define SOUNDSTREAMFMOD_H__
 
@@ -7,15 +28,15 @@
 class SoundStreamFMOD : public kerosin::SoundStream
 {
 public:
-	SoundStreamFMOD(kerosin::SoundServer &inServer);
-	virtual ~SoundStreamFMOD();
+        SoundStreamFMOD(kerosin::SoundServer &inServer);
+        virtual ~SoundStreamFMOD();
 
-	void Load(const char *inName);
-	void Load(void *inBuffer, int inSize);
-	void Play();
+        void Load(const char *inName);
+        void Load(void *inBuffer, int inSize);
+        void Play();
 
 protected:
-	FSOUND_STREAM*	mHandle;
-	char*			mBuffer;
+        FSOUND_STREAM*  mHandle;
+        char*                   mBuffer;
 };
 #endif //SOUNDSTREAMFMOD_H__
