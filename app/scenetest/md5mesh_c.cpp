f1c7b1dc598ed5e30d2b0243ba6cb272936ac2b0
Oliver Obst <oliverobst@gmail.com>
2003-11-10 12:47:33 +0000

    whitespace only

--- app/scenetest/md5mesh_c.cpp
+++ app/scenetest/md5mesh_c.cpp
@@ -4,25 +4,25 @@ using namespace boost;
 
 FUNCTION(load)
 {
-	if (in.size() == 1)
-	{
-		MD5Mesh *m = static_cast<MD5Mesh*>(obj);
-		m->Load(any_cast<char*>(in[0]));
-	}
+        if (in.size() == 1)
+        {
+                MD5Mesh *m = static_cast<MD5Mesh*>(obj);
+                m->Load(any_cast<char*>(in[0]));
+        }
 }
 
 FUNCTION(loadAnimation)
 {
-	if (in.size() == 1)
-	{
-		MD5Mesh *m = static_cast<MD5Mesh*>(obj);
-		m->LoadAnimation(any_cast<char*>(in[0]));
-	}
+        if (in.size() == 1)
+        {
+                MD5Mesh *m = static_cast<MD5Mesh*>(obj);
+                m->LoadAnimation(any_cast<char*>(in[0]));
+        }
 }
 
 void CLASS(MD5Mesh)::DefineClass()
 {
-	DEFINE_BASECLASS(kerosin/BaseNode);
-	DEFINE_FUNCTION(load);
-	DEFINE_FUNCTION(loadAnimation);
+        DEFINE_BASECLASS(kerosin/BaseNode);
+        DEFINE_FUNCTION(load);
+        DEFINE_FUNCTION(loadAnimation);
 }
