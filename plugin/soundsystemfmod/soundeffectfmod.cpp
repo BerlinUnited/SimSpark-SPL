5c0785d43ae34f53ac86411bb43a15a523e738af
Oliver Obst <oliverobst@gmail.com>
2003-11-10 16:34:29 +0000

    - interface changed in FSOUND_Sample_Load function for FMOD 3.7

--- plugin/soundsystemfmod/soundeffectfmod.cpp
+++ plugin/soundsystemfmod/soundeffectfmod.cpp
@@ -2,35 +2,35 @@
 
 SoundEffectFMOD::SoundEffectFMOD(kerosin::SoundServer &inServer) : SoundEffect(inServer)
 {
-	mHandle = NULL;
+        mHandle = NULL;
 }
 
 SoundEffectFMOD::~SoundEffectFMOD()
 {
-	if (mHandle != NULL)
-	{
-		FSOUND_Sample_Free(mHandle);
-	}
+        if (mHandle != NULL)
+        {
+                FSOUND_Sample_Free(mHandle);
+        }
 }
 
 void SoundEffectFMOD::Load(const char* inName)
 {
-	// Loading the sound as a Loop off, 8bit mono, signed, not hardware accelerated sample
-	mHandle = FSOUND_Sample_Load(FSOUND_FREE, inName, FSOUND_NORMAL, 0);
-	SetFileName(inName);
+        // Loading the sound as a Loop off, 8bit mono, signed, not hardware accelerated sample
+        mHandle = FSOUND_Sample_Load(FSOUND_FREE, inName, FSOUND_NORMAL, 0, 0);
+        SetFileName(inName);
 }
 
 void SoundEffectFMOD::Load(void *inBuffer, int inSize)
 {
-	// Loading the sound as a Loop off, 8bit mono, signed, not hardware accelerated sample
+        // Loading the sound as a Loop off, 8bit mono, signed, not hardware accelerated sample
 
-	mHandle = FSOUND_Sample_Load(FSOUND_FREE, (const char*)inBuffer, (FSOUND_NORMAL | FSOUND_LOADMEMORY), inSize);
+        mHandle = FSOUND_Sample_Load(FSOUND_FREE, (const char*)inBuffer, (FSOUND_NORMAL | FSOUND_LOADMEMORY), inSize, 0);
 }
 
 void SoundEffectFMOD::Play()
 {
-	if (mHandle != NULL)
-	{
-		FSOUND_PlaySound(FSOUND_FREE, mHandle);
-	}
+        if (mHandle != NULL)
+        {
+                FSOUND_PlaySound(FSOUND_FREE, mHandle);
+        }
 }
