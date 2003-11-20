4edbf7fd2a8d35e14022f649e3c5750acab752e3
Markus Rollmann <rollmark@uni-koblenz.de>
2003-11-20 20:15:59 +0000

    added missing gpl header removed Init() declaration from header with no corresponding implementation added more diagnostic output to the openglserver

--- lib/kerosin/openglserver/openglserver.cpp
+++ lib/kerosin/openglserver/openglserver.cpp
@@ -1,22 +1,46 @@
+/* -*- mode: c++; c-basic-indent: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: openglserver.cpp,v 1.9.2.1 2003/11/20 20:15:59 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include "openglserver.h"
 #include <SDL/SDL.h>
 #include <zeitgeist/scriptserver/scriptserver.h>
 #include <zeitgeist/fileserver/fileserver.h>
 #include <zeitgeist/logserver/logserver.h>
 
+using namespace std;
+
 namespace kerosin
 {
-    class MapHolder
-    {
-    public:
+  class MapHolder
+  {
+  public:
     //! this structure will be used to map program names to OpenGL IDs
 #if HAVE_HASH_MAP
-        typedef std::hash_map<std::string, unsigned int> TProgramCache;
+    typedef hash_map<string, unsigned int> TProgramCache;
 #else
-        typedef std::map<std::string, unsigned int> TProgramCache;
+    typedef map<string, unsigned int> TProgramCache;
 #endif
-        TProgramCache mPrograms;
-    };
+    TProgramCache mPrograms;
+  };
 }
 
 using namespace boost;
@@ -29,224 +53,233 @@ OpenGLServer::OpenGLServer() : Leaf(), mExtensionReg(new GLExtensionReg()), mWan
 
 OpenGLServer::~OpenGLServer()
 {
-        SDL_Quit();
+  SDL_Quit();
 }
 
 boost::shared_ptr<GLExtensionReg> OpenGLServer::GetExtensionReg() const
 {
-        return mExtensionReg;
+  return mExtensionReg;
 }
 
 void OpenGLServer::Quit()
 {
-        mWantsToQuit = true;
+  mWantsToQuit = true;
 }
 
 bool OpenGLServer::WantsToQuit() const
 {
-        return mWantsToQuit;
+  return mWantsToQuit;
 }
 
 void OpenGLServer::Update()
 {
-    // Our SDL event placeholder.
-    SDL_Event event;
+  // Our SDL event placeholder.
+  SDL_Event event;
 
-    // Grab all the events off the queue.
-    while( SDL_PollEvent( &event ) )
-        {
-        }
+  // Grab all the events off the queue.
+  while( SDL_PollEvent( &event ) )
+    {
+    }
 }
 
 void OpenGLServer::SwapBuffers() const
 {
-        SDL_GL_SwapBuffers();
+  SDL_GL_SwapBuffers();
 }
 
 /*!
-        Load an ARB program
+  Load an ARB program
 
-        returns 0 on fail, non-zero on success
+  returns 0 on fail, non-zero on success
 */
 unsigned int OpenGLServer::LoadARBProgram(GLenum /*target*/, const char* /*fileName*/)
 {
 #if 0
-        // only try to load stuff if the extension is supported
-        if (!mExtensionReg->Has_GL_ARB_vertex_program())
-        {
-                return 0;
-        }
+  // only try to load stuff if the extension is supported
+  if (!mExtensionReg->Has_GL_ARB_vertex_program())
+    {
+      return 0;
+    }
 
-        // before actually loading, try the cache
-        MapHolder::TProgramCache::iterator entry = mHolder->mPrograms.find(fileName);
+  // before actually loading, try the cache
+  MapHolder::TProgramCache::iterator entry = mHolder->mPrograms.find(fileName);
 
-        if (entry != mHolder->mPrograms.end())
-        {
-                // we already have a match
-                return (*entry).second;
-        }
+  if (entry != mHolder->mPrograms.end())
+    {
+      // we already have a match
+      return (*entry).second;
+    }
 
-        unsigned int id = 0;
+  unsigned int id = 0;
 
-        // open file
-        shared_ptr<FileServer> fileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
-        salt::RFile *file = fileServer->Open(fileName);
+  // open file
+  shared_ptr<FileServer> fileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
+  salt::RFile *file = fileServer->Open(fileName);
 
-        if (!file) return 0;
+  if (!file) return 0;
 
-        unsigned char *buffer = new unsigned char[file->Size()+1];
-        file->Read(buffer, file->Size());
+  unsigned char *buffer = new unsigned char[file->Size()+1];
+  file->Read(buffer, file->Size());
 
-        glGenProgramsARB(1, &id);
-        glBindProgramARB(target, id);
+  glGenProgramsARB(1, &id);
+  glBindProgramARB(target, id);
 
-        // try to load the actual program
-        glProgramStringARB(target, GL_PROGRAM_FORMAT_ASCII_ARB, file->Size(), buffer);
+  // try to load the actual program
+  glProgramStringARB(target, GL_PROGRAM_FORMAT_ASCII_ARB, file->Size(), buffer);
 
-        // free memory
-        delete file;
-        delete []buffer;
+  // free memory
+  delete file;
+  delete []buffer;
 
-        const unsigned char* error = glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+  const unsigned char* error = glGetString(GL_PROGRAM_ERROR_STRING_ARB);
 
-        // if an error occured, display error message
-        if (error[0] != 0)
-        {
-                int i;
-                glDeleteProgramsARB(1, &id);
-                glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &i);
-                GetCore()->GetLogServer()->Error() << "ERROR: Loading ARB program (Pos: " << i << ")..." << std::endl;
-                GetCore()->GetLogServer()->Error() << "  => " << error << std::endl;
-                return 0;
-        }
+  // if an error occured, display error message
+  if (error[0] != 0)
+    {
+      int i;
+      glDeleteProgramsARB(1, &id);
+      glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &i);
+      GetCore()->GetLogServer()->Error() << "ERROR: Loading ARB program (Pos: " << i << ")..." << endl;
+      GetCore()->GetLogServer()->Error() << "  => " << error << endl;
+      return 0;
+    }
 
-        // enter program into the cache
-        mHolder->mPrograms[fileName] = id;
+  // enter program into the cache
+  mHolder->mPrograms[fileName] = id;
 
-        return id;
+  return id;
 #else
-	return 0;
-#endif	
+  return 0;
+#endif
 }
 
 /*!
-        Load a vertex program
+  Load a vertex program
 
-        returns 0 on fail, non-zero on success
+  returns 0 on fail, non-zero on success
 */
 unsigned int OpenGLServer::LoadARBVertexProgram(const char* fileName)
 {
-        // only try to load stuff if the extension is supported
-        if (!mExtensionReg->Has_GL_ARB_vertex_program())
-        {
-                return 0;
-        }
+  // only try to load stuff if the extension is supported
+  if (!mExtensionReg->Has_GL_ARB_vertex_program())
+    {
+      return 0;
+    }
 
-        return LoadARBProgram(GL_VERTEX_PROGRAM_ARB, fileName);
+  return LoadARBProgram(GL_VERTEX_PROGRAM_ARB, fileName);
 }
 
 /*!
-        Load a fragment program
+  Load a fragment program
 
-        returns 0 on fail, non-zero on success
+  returns 0 on fail, non-zero on success
 */
 unsigned int OpenGLServer::LoadARBFragmentProgram(const char* /*fileName*/)
 {
-        // only try to load stuff if the extension is supported
-//      if (!mExtensionReg->Has_GL_ARB_fragment_program())
-//      {
-//              return 0;
-//      }
+  // only try to load stuff if the extension is supported
+  //      if (!mExtensionReg->Has_GL_ARB_fragment_program())
+  //      {
+  //              return 0;
+  //      }
 
-//      return LoadARBProgram(GL_FRAGMENT_PROGRAM_ARB, fileName);
-    return 0;
+  //      return LoadARBProgram(GL_FRAGMENT_PROGRAM_ARB, fileName);
+  return 0;
 }
 
 /*!
-        Set up the OpenGL viewport, initialize extension registry
+  Set up the OpenGL viewport, initialize extension registry
 */
 bool OpenGLServer::ConstructInternal()
 {
-        if( SDL_Init(SDL_INIT_VIDEO) < 0 )
-        {
-                return false;
-        }
+  int result = SDL_Init(SDL_INIT_VIDEO);
 
-        const SDL_VideoInfo* info = SDL_GetVideoInfo();
-
-        int                     intValue;
-        bool            boolValue;
-        std::string strVal;
-
-        GetScript()->GetVariable("Viewport.redBits", intValue);
-        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, intValue);
-        GetScript()->GetVariable("Viewport.greenBits", intValue);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, intValue);
-        GetScript()->GetVariable("Viewport.blueBits", intValue);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, intValue);
-        GetScript()->GetVariable("Viewport.alphaBits", intValue);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, intValue);
-
-        GetScript()->GetVariable("Viewport.depthBits", intValue);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, intValue);
-        GetScript()->GetVariable("Viewport.stencilBits", intValue);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, intValue);
-
-        GetScript()->GetVariable("Viewport.doubleBuffer", boolValue);
-        if (boolValue)
-                SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1);
-        else
-                SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 0);
-
-        int flags = SDL_OPENGL;
-        GetScript()->GetVariable("Viewport.fullScreen", boolValue);
-        if (boolValue)
-                flags |= SDL_FULLSCREEN;
-
-        int xRes, yRes;
-        GetScript()->GetVariable("Viewport.xRes", xRes);
-        GetScript()->GetVariable("Viewport.yRes", yRes);
-
-        SDL_Surface *screen = SDL_SetVideoMode(xRes, yRes, info->vfmt->BitsPerPixel, flags);
-        if ( screen == NULL )
-        {
-                return false;
+  if( result < 0 )
+    {
+      GetLog()->Error() << "ERROR: (OpenGLServer) Could not init SDL. SDL_Init returned error "
+                        << result << endl;
+      return false;
     }
 
-        SDL_ShowCursor(SDL_DISABLE);
+  const SDL_VideoInfo* info = SDL_GetVideoInfo();
 
-        GetScript()->GetVariable("Application.title", strVal);
-        SDL_WM_SetCaption(strVal.c_str(), NULL);
+  int             intValue;
+  bool            boolValue;
+  string strVal;
 
-        mExtensionReg->Init();
+  GetScript()->GetVariable("Viewport.redBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, intValue);
+  GetScript()->GetVariable("Viewport.greenBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, intValue);
+  GetScript()->GetVariable("Viewport.blueBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, intValue);
+  GetScript()->GetVariable("Viewport.alphaBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, intValue);
 
-        // check if fancy lighting is supported
-        mSupportsFancyLighting = true;
-//      if (!mExtensionReg->Has_GL_ARB_vertex_program() || !mExtensionReg->Has_GL_ARB_fragment_program())
-//      {
-                GetLog()->Normal() << "WARNING: GL_ARB_vertex_program not supported ... disabling fancy lighting" << std::endl;
-                mSupportsFancyLighting = false;
-//      }
+  GetScript()->GetVariable("Viewport.depthBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, intValue);
+  GetScript()->GetVariable("Viewport.stencilBits", intValue);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, intValue);
 
-        glClear(GL_COLOR_BUFFER_BIT);
+  GetScript()->GetVariable("Viewport.doubleBuffer", boolValue);
+  if (boolValue)
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1);
+  else
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 0);
 
-        GetLog()->Normal() << "Initialized OpenGL Window" << std::endl;
-        const unsigned char* val = NULL;
-        val = glGetString(GL_RENDERER);
-        GetLog()->Normal() << "  GL_RENDERER:   " << val << std::endl;
-        val = glGetString(GL_VERSION);
-        GetLog()->Normal() << "  GL_VERSION:    " << val << std::endl;
-        val = glGetString(GL_EXTENSIONS);
-        GetLog()->Normal() << "  GL_EXTENSIONS: " << val << std::endl << std::endl;
+  int flags = SDL_OPENGL;
+  GetScript()->GetVariable("Viewport.fullScreen", boolValue);
+  if (boolValue)
+    flags |= SDL_FULLSCREEN;
 
-        return true;
+  int xRes, yRes;
+  GetScript()->GetVariable("Viewport.xRes", xRes);
+  GetScript()->GetVariable("Viewport.yRes", yRes);
+
+  SDL_Surface *screen = SDL_SetVideoMode(xRes, yRes, info->vfmt->BitsPerPixel, flags);
+  if ( screen == NULL )
+    {
+      GetLog()->Error() << "ERROR: (OpenGLServer) SDL_SetVideoMode() failed " << endl;
+      return false;
+    }
+
+  SDL_ShowCursor(SDL_DISABLE);
+
+  GetScript()->GetVariable("Application.title", strVal);
+  SDL_WM_SetCaption(strVal.c_str(), NULL);
+
+  mExtensionReg->Init();
+
+  // check if fancy lighting is supported
+  mSupportsFancyLighting = true;
+
+  //      if (!mExtensionReg->Has_GL_ARB_vertex_program() || !mExtensionReg->Has_GL_ARB_fragment_program())
+  //      {
+  GetLog()->Normal() << "WARNING: GL_ARB_vertex_program not supported ... "
+                     << "disabling fancy lighting" << endl;
+  mSupportsFancyLighting = false;
+  //      }
+
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  GetLog()->Normal() << "Initialized OpenGL Window" << endl;
+
+  const unsigned char* val = NULL;
+  val = glGetString(GL_RENDERER);
+
+  GetLog()->Normal() << "  GL_RENDERER:   " << val << endl;
+  val = glGetString(GL_VERSION);
+  GetLog()->Normal() << "  GL_VERSION:    " << val << endl;
+  val = glGetString(GL_EXTENSIONS);
+  GetLog()->Normal() << "  GL_EXTENSIONS: " << val << endl << endl;
+
+  return true;
 }
 
 void OpenGLServer::ToggleFancyLighting()
 {
-        if (mSupportsFancyLighting)
-                mSupportsFancyLighting = false;
-        else
-                mSupportsFancyLighting = true;
+  if (mSupportsFancyLighting)
+    mSupportsFancyLighting = false;
+  else
+    mSupportsFancyLighting = true;
 }
 
--- lib/kerosin/openglserver/openglserver.h
+++ lib/kerosin/openglserver/openglserver.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: openglserver.h,v 1.7 2003/11/14 14:05:52 fruit Exp $
+   $Id: openglserver.h,v 1.7.2.1 2003/11/20 20:15:59 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -56,9 +56,6 @@ private:
 public:
     OpenGLServer();
     ~OpenGLServer();
-
-    //! setup application window, initialize OpenGL, autoregister extensions
-    bool Init();
 
     boost::shared_ptr<GLExtensionReg> GetExtensionReg() const;
     //! if this is called, the application will 'want to quit'
