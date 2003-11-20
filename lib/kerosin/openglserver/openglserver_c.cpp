96a4f3c77450d623c0b0a4e0899f899d8370183c
Markus Rollmann <rollmark@uni-koblenz.de>
2003-11-20 20:16:57 +0000

    whitespace only

--- lib/kerosin/openglserver/openglserver_c.cpp
+++ lib/kerosin/openglserver/openglserver_c.cpp
@@ -4,8 +4,7 @@ using namespace boost;
 using namespace kerosin;
 using namespace zeitgeist;
 
-
 void CLASS(OpenGLServer)::DefineClass()
 {
-	DEFINE_BASECLASS(zeitgeist/Leaf);
+  DEFINE_BASECLASS(zeitgeist/Leaf);
 }
