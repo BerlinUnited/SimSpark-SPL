592284dddfdc09b55fe581b18c05656cf8b20c6d
Oliver Obst <oliverobst@gmail.com>
2003-12-01 16:17:49 +0000

    Added a base parser class BaseParser that the GameControlServer uses to hold a reference to the specific parser used by a simulation.

---
+++ lib/oxygen/gamecontrolserver/baseparser.h
@@ -0,0 +1,45 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: baseparser.h,v 1.1.2.1 2003/12/01 16:17:49 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef OXYGEN_BASEPARSER_H
+#define OXYGEN_BASEPARSER_H
+
+#include <list>
+#include <boost/any.hpp>
+#include <zeitgeist/class.h>
+#include <zeitgeist/node.h>
+
+namespace oxygen
+{
+
+class BaseParser : public zeitgeist::Leaf
+{
+public:
+    typedef std::list<boost::any> TPredicateList;
+
+    virtual TPredicateList Parse(const std::string input) = 0;
+};
+
+DECLARE_ABSTRACTCLASS(BaseParser);
+
+}
+
+#endif // OXYGEN_BASEPARSER_H
