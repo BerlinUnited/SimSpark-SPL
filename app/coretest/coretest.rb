9f95b1fd7ca253f22dd74ad9a3986ba0f4eb00c0
Oliver Obst <oliverobst@gmail.com>
2003-12-08 15:42:49 +0000

    whitespace, comments

--- app/coretest/coretest.rb
+++ app/coretest/coretest.rb
@@ -1,23 +1,22 @@
 importBundle "filesystemstd"
+importBundle "sexpparser"
 
 # mount a standard file system
-fileServer = get ('/sys/server/file');
-fileServer.mount ('FileSystemSTD', 'data/');
+fileServer = get('/sys/server/file');
+fileServer.mount('FileSystemSTD', 'data/');
 
 def testSoundSystem
-	importBundle "soundsystemfmod"
-	#importBundle "soundsystembass"
-	print "SoundServer Test\n"
-	print "Creating SoundServer\n"
-	soundServer = new ('kerosin/SoundServer', '/sys/server/sound');
+  importBundle "soundsystemfmod"
+  #importBundle "soundsystembass"
+  print "SoundServer Test\n"
+  print "Creating SoundServer\n"
+  soundServer = new('kerosin/SoundServer', '/sys/server/sound');
 
-	print "  Adding SoundSystem\n"
-	soundServer.init ('SoundSystemFMOD');
-	#soundServer.init ('SoundSystemBASS');
-	#playEffect "shotgun.wav"
-	playModule "BTGATESF.XM"
-	#playModule 'F:\Audio\Modules\Donna.xm'
-	#soundServer.playStream ('sinch.mp3');
+  print "  Adding SoundSystem\n"
+  soundServer.init('SoundSystemFMOD');
+  #soundServer.init('SoundSystemBASS');
+  #playModule "BTGATESF.XM"
+  soundServer.playStream('sinch.mp3');
 end
 
 testSoundSystem
