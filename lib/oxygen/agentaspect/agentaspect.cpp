acc1f8c6fc59fa2e825e73fc397921cf886b4615
Markus Rollmann <rollmark@uni-koblenz.de>
2003-12-10 10:26:35 +0000

    implemented UpdateEffectorMap. This method builds a map from a predicate string to the corresponding Effector below the AgentAspect node. implemented RealizeActions. This method traverses a list of ActionObjects and realizes them using the corresponding effector

--- lib/oxygen/agentaspect/agentaspect.cpp
+++ lib/oxygen/agentaspect/agentaspect.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: agentaspect.cpp,v 1.3.4.2 2003/12/09 20:23:59 rollmark Exp $
+   $Id: agentaspect.cpp,v 1.3.4.3 2003/12/10 10:26:35 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,23 +20,81 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #include "agentaspect.h"
-#include "../physicsserver/body.h"
+#include <zeitgeist/logserver/logserver.h>
 
 using namespace boost;
 using namespace oxygen;
 using namespace salt;
+using namespace std;
 
-bool AgentAspect::RealizeActions(boost::shared_ptr<ActionObject::TList> /*actions*/)
+bool
+AgentAspect::RealizeActions(boost::shared_ptr<ActionObject::TList> actions)
 {
-  // traverse the actionlist, find the corresponding effector
-  // for each action and realize it
+  UpdateEffectorMap();
+
+  for (
+       ActionObject::TList::iterator iter = actions->begin();
+       iter != actions->end();
+       ++iter
+       )
+    {
+      shared_ptr<ActionObject> action = (*iter);
+      std::string predicate = action->GetPredicate();
+
+      TEffectorMap::iterator iter = mEffectorMap.find(predicate);
+      if (iter == mEffectorMap.end())
+        {
+          GetLog()->Warning()
+            <<  "(AgentAspect) No effector found for predicate "
+            << predicate << "\n";
+          continue;
+        }
+
+      shared_ptr<Effector> effector = (*iter).second;
+      bool realized = effector->Realize(action);
+
+      if (! realized)
+        {
+          GetLog()->Warning()
+            << "(AgentAspect) Failed to realize predicate "
+            << predicate << "\n";
+        }
+    }
+
   return true;
 }
 
-BaseParser::TPredicateList AgentAspect::QueryPerceptors()
+BaseParser::TPredicateList
+AgentAspect::QueryPerceptors()
 {
   // traverse all Perceptors and build a list of the collected
   // Predicates
   return BaseParser::TPredicateList();
 }
 
+void AgentAspect::UpdateEffectorMap()
+{
+  // build list of effectors, searching recursively
+  TLeafList effectors;
+  GetChildrenSupportingClass("oxygen/Effector",effectors,true);
+
+  // build the effector map
+  mEffectorMap.clear();
+
+  for (
+       TLeafList::iterator iter = effectors.begin();
+       iter != effectors.end();
+       ++iter
+       )
+    {
+      shared_ptr<Effector> effector
+        = shared_dynamic_cast<Effector>(*iter);
+
+      if (effector.get() == 0)
+        {
+          continue;
+        }
+
+      mEffectorMap[effector->GetPredicate()] = effector;
+    }
+}
