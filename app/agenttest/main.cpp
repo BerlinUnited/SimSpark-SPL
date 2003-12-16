2c46f8e412b96fca02cc48abb4d3aedbd108f110
Markus Rollmann <rollmark@uni-koblenz.de>
2003-12-16 15:03:37 +0000

    app/agenttest is a dummy agent created for testing with the SpadesTest. It reads length prefixed data (see spades manual 6.3) from fd 3 and writes to fd 4. A corresponding agent DB (app/spadestest/agentdb.list, entry 'default') exists in the spadestest directory. Currently it does nothing but sending an 'init done' message, i.e. 'I' (see spades manual 6.4) back to spades.

---
+++ app/agenttest/main.cpp
@@ -0,0 +1,77 @@
+// This is a dummy agent created for testing with the SpadesTest. It
+// reads length prefixed data (see spades manual 6.3) from fd 3 and
+// writes to fd 4. A corresponding agent DB (agentdb.list, entry
+// 'default') exists in the spadestest directory.
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+static char buffer[4096];
+
+void
+log(const char* out)
+{
+  printf(out);
+  fflush(NULL);
+}
+
+int
+getInput()
+{
+  ssize_t sz = read(3, buffer, sizeof(buffer));
+
+  if ((unsigned int)sz < sizeof(long))
+    {
+      return 0;
+    }
+
+  long len;
+  memcpy(&len, buffer, sizeof(long));
+  len = ntohl(l);
+
+  // zero terminate received data
+  buffer[sizeof(long) + len] = 0;
+
+  return len;
+}
+
+void
+putOutput(const char* out)
+{
+  strcpy(&buffer[4], out);
+  long l = htonl(strlen(out));
+  memcpy(buffer,&l,sizeof(long));
+  write(4, buffer, l + sizeof(long));
+}
+
+int
+main(int /*argc*/, const char *const */*argv*/)
+{
+  log("AgentTest started\n");
+
+  //
+  // perfom any agent init here
+  //
+
+  // 'initialization done' message
+  log("writing init done message\n");
+  putOutput("I");
+
+  // the agent process will block after the init done message is
+  // written
+
+  char* data = &buffer[sizeof(long)];
+  while (true)
+    {
+      if (getInput())
+        {
+          log("received data: ");
+          log(data);
+          log("\n");
+        }
+    }
+}
