54189b719957a233990a337e4c6ea369549e2bc5
Oliver Obst <oliverobst@gmail.com>
2003-12-18 22:57:23 +0000

    added some of the required files. inputtest doesn't work yet

--- app/inputtest/inputtest.rb
+++ app/inputtest/inputtest.rb
@@ -3,9 +3,9 @@ print "start inputtest.rb\n"
 ####
 # Input Server
 #
-inputServer = new ('kerosin/InputServer', '/sys/server/input');
-# inputServer.init ('InputSystemSDL');
+inputServer = new('kerosin/InputServer', '/sys/server/input');
+inputServer.init('InputSystemSDL');
 # load german keyboard layout
-inputServer.importScanCodeMapping ('german.scan.rb');
+inputServer.importScanCodeMapping('german.scan.rb');
 
 print "end inputtest.rb\n"
