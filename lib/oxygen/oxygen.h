822a38dc761f9f346513ed21774222c65d3d6689
Markus Rollmann <rollmark@uni-koblenz.de>
2003-12-25 12:27:12 +0000

    removed CreateEffector include

--- lib/oxygen/oxygen.h
+++ lib/oxygen/oxygen.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: oxygen.h,v 1.4 2003/12/21 23:36:36 fruit Exp $
+   $Id: oxygen.h,v 1.4.2.1 2003/12/25 12:27:12 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -26,7 +26,6 @@
 #include "agentaspect/collisionperceptor.h"
 #include "agentaspect/effector.h"
 #include "agentaspect/perceptor.h"
-#include "agentaspect/createeffector.h"
 
 #include "controlaspect/controlaspect.h"
 
