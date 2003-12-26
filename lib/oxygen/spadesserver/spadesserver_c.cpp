a6102b5426c62af28c222dd826b6de622b1df336
Oliver Obst <oliverobst@gmail.com>
2003-12-26 16:28:39 +0000

    added ruby interface for the QueueAgents method

--- lib/oxygen/spadesserver/spadesserver_c.cpp
+++ lib/oxygen/spadesserver/spadesserver_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: spadesserver_c.cpp,v 1.2 2003/12/21 23:36:37 fruit Exp $
+   $Id: spadesserver_c.cpp,v 1.2.2.1 2003/12/26 16:28:39 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,7 +24,27 @@
 
 using namespace oxygen;
 
+FUNCTION(queueAgents)
+{
+    SpadesServer* ss = static_cast<SpadesServer*>(obj);
+    switch (in.size())
+    {
+    case 0:
+        ss->QueueAgents();
+        break;
+    case 1:
+        ss->QueueAgents(boost::any_cast<char*>(in[0]));
+        break;
+    case 2:
+        ss->QueueAgents(boost::any_cast<char*>(in[0]), boost::any_cast<int>(in[1]));
+        break;
+    default:
+        ;
+    }
+}
+
 void CLASS(SpadesServer)::DefineClass()
 {
     DEFINE_BASECLASS(zeitgeist/Node);
+    DEFINE_FUNCTION(queueAgents);
 }
