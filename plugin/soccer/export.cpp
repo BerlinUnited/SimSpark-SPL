f963ff3d55c30119c5117e8443e94d0c6bd7db2f
Markus Rollmann <rollmark@uni-koblenz.de>
2004-01-08 16:03:06 +0000

    exporting sexpmonitor and fieldflag to zeitgeist

--- plugin/soccer/export.cpp
+++ plugin/soccer/export.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: export.cpp,v 1.1.2.3 2004/01/08 12:44:57 rollmark Exp $
+   $Id: export.cpp,v 1.1.2.4 2004/01/08 16:03:06 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,11 +25,15 @@
 #include "initeffector/initeffector.h"
 #include "gamestateaspect/gamestateaspect.h"
 #include "agentstate/agentstate.h"
+#include "sexpmonitor/sexpmonitor.h"
+#include "fieldflag/fieldflag.h"
 
 ZEITGEIST_EXPORT_BEGIN()
         ZEITGEIST_EXPORT(GameStateAspect);
         ZEITGEIST_EXPORT(CreateEffector);
         ZEITGEIST_EXPORT(InitEffector);
         ZEITGEIST_EXPORT(AgentState);
+        ZEITGEIST_EXPORT(SexpMonitor);
+        ZEITGEIST_EXPORT(FieldFlag);
 ZEITGEIST_EXPORT_END()
 
