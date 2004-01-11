ebed4d4b6493dc4862335d33b9555bf47d97d481
Markus Rollmann <rollmark@uni-koblenz.de>
2004-01-11 11:23:52 +0000

    removed method NotifyCollisionPerceptor and GetTransformParent implemented method GetODESpace changed method HandleCollide to call the OnCollide callback of the affected collider. The collider in turn is responsible to construct a contact joint.

--- lib/oxygen/physicsserver/space.cpp
+++ lib/oxygen/physicsserver/space.cpp
@@ -3,7 +3,7 @@
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2003 Koblenz University
-   $Id: space.cpp,v 1.4.8.2 2004/01/09 13:45:08 rollmark Exp $
+   $Id: space.cpp,v 1.4.8.3 2004/01/11 11:23:52 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,11 +21,8 @@
 
 #include "space.h"
 #include "world.h"
-#include "body.h"
-#include "physicsserver.h"
+#include "collider.h"
 #include <oxygen/sceneserver/scene.h>
-#include <oxygen/sceneserver/transform.h>
-#include <oxygen/agentaspect/collisionperceptor.h>
 #include <zeitgeist/logserver/logserver.h>
 
 using namespace boost;
@@ -46,19 +43,25 @@ Space::Space() :
 Space::~Space()
 {
     if (mODEContactGroup)
-    {
-        //dJointGroupEmpty(mODEContactGroup);
+      {
         dJointGroupDestroy(mODEContactGroup);
-    }
+      }
 
     // release the ODE space
     if (mODESpace)
+      {
         dSpaceDestroy(mODESpace);
+      }
 }
 
 dSpaceID Space::GetODESpace() const
 {
-    return mODESpace;
+  return mODESpace;
+}
+
+dJointGroupID Space::GetODEJointGroup() const
+{
+  return mODEContactGroup;
 }
 
 void Space::Collide()
@@ -67,107 +70,47 @@ void Space::Collide()
     dSpaceCollide(mODESpace, this, collisionNearCallback);
 }
 
-shared_ptr<Transform> Space::GetTransformParent(Body* body)
-{
-  return shared_static_cast<Transform>
-    (make_shared(body->GetParentSupportingClass("Transform")));
-}
-
-void Space::NotifyCollisionPerceptor(Body* body, Body* collidee)
-{
-  if (
-      (body == 0) ||
-      (collidee == 0)
-      )
-    {
-      return;
-    }
-
-  shared_ptr<Transform> transformParent = GetTransformParent(body);
-
-  if (transformParent.get() == 0)
-    {
-      return;
-    }
-
-  shared_ptr<CollisionPerceptor> colPerceptor =
-    shared_static_cast<CollisionPerceptor>(transformParent->GetChildOfClass("CollisionPerceptor", true));
-
-  if (colPerceptor.get() == 0)
-    {
-      return;
-    }
-
-  colPerceptor->AddCollidee(GetTransformParent(collidee));
-}
-
 void Space::HandleCollide(dGeomID obj1, dGeomID obj2)
 {
-    // exit without doing anything if the two bodies are connected by a joint
-    if (
-        (dGeomGetClass(obj1) == dPlaneClass) &&
-        (dGeomGetClass(obj2) == dPlaneClass)
-        )
-      {
-        return;
-      }
-
+    // if the two bodies corresponding to the geoms are connected by a
+    // joint
     dBodyID ODEBody1 = dGeomGetBody(obj1);
     dBodyID ODEBody2 = dGeomGetBody(obj2);
 
     if (
-        (ODEBody1) && (ODEBody2) &&
+        (ODEBody1) &&
+        (ODEBody2) &&
         (dAreConnected (ODEBody1, ODEBody2))
         )
       {
         return;
       }
 
+    // dSpaceCollide(), is guaranteed to pass all intersecting geom
+    // pairs to the callback function, but depending on the internal
+    // algorithms used by the space it may also make mistakes and pass
+    // non-intersecting pairs. Thus we can not expect that dCollide()
+    // will return contacts for every pair passed to the callback.
     dContact contact;
-    if (dCollide (obj1, obj2, 0, &contact.geom, sizeof(dContactGeom)))
+    if (! dCollide (obj1, obj2, 0, &contact.geom, sizeof(dContactGeom)))
     {
-        Body* body1 = PhysicsServer::GetBody(ODEBody1);
-        Body* body2 = PhysicsServer::GetBody(ODEBody2);
-
-        // notify potential CollisionPerceptors
-        NotifyCollisionPerceptor(body1, body2);
-        NotifyCollisionPerceptor(body2, body1);
-
-        // ensure that a camera only collides whithout affecting the
-        // simulation
-        bool camera =
-          (
-           (
-            (body1) &&
-            (make_shared(body1->GetParent())->GetChildOfClass("Camera").get() != 0)
-            ) ||
-           (
-            (body2) &&
-            (make_shared(body2->GetParent())->GetChildOfClass("Camera").get() != 0)
-            )
-           );
-
-        if (! camera)
-        {
-            contact.surface.mode = dContactBounce;
-            contact.surface.mu = dInfinity;
-            contact.surface.bounce = 0.8f;
-            contact.surface.bounce_vel = 2.0f;
-        }
-        else
-        {
-            contact.surface.mode = 0;
-            contact.surface.mu = 0;
-        }
-
-        dJointID joint = dJointCreateContact (mWorld, mODEContactGroup, &contact);
-        dJointAttach (joint, ODEBody1, ODEBody2);
+      return;
     }
+
+    // lookup and notify the collider node
+    Collider* collider = Collider::GetCollider(obj1);
+    if (collider)
+      {
+        collider->OnCollision(obj2, contact);
+      }
 }
 
 bool Space::ConstructInternal()
 {
-    if (!ODEObject::ConstructInternal()) return false;
+    if (! ODEObject::ConstructInternal())
+      {
+        return false;
+      }
 
     // create the ode space, 0 indicates that this space should
     // not be inserted into another space, i.e. we always create a
@@ -180,9 +123,7 @@ bool Space::ConstructInternal()
 
     // create a joint group for the contacts
     mODEContactGroup = dJointGroupCreate(0);
-    if (mODEContactGroup == 0) return false;
-
-    return true;
+    return (mODEContactGroup != 0);
 }
 
 void Space::OnLink()
@@ -193,9 +134,13 @@ void Space::OnLink()
     {
         shared_ptr<World> world = shared_static_cast<World>(scene->GetChildOfClass("World"));
         if (world)
+          {
             mWorld = world->GetODEWorld();
+          }
         else
+          {
             GetLog()->Error() << "ERROR: Could not find a World to link to..." << std::endl;
+          }
     }
 }
 
@@ -209,3 +154,4 @@ void Space::PrePhysicsUpdateInternal(float /*deltaTime*/)
     // remove all contact joints
     dJointGroupEmpty (mODEContactGroup);
 }
+
