908867033fce630f4cfee53eae3e8926bc69141c
Markus Rollmann <rollmark@uni-koblenz.de>
2004-01-20 19:06:47 +0000

    added soccer/ball files. A ball constructs a simple sphere together with a collider

---
+++ plugin/soccer/ball/ball.h
@@ -0,0 +1,40 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: ball.h,v 1.1.2.1 2004/01/20 19:06:47 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef BALL_H
+#define BALL_H
+
+#include <oxygen/sceneserver/transform.h>
+
+/** \class Ball represents the ball on the soccer field ;) */
+class Ball : public oxygen::Transform
+{
+ public:
+  Ball() : Transform() {};
+  virtual ~Ball() {};
+
+  virtual void OnLink();
+};
+
+DECLARE_CLASS(Ball);
+
+
+#endif // BALL_H
