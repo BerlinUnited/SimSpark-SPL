1cd3e23925cfe6db0dcb199cb5200f174b8e7303
Markus Rollmann <rollmark@uni-koblenz.de>
2004-01-25 12:25:54 +0000

    changed ForceAction constructor to accept a string with the name of the predicate. This way the name of the predicate has to be explicitly set in the ForceEffector and occurs only in the ForceEffector, preventing the ActionObject and Effector to be out og sync wrt to the predicate name

--- plugin/forceeffector/forceaction.h
+++ plugin/forceeffector/forceaction.h
@@ -1,10 +1,10 @@
-/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+7/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: forceaction.h,v 1.2 2003/12/21 23:36:38 fruit Exp $
+   $Id: forceaction.h,v 1.2.4.1 2004/01/25 12:25:54 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -28,10 +28,11 @@
 class ForceAction : public oxygen::ActionObject
 {
 public:
-    ForceAction(salt::Vector3f force)
-        : ActionObject("force"), mForce(force) {}
+    ForceAction(const std::string& predicate, salt::Vector3f force)
+        : ActionObject(predicate), mForce(force) {}
     virtual ~ForceAction() {}
 
+    /** returns the stored force vector */
     const salt::Vector3f& GetForce() { return mForce; }
 
 protected:
