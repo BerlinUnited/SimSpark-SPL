b62e19c21146e08f2ddb7ac3356c507ec7f585e1
Oliver Obst <oliverobst@gmail.com>
2004-01-31 14:10:11 +0000

    - introduced a "force factor". The applied force to the robot is 	  multiplied with this factor (divided by the maximum force). The 	  default force factor is 500.0. 	- The applied dash vector is length restricted now. If the length 	  of the dash power vector is greater the the allowed maximum 	  length, the dash vector is reduced to the maximum length. The 	  default value of the maximum dash vector length is 100.0. 	- It is possible to use a (normally distributed) error to 	  calculate the resulting force applied to the robot. When 	  calculating the resulting force, the dash power is multiplied with 	  the force factor divided by the maximum dash power. The error is 	  added to the maximum dash power in this calculation. By default, 	  error is turned off. 	- changed the order of the dash power components. For the user, 	  the 3rd component of the dash power vector is used to let the 	  robot jump up. 	- the dash effector takes effect only if the sphere is touching 	  the ground.

--- plugin/soccer/dasheffector/dasheffector.h
+++ plugin/soccer/dasheffector/dasheffector.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: dasheffector.h,v 1.1.2.1 2004/01/25 12:09:24 rollmark Exp $
+   $Id: dasheffector.h,v 1.1.2.2 2004/01/31 14:10:11 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -27,9 +27,6 @@
 
 class DashEffector : public oxygen::Effector
 {
-    //
-    // functions
-    //
 public:
     DashEffector();
     virtual ~DashEffector();
@@ -44,6 +41,25 @@ public:
     virtual boost::shared_ptr<oxygen::ActionObject>
     GetActionObject(const oxygen::Predicate& predicate);
 
+    /** Set the force factor.
+     *
+     * The dash power vector is multiplied by this factor divided by the maximum length
+     * of the dash power vector.
+     */
+    void SetForceFactor(float force_factor);
+
+    /** Set the error distribution for calculating the applied force.
+     *
+     * The force applied to a sphere is the dash power vector multiplied by
+     * the force factor divided by (maximum length of the dash power vector + Error).
+     * The error is normaly distributed around zero with a given sigma. If sigma is <= 0,
+     * no error will be applied.
+     */
+    void SetSigma(float sigma);
+
+    /** Set the maximum length of the dash power vector. */
+    void SetMaxPower(float max_power);
+
     /** setup the reference to the agents body node */
     virtual void OnLink();
 
@@ -53,6 +69,16 @@ public:
 protected:
     /** the reference to the parents body node */
     boost::shared_ptr<oxygen::Body> mBody;
+
+    /** The force factor is the force applied to the body if the length of
+        the dash power vector is greater or equal to mMaxDashPower */
+    float mForceFactor;
+
+    /** sigma for the error distribution to calculate the applied force */
+    float mSigma;
+
+    /** The maximum length of the dash power vector. */
+    float mMaxPower;
 };
 
 DECLARE_CLASS(DashEffector);
