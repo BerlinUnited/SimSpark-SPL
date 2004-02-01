270ed2b6d817b27e1d6b2562d7a69340db8c9fa2
Oliver Obst <oliverobst@gmail.com>
2004-02-01 19:04:25 +0000

    - exporting methods setKickMargin, setForceFactor, setSteps, setNoiseParams,   setMaxPower to ruby

--- plugin/soccer/kickeffector/kickeffector_c.cpp
+++ plugin/soccer/kickeffector/kickeffector_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: kickeffector_c.cpp,v 1.1.2.1 2004/01/25 12:14:14 rollmark Exp $
+   $Id: kickeffector_c.cpp,v 1.1.2.2 2004/02/01 19:04:25 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,7 +24,60 @@
 
 using namespace oxygen;
 
+FUNCTION(setKickMargin)
+{
+    if (in.size() == 1)
+    {
+        KickEffector* ke = static_cast<KickEffector*>(obj);
+        ke->SetKickMargin(boost::any_cast<float>(in[0]));
+    }
+}
+
+FUNCTION(setForceFactor)
+{
+    if (in.size() == 1)
+    {
+        KickEffector* ke = static_cast<KickEffector*>(obj);
+        ke->SetForceFactor(boost::any_cast<float>(in[0]));
+    }
+}
+
+FUNCTION(setSteps)
+{
+    if (in.size() == 2)
+    {
+        KickEffector* ke = static_cast<KickEffector*>(obj);
+        ke->SetSteps(boost::any_cast<int>(in[0]),
+                     boost::any_cast<int>(in[1]));
+    }
+}
+
+FUNCTION(setNoiseParams)
+{
+    if (in.size() == 3)
+    {
+        KickEffector* ke = static_cast<KickEffector*>(obj);
+        ke->SetNoiseParams(boost::any_cast<float>(in[0]),
+                           boost::any_cast<float>(in[1]),
+                           boost::any_cast<float>(in[2]));
+    }
+}
+
+FUNCTION(setMaxPower)
+{
+    if (in.size() == 1)
+    {
+        KickEffector* ke = static_cast<KickEffector*>(obj);
+        ke->SetMaxPower(boost::any_cast<float>(in[0]));
+    }
+}
+
 void CLASS(KickEffector)::DefineClass()
 {
     DEFINE_BASECLASS(kerosin/Effector);
+    DEFINE_FUNCTION(setKickMargin);
+    DEFINE_FUNCTION(setForceFactor);
+    DEFINE_FUNCTION(setSteps);
+    DEFINE_FUNCTION(setNoiseParams);
+    DEFINE_FUNCTION(setMaxPower);
 }
