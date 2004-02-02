1371227686184402a21a1fa9fa58f240ee6eb663
Oliver Obst <oliverobst@gmail.com>
2004-02-02 18:31:52 +0000

    - exporting method to set noise params - exporting method to switch on/off noise - exporting method to set the predicate name

--- plugin/soccer/visionperceptor/visionperceptor_c.cpp
+++ plugin/soccer/visionperceptor/visionperceptor_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: visionperceptor_c.cpp,v 1.1.2.1 2004/01/22 06:44:10 fruit Exp $
+   $Id: visionperceptor_c.cpp,v 1.1.2.2 2004/02/02 18:31:52 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,8 +25,41 @@
 using namespace boost;
 using namespace oxygen;
 
+FUNCTION(setNoiseParams)
+{
+    if (in.size() == 4)
+    {
+        VisionPerceptor* vp = static_cast<VisionPerceptor*>(obj);
+        vp->SetNoiseParams(boost::any_cast<float>(in[0]),
+                           boost::any_cast<float>(in[1]),
+                           boost::any_cast<float>(in[2]),
+                           boost::any_cast<float>(in[3]));
+    }
+}
+
+FUNCTION(addNoise)
+{
+    if (in.size() == 1)
+    {
+        VisionPerceptor* vp = static_cast<VisionPerceptor*>(obj);
+        vp->AddNoise(boost::any_cast<bool>(in[0]));
+    }
+}
+
+FUNCTION(setPredicateName)
+{
+    if (in.size() == 1)
+    {
+        VisionPerceptor* vp = static_cast<VisionPerceptor*>(obj);
+        vp->SetPredicateName(boost::any_cast<char*>(in[0]));
+    }
+}
+
 void
 CLASS(VisionPerceptor)::DefineClass()
 {
     DEFINE_BASECLASS(kerosin/Perceptor);
+    DEFINE_FUNCTION(setNoiseParams);
+    DEFINE_FUNCTION(addNoise);
+    DEFINE_FUNCTION(setPredicateName);
 }
