673d85cf17abb49dbb58dcbb9765b80667a0c0c1
Markus Rollmann <rollmark@uni-koblenz.de>
2004-02-06 10:03:07 +0000

    - implemented GetBal, GetBallBody - changed GetAgentState to call GetTransformParent internally rather than expecting a reference as a paramete

--- plugin/soccer/soccerbase/soccerbase.cpp
+++ plugin/soccer/soccerbase/soccerbase.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: soccerbase.cpp,v 1.1.2.1 2004/02/05 10:09:03 fruit Exp $
+   $Id: soccerbase.cpp,v 1.1.2.2 2004/02/06 10:03:07 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,25 +20,39 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #include "soccerbase.h"
+#include <oxygen/physicsserver/body.h>
+#include <oxygen/agentaspect/perceptor.h>
+#include <oxygen/sceneserver/sceneserver.h>
+#include <oxygen/sceneserver/scene.h>
+#include <oxygen/sceneserver/transform.h>
+#include <soccer/agentstate/agentstate.h>
+#include <soccer/ball/ball.h>
 
 using namespace boost;
 using namespace oxygen;
 
 bool
-SoccerBase::GetSceneServer(const oxygen::BaseNode& base,
-                           boost::shared_ptr<oxygen::SceneServer>& scene_server)
+SoccerBase::GetSceneServer(const BaseNode& base,
+                           shared_ptr<SceneServer>& scene_server)
 {
     scene_server = shared_static_cast<SceneServer>
         (base.GetCore()->Get("/sys/server/scene"));
+
+    if (scene_server.get() == 0)
+    {
+        base.GetLog()->Error()
+            << "Error: (SoccerBase: " << base.GetName()
+            << ") scene server not found.\n";
+        return false;
+    }
+
     return true;
 }
 
-
 bool
-SoccerBase::GetTransformParent(const oxygen::BaseNode& base,
-                               boost::shared_ptr<oxygen::Transform>& transform_parent)
+SoccerBase::GetTransformParent(const BaseNode& base,
+                               shared_ptr<Transform>& transform_parent)
 {
-    // we want positions relative to the closest parent transform node
     transform_parent = shared_dynamic_cast<Transform>
         (make_shared(base.GetParentSupportingClass("Transform")));
 
@@ -54,15 +68,12 @@ SoccerBase::GetTransformParent(const oxygen::BaseNode& base,
 
 
 bool
-SoccerBase::GetAgentState(const oxygen::BaseNode& base,
-                          boost::shared_ptr<oxygen::Transform> parent,
-                          boost::shared_ptr<AgentState>& agent_state)
+SoccerBase::GetAgentState(const BaseNode& base,
+                          shared_ptr<AgentState>& agent_state)
 {
-    if (parent.get() == 0)
+    shared_ptr<Transform> parent;
+    if (! GetTransformParent(base,parent))
     {
-        base.GetLog()->Error()
-            << "Error: (SoccerBase: " << base.GetName()
-            << ") cannot get AgentState with no parent node\n";
         return false;
     }
 
@@ -76,24 +87,21 @@ SoccerBase::GetAgentState(const oxygen::BaseNode& base,
             << ") parent node has no AgentState child\n";
         return false;
     }
+
     return true;
 }
 
-
 bool
-SoccerBase::GetActiveScene(const oxygen::BaseNode& base,
-                           boost::shared_ptr<oxygen::SceneServer> scene_server,
-                           boost::shared_ptr<oxygen::Scene>& active_scene)
+SoccerBase::GetActiveScene(const BaseNode& base,
+                           shared_ptr<Scene>& active_scene)
 {
-    if (scene_server.get() == 0)
-    {
-        base.GetLog()->Error()
-            << "ERROR: (SoccerBase: " << base.GetName()
-            << ") no SceneServer to get active scene from\n";
-        return false;
-    }
+    shared_ptr<SceneServer> sceneServer;
+    if (! GetSceneServer(base,sceneServer))
+        {
+            return false;
+        }
 
-    active_scene = scene_server->GetActiveScene();
+    active_scene = sceneServer->GetActiveScene();
     if (active_scene.get() == 0)
     {
         base.GetLog()->Error()
@@ -103,6 +111,75 @@ SoccerBase::GetActiveScene(const oxygen::BaseNode& base,
     }
     return true;
 }
+
+bool
+SoccerBase::GetBody(const BaseNode& base, shared_ptr<Body>& body)
+{
+    shared_ptr<Transform> parent;
+    if (! GetTransformParent(base,parent))
+    {
+        return false;
+    }
+
+  body = shared_dynamic_cast<Body>(parent->GetChildOfClass("Body"));
+
+  if (body.get() == 0)
+    {
+      base.GetLog()->Error()
+          << "ERROR: (SoccerBase: " << base.GetName()
+          << ") parent node has no Body child.";
+      return false;
+    }
+
+  return true;
+}
+
+bool
+SoccerBase::GetBall(const BaseNode& base, shared_ptr<Ball>& ball)
+{
+    shared_ptr<Scene> scene;
+    if (! GetActiveScene(base,scene))
+        {
+            return false;
+        }
+
+    ball = shared_dynamic_cast<Ball>
+        (base.GetCore()->Get(scene->GetFullPath() + "Ball"));
+
+    if (ball.get() == 0)
+        {
+            base.GetLog()->Error()
+                << "ERROR: (SoccerBase: " << base.GetName()
+                << " found no ball node\n";
+            return false;
+        }
+
+    return true;
+}
+
+bool
+SoccerBase::GetBallBody(const BaseNode& base, shared_ptr<Body>& body)
+{
+    shared_ptr<Scene> scene;
+    if (! GetActiveScene(base,scene))
+        {
+            return false;
+        }
+
+   body = shared_dynamic_cast<Body>
+        (base.GetCore()->Get(scene->GetFullPath() + "Ball/physics"));
+
+   if (body.get() == 0)
+       {
+            base.GetLog()->Error()
+                << "ERROR: (SoccerBase: " << base.GetName()
+                << " found no ball body node\n";
+            return false;
+       }
+
+   return true;
+}
+
 
 salt::Vector3f
 SoccerBase::FlipView(const salt::Vector3f& pos, TTeamIndex ti)
