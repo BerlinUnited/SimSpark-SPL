9c665a073cc1d32ea32558ffd559b0a57a663858
Markus Rollmann <rollmark@uni-koblenz.de>
2004-02-10 19:45:21 +0000

    - better handling of KickIn and Goal situations; still rough edges - after a the ball leaves the field, the playmode changes immediately to KickIn, but the SoccerRuleAspect waits at least mKickInPauseTime until it places the ball back on the field - after a goal has happened the RuleAspect waits at least mGoalPauseTime until it places the ball back in the middle of the field and switches to KickOff

--- plugin/soccer/soccerruleaspect/soccerruleaspect.cpp
+++ plugin/soccer/soccerruleaspect/soccerruleaspect.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: soccerruleaspect.cpp,v 1.1.2.5 2004/02/08 15:22:30 rollmark Exp $
+   $Id: soccerruleaspect.cpp,v 1.1.2.6 2004/02/10 19:45:21 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 #include <soccer/soccerbase/soccerbase.h>
 #include <soccer/gamestateaspect/gamestateaspect.h>
 #include <soccer/ballstateaspect/ballstateaspect.h>
+#include <soccer/agentstate/agentstate.h>
 
 using namespace oxygen;
 using namespace boost;
@@ -41,23 +42,60 @@ SoccerRuleAspect::~SoccerRuleAspect()
 {
 }
 
-void SoccerRuleAspect::UpdateBeforeKickOff()
+void SoccerRuleAspect::MoveBall(const salt::Vector3f& pos)
 {
-    // before the game starts the ball should stay in the middle of
-    // the playing field
-    salt::Vector3f pos(0,mBallRadius,0);
     mBallBody->SetPosition(pos);
     mBallBody->SetVelocity(Vector3f(0,0,0));
     mBallBody->SetAngularVelocity(Vector3f(0,0,0));
 }
 
+void SoccerRuleAspect::UpdateBeforeKickOff()
+{
+    // before the game starts the ball should stay in the middle of
+    // the playing field
+    salt::Vector3f pos(0,mBallRadius,0);
+    MoveBall(pos);
+}
+
 void SoccerRuleAspect::UpdateKickOff()
 {
     // after the first agent touches the ball move to PM_PLAYON
-    if (mBallState->GetLastCollidingAgent().get() != 0)
+    shared_ptr<AgentAspect> agent;
+    TTime time;
+    if (mBallState->GetLastCollidingAgent(agent,time))
         {
             mGameState->SetPlayMode(PM_PlayOn);
         }
+}
+
+void SoccerRuleAspect::UpdateKickIn()
+{
+    // do nothing for the duration of mKickInPauseTime
+    if (mGameState->GetModeTime() < mKickInPauseTime)
+        {
+            return;
+        }
+
+    // after the first agent touches the ball move to PM_PLAYON. the
+    // time when the agent last touches the ball must be after the
+    // change to the KickIn mode
+    shared_ptr<AgentAspect> agent;
+    TTime time;
+    if (! mBallState->GetLastCollidingAgent(agent,time))
+        {
+            return;
+        }
+
+    TTime lastChange = mGameState->GetLastModeChange();
+    if (time > lastChange)
+        {
+            mGameState->SetPlayMode(PM_PlayOn);
+        } else
+            {
+                // move the ball back on the ground where it left the playing
+                // field
+                MoveBall(mLastValidBallPos);
+            }
 }
 
 bool SoccerRuleAspect::CheckBallLeftField()
@@ -67,16 +105,34 @@ bool SoccerRuleAspect::CheckBallLeftField()
             return false;
         }
 
-    // move the ball back on the ground where it left the playing field
-    Vector3f lastValid = mBallState->GetLastValidBallPosition();
-    lastValid[1] = mBallRadius;
-    mBallBody->SetPosition(lastValid);
+    // get the team of the last agent touching the ball and set the
+    // correct kick in playmode
+    shared_ptr<AgentAspect> agent;
+    TTime time;
+
+    if (mBallState->GetLastCollidingAgent(agent,time))
+        {
+            shared_ptr<AgentState> agentState;
+            if (SoccerBase::GetAgentState(agent,agentState))
+                {
+                    mGameState->SetPlayMode
+                        (
+                         (agentState->GetTeamIndex() == TI_LEFT) ?
+                         PM_KickIn_Right : PM_KickIn_Left
+                         );
+
+                    mLastValidBallPos =
+                        mBallState->GetLastValidBallPosition();
+                    mLastValidBallPos[1] = mBallRadius;
+                }
+        }
 
     return true;
 }
 
 bool SoccerRuleAspect::CheckGoal()
 {
+    // check if the ball is in one of the goals
     TTeamIndex idx = mBallState->GetGoalState();
     if (idx == TI_NONE)
         {
@@ -84,20 +140,8 @@ bool SoccerRuleAspect::CheckGoal()
         }
 
     // score the lucky team
-    if (idx == TI_LEFT)
-        {
-            // ball is in the left goal, score the right team
-            mGameState->ScoreTeam(TI_RIGHT);
-        } else
-            {
-                // ball is in the right goal, score the left team
-                mGameState->ScoreTeam(TI_LEFT);
-            }
-
-    // put the ball back in the middle of the playing field
-    salt::Vector3f pos(0,0,0);
-    SoccerBase::GetSoccerVar(*this,"BallRadius",pos[1]);
-    mBallBody->SetPosition(pos);
+    mGameState->ScoreTeam((idx == TI_LEFT) ? TI_RIGHT : TI_LEFT);
+    mGameState->SetPlayMode((idx == TI_LEFT) ? PM_Goal_Right : PM_Goal_Left);
 
     return true;
 }
@@ -117,6 +161,26 @@ void SoccerRuleAspect::UpdatePlayOn()
         }
 
     // other checks go here...
+}
+
+void SoccerRuleAspect::UpdateGoal()
+{
+    // check if the pause time after the goal has elapsed
+    if (mGameState->GetModeTime() < mGoalPauseTime)
+        {
+            return;
+        }
+
+    // put the ball back in the middle of the playing field
+    salt::Vector3f pos(0,0,0);
+    SoccerBase::GetSoccerVar(*this,"BallRadius",pos[1]);
+    MoveBall(pos);
+
+    // kick off for the opposite team
+    mGameState->SetPlayMode(
+                            mGameState->GetPlayMode() == PM_Goal_Left ?
+                            PM_KickOff_Right : PM_KickOff_Left
+                            );
 }
 
 void SoccerRuleAspect::Update(float deltaTime)
@@ -142,8 +206,19 @@ void SoccerRuleAspect::Update(float deltaTime)
             UpdatePlayOn();
             break;
 
-        case PM_KickOff:
+        case PM_KickOff_Left:
+        case PM_KickOff_Right:
             UpdateKickOff();
+            break;
+
+        case PM_KickIn_Left:
+        case PM_KickIn_Right:
+            UpdateKickIn();
+            break;
+
+        case PM_Goal_Left:
+        case PM_Goal_Right:
+            UpdateGoal();
             break;
 
         default:
@@ -168,6 +243,12 @@ void SoccerRuleAspect::OnLink()
 
     mBallRadius = 0.111f;
     SoccerBase::GetSoccerVar(*this,"BallRadius",mBallRadius);
+
+    mGoalPauseTime = 3;
+    SoccerBase::GetSoccerVar(*this,"RuleGoalPauseTime",mGoalPauseTime);
+
+    mKickInPauseTime = 1;
+    SoccerBase::GetSoccerVar(*this,"RuleKickingPauseTime",mKickInPauseTime);
 }
 
 void SoccerRuleAspect::OnUnlink()
