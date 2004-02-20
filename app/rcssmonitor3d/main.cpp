4cfeaf7fe1cb8b7e7b29fafe985f17b315b1b401
Markus Rollmann <rollmark@uni-koblenz.de>
2004-02-20 17:38:03 +0000

    - cosmetic change: use STR_PM_Unknown constant instead of a string constant

--- app/rcssmonitor3d/main.cpp
+++ app/rcssmonitor3d/main.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: main.cpp,v 1.5 2004/02/12 14:07:21 fruit Exp $
+   $Id: main.cpp,v 1.6 2004/02/20 17:38:03 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -509,7 +509,7 @@ void drawStatusText()
             mode = STR_PM_Goal_Left;
             break;
         default:
-            mode = "(unknown playmode)";
+            mode = STR_PM_Unknown;
             break;
 
             }
