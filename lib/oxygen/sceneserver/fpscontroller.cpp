954945a12a7d3611f00322f51a64ce04210ab4a5
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-09 12:13:57 +0000

    - cleaned up fpscontroller implementation - added additional diagnostic messages - using Body methods instead of directly calling ODE functions

--- lib/oxygen/sceneserver/fpscontroller.cpp
+++ lib/oxygen/sceneserver/fpscontroller.cpp
@@ -1,9 +1,8 @@
-/* -*- mode: c++ -*-
-
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2003 Koblenz University
-   $Id: fpscontroller.cpp,v 1.5 2004/02/12 14:07:23 fruit Exp $
+   $Id: fpscontroller.cpp,v 1.6 2004/03/09 12:13:57 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,6 +20,7 @@
 
 #include "fpscontroller.h"
 #include <oxygen/physicsserver/body.h>
+#include <zeitgeist/logserver/logserver.h>
 
 using namespace boost;
 using namespace oxygen;
@@ -28,123 +28,123 @@ using namespace salt;
 
 FPSController::FPSController() : BaseNode()
 {
-  mHAngle = 0.0f;
-  mVAngle = 0.0f;
-  mForward = false;
-  mBackward = false;
-  mLeft = false;
-  mRight = false;
-  mUp = false;
-  mDown = false;
+    mHAngle   = 0.0f;
+    mVAngle   = 0.0f;
+    mForward  = false;
+    mBackward = false;
+    mLeft     = false;
+    mRight    = false;
+    mUp       = false;
+    mDown     = false;
 }
 
 FPSController::~FPSController()
 {
 }
 
+void FPSController::OnLink()
+{
+    mBody = shared_dynamic_cast<Body>(make_shared(GetParent()));
+
+    if (mBody.get() == 0)
+        {
+            GetLog()->Error()
+                << "(FPSController) ERROR: found no parent body.\n";
+        }
+}
+
+void FPSController::OnUnlink()
+{
+    mBody.reset();
+}
+
 void FPSController::PrePhysicsUpdateInternal(float deltaTime)
 {
-  // determine force direction
-  Vector3f force(0,0,0);
-
-  if (mForward)  force.z() -= 1.0f;
-  if (mBackward) force.z() += 1.0f;
-  if (mRight)    force.x() += 1.0f;
-  if (mLeft)     force.x() -= 1.0f;
-  if (mUp)       force.y() += 1.0f;
-  if (mDown)     force.y() -= 1.0f;
-
-  if (force.Length() > 1.0f)
-    {
-      force.Normalize();
-    }
-
-  shared_ptr<Body> body = shared_static_cast<Body>(make_shared(GetParent()));
-
-  if (body.get() != NULL)
-    {
-      // constrain angles
-      if(mVAngle>88.0f)
+    if (mBody.get() == 0)
         {
-          mVAngle = 88.0f;
-        }
-      else if(mVAngle<-88.0f)
-        {
-          mVAngle = -88.0f;
+            return;
         }
 
-      Matrix matrix;
-      matrix.RotationY(gDegToRad(-mHAngle));
-      matrix.RotateX(gDegToRad(-mVAngle));
+    // determine force direction
+    Vector3f force(0,0,0);
 
-      dMatrix3 ODEMatrix;
-      dRSetIdentity(ODEMatrix);
-      ODEMatrix[0]=matrix.m[0];
-      ODEMatrix[4]=matrix.m[1];
-      ODEMatrix[8]=matrix.m[2];
-      ODEMatrix[1]=matrix.m[4];
-      ODEMatrix[5]=matrix.m[5];
-      ODEMatrix[9]=matrix.m[6];
-      ODEMatrix[2]=matrix.m[8];
-      ODEMatrix[6]=matrix.m[9];
-      ODEMatrix[10]=matrix.m[10];
-      dBodySetRotation(body->GetODEBody(), ODEMatrix);
+    if (mForward)  force.z() -= 1.0f;
+    if (mBackward) force.z() += 1.0f;
+    if (mRight)    force.x() += 1.0f;
+    if (mLeft)     force.x() -= 1.0f;
+    if (mUp)       force.y() += 1.0f;
+    if (mDown)     force.y() -= 1.0f;
 
-      force *= 20.0f/deltaTime;
-      force = matrix.Rotate(force);
-      dBodyAddForce(body->GetODEBody(), force.x(), force.y(), force.z());
+    if (force.Length() > 1.0f)
+        {
+            force.Normalize();
+        }
 
-      //force = - drag_coefficient * mass * velocity
-      dMass m;
-      dBodyGetMass(body->GetODEBody(), &m);
-      const dReal* vel = dBodyGetLinearVel(body->GetODEBody());
+    // constrain angles
+    if(mVAngle>88.0f)
+        {
+            mVAngle = 88.0f;
+        } else if(mVAngle<-88.0f)
+            {
+                mVAngle = -88.0f;
+            }
 
-      dBodyAddForce(
-                    body->GetODEBody(),
-                    -vel[0]*m.mass/deltaTime,
-                    -vel[1]*m.mass/deltaTime,
-                    -vel[2]*m.mass/deltaTime
-                    );
-    }
+    Matrix matrix;
+    matrix.RotationY(gDegToRad(-mHAngle));
+    matrix.RotateX(gDegToRad(-mVAngle));
+    mBody->SetRotation(matrix);
+
+    force *= 20.0f/deltaTime;
+    force = matrix.Rotate(force);
+    mBody->AddForce(force);
+
+    float mass = mBody->GetMass();
+    Vector3f vel = mBody->GetVelocity();
+
+    mBody->AddForce(
+                    Vector3f(
+                             -vel[0]*mass/deltaTime,
+                             -vel[1]*mass/deltaTime,
+                             -vel[2]*mass/deltaTime
+                             ));
 }
 
 void FPSController::AdjustHAngle(const float delta)
 {
-  mHAngle += delta;
+    mHAngle += delta;
 }
 
 void FPSController::AdjustVAngle(const float delta)
 {
-  mVAngle += delta;
+    mVAngle += delta;
 }
 
 void FPSController::Forward(const bool state)
 {
-  mForward = state;
+    mForward = state;
 }
 
 void FPSController::Backward(const bool state)
-{       mBackward = state;
+{
+    mBackward = state;
 }
 
 void FPSController::StrafeLeft(const bool state)
 {
-  mLeft = state;
+    mLeft = state;
 }
 
 void FPSController::StrafeRight(const bool state)
 {
-  mRight= state;
+    mRight= state;
 }
 
 void FPSController::Up(const bool state)
 {
-  mUp = state;
+    mUp = state;
 }
 
 void FPSController::Down(const bool state)
 {
-  mDown = state;
+    mDown = state;
 }
-
-
