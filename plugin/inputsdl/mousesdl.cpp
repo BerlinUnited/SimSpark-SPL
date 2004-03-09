da9d55d4dacb16794b338c2ec111a8346c24413a
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-09 12:47:37 +0000

    - cleaned up MouseSDL implementation

--- plugin/inputsdl/mousesdl.cpp
+++ plugin/inputsdl/mousesdl.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: mousesdl.cpp,v 1.3 2003/12/21 23:36:38 fruit Exp $
+   $Id: mousesdl.cpp,v 1.4 2004/03/09 12:47:37 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,8 +24,7 @@
 
 using namespace kerosin;
 
-MouseSDL::MouseSDL() :
-InputDeviceSDL()
+MouseSDL::MouseSDL() : InputDeviceSDL()
 {
 }
 
@@ -35,46 +34,63 @@ MouseSDL::~MouseSDL()
 
 bool MouseSDL::Init(kerosin::InputSystem *inputSystem)
 {
-        if (InputDevice::Init(inputSystem) == false) return false;
-
-        return true;
+    return InputDevice::Init(inputSystem);
 }
 
 int MouseSDL::EventFilter(const SDL_Event *event)
 {
-        // we only want mouse events
-        switch (event->type)
+    // we only want mouse events
+    switch (event->type)
         {
         case SDL_MOUSEBUTTONDOWN:
         case SDL_MOUSEBUTTONUP:
-                {
-                        InputServer::Input input(InputServer::eButton);
-                        if (event->button.button == SDL_BUTTON_LEFT)
-                                input.code = mInputSystem->GetInputServer()->IC_MOUSE_LEFT;
-                        else if (event->button.button == SDL_BUTTON_RIGHT)
-                                input.code = mInputSystem->GetInputServer()->IC_MOUSE_RIGHT;
-                        else if (event->button.button == SDL_BUTTON_MIDDLE)
-                                input.code = mInputSystem->GetInputServer()->IC_MOUSE_MIDDLE;
-                        else return 1;
+            {
+                InputServer::Input input(InputServer::eButton);
 
-                        input.data.l = (event->type == SDL_MOUSEBUTTONDOWN);
+                switch (event->button.button)
+                    {
+                    case SDL_BUTTON_LEFT:
+                        input.code =
+                            mInputSystem->GetInputServer()->IC_MOUSE_LEFT;
+                        break;
 
-                        mInputSystem->AddInputInternal(input);
-                        return 0;
-                }
-                break;
+                    case SDL_BUTTON_RIGHT:
+                        input.code =
+                            mInputSystem->GetInputServer()->IC_MOUSE_RIGHT;
+                        break;
+
+                    case SDL_BUTTON_MIDDLE:
+                        input.code =
+                            mInputSystem->GetInputServer()->IC_MOUSE_MIDDLE;
+                        break;
+
+                    default:
+                        return 1;
+                    }
+
+                input.data.l = (event->type == SDL_MOUSEBUTTONDOWN);
+                mInputSystem->AddInputInternal(input);
+                return 0;
+            }
+            break;
+
         case SDL_MOUSEMOTION:
-                {
-                        InputServer::Input input(InputServer::eAxis, mInputSystem->GetInputServer()->IC_AXISX);
-                        input.data.l = event->motion.xrel;
-                        mInputSystem->AddInputInternal(input);
+            {
+                InputServer::Input input
+                    (
+                     InputServer::eAxis,
+                     mInputSystem->GetInputServer()->IC_AXISX
+                     );
+                input.data.l = event->motion.xrel;
+                mInputSystem->AddInputInternal(input);
 
-                        input.code = mInputSystem->GetInputServer()->IC_AXISY;
-                        input.data.l = event->motion.yrel;
-                        mInputSystem->AddInputInternal(input);
-                        return 0;
-                }
-                break;
+                input.code = mInputSystem->GetInputServer()->IC_AXISY;
+                input.data.l = event->motion.yrel;
+                mInputSystem->AddInputInternal(input);
+                return 0;
+            }
+            break;
         }
-        return 1;
+
+    return 1;
 }
