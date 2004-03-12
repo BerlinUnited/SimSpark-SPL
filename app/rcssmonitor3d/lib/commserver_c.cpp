8d7c87efb79d1b570b23c242a53f0640dc0b5d0b
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-12 17:25:45 +0000

    - changed BaseClass definition to zeitgeist/Leaf

--- app/rcssmonitor3d/lib/commserver_c.cpp
+++ app/rcssmonitor3d/lib/commserver_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: commserver_c.cpp,v 1.1 2004/03/09 17:13:33 fruit Exp $
+   $Id: commserver_c.cpp,v 1.2 2004/03/12 17:25:45 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,5 +24,5 @@
 void
 CLASS(CommServer)::DefineClass()
 {
-    DEFINE_BASECLASS(zeitgeist/Object);
+    DEFINE_BASECLASS(zeitgeist/Leaf);
 }
