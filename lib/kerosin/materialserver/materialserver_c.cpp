4deca08b3c2ad80afa95041ff07ac5c8e8d9d9d1
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-20 12:49:53 +0000

    - removed function touch()

--- lib/kerosin/materialserver/materialserver_c.cpp
+++ lib/kerosin/materialserver/materialserver_c.cpp
@@ -1,20 +1,32 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: materialserver_c.cpp,v 1.2 2004/03/20 12:49:53 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include "materialserver.h"
 
 using namespace boost;
 using namespace kerosin;
 using namespace zeitgeist;
 
-FUNCTION(touch)
-{
-	if (in.size() == 1)
-	{
-		MaterialServer *ms = static_cast<MaterialServer*>(obj);
-		ms->Touch(any_cast<char*>(in[0]));
-	}
-}
-
 void CLASS(MaterialServer)::DefineClass()
 {
-	DEFINE_BASECLASS(zeitgeist/Leaf);
-	DEFINE_FUNCTION(touch);
+        DEFINE_BASECLASS(zeitgeist/Node);
 }
