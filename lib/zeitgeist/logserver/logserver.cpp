971481bba3f7d36bb6dd82f23eabc2f3b0c1f7f1
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-22 10:40:17 +0000

    - added GPL header and whitespace fixes

--- lib/zeitgeist/logserver/logserver.cpp
+++ lib/zeitgeist/logserver/logserver.cpp
@@ -1,3 +1,24 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: logserver.cpp,v 1.2 2004/03/22 10:40:17 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #include "logserver.h"
 #include "logserverstreambuf.h"
 #include <stdarg.h>
@@ -6,14 +27,14 @@ using namespace std;
 using namespace zeitgeist;
 
 LogServer::LogServer(unsigned int size) :
-Node(), ostream(new LogServerStreamBuf(size))
+    Node(), ostream(new LogServerStreamBuf(size))
 {
 }
 
 LogServer::~LogServer()
 {
-	flush();
-	
+    flush();
+
     // delete the created streambuf
     delete rdbuf();
 }
@@ -27,12 +48,12 @@ bool LogServer::RemoveStream(const std::ostream* stream)
 {
     return GetStreamBuf().RemoveStream(stream);
 }
-    
+
 unsigned int LogServer::GetPriorityMask(const std::ostream* stream) const
 {
     return GetStreamBuf().GetPriorityMask(stream);
 }
-    
+
 bool LogServer::SetPriorityMask(const std::ostream* stream, unsigned int mask)
 {
     return GetStreamBuf().SetPriorityMask(stream, mask);
@@ -46,13 +67,13 @@ LogServer& LogServer::Priority(unsigned int prio)
 
 void LogServer::Printf(const char *inFormat, ...)
 {
-	char copyBuffer[4096];
-	va_list args;
-	va_start(args, inFormat);
-	vsprintf(copyBuffer, inFormat, args);
-	va_end(args);
-	(*this) << copyBuffer;
-	this->flush();
+    char copyBuffer[4096];
+    va_list args;
+    va_start(args, inFormat);
+    vsprintf(copyBuffer, inFormat, args);
+    va_end(args);
+    (*this) << copyBuffer;
+    this->flush();
 }
 
 const LogServerStreamBuf& LogServer::GetStreamBuf() const
