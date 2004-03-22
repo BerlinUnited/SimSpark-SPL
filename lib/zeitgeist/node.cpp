299e448439ade95e0710471a83376006d85a4d98
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-22 10:37:41 +0000

    - added GNU header

--- lib/zeitgeist/node.cpp
+++ lib/zeitgeist/node.cpp
@@ -1,3 +1,31 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: node.cpp,v 1.6 2004/03/22 10:37:41 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Class
+
+   HISTORY:
+                04.06.2002 MK
+                        - initial version
+
+*/
 #include <boost/tokenizer.hpp>
 #include "node.h"
 #include <salt/path.h>
