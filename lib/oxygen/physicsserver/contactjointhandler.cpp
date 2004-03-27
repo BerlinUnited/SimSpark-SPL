2c8705a8c77195c13175ab0bfca350b447e59f97
Markus Rollmann <rollmark@uni-koblenz.de>
2004-03-27 11:40:31 +0000

    - changed HandleCollision to use FindChildSupportingClass template

--- lib/oxygen/physicsserver/contactjointhandler.cpp
+++ lib/oxygen/physicsserver/contactjointhandler.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: contactjointhandler.cpp,v 1.2 2004/02/12 14:07:23 fruit Exp $
+   $Id: contactjointhandler.cpp,v 1.2.2.1 2004/03/27 11:40:31 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -55,10 +55,7 @@ ContactJointHandler::HandleCollision(shared_ptr<Collider> collidee, dContact& co
       }
 
   // check if the collidee has a ContactJointHandler registered to it
-  shared_ptr<Leaf> handler =
-      collidee->GetChildSupportingClass("ContactJointHandler");
-
-  if (handler.get() == 0)
+  if (collidee->FindChildSupportingClass<ContactJointHandler>().get() == 0)
       {
           return;
       }
