336670498819a8e2e6b80a050753c5996465eb68
Oliver Obst <oliverobst@gmail.com>
2004-04-07 13:37:26 +0000

    whitespace only

--- app/agenttest/main.cpp
+++ app/agenttest/main.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: main.cpp,v 1.9 2004/03/22 18:12:00 fruit Exp $
+   $Id: main.cpp,v 1.10 2004/04/07 13:37:26 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -66,118 +66,123 @@ int teamUnum = 0;
 #define ENABLE_LOGGING 1
 
 #ifdef ENABLE_LOGGING
-void Log(const char* out)
+void
+Log(const char* out)
 {
-   printf(out);
-   fflush(0);
+    printf(out);
+    fflush(0);
 }
 #else
- #define Log(x)
+#define Log(x)
 #endif
 
-void printHelp()
+void
+printHelp()
 {
-  cout << "\nusage: agenttest [options]" << endl;
-  cout << "\noptions:" << endl;
-  cout << " --help      prints this message." << endl;
-  cout << " --teamname  sets the team name. " << endl;
-  cout << " --unum      sets the uniform number." << endl;
-  cout << "\n";
+    cout << "\nusage: agenttest [options]" << endl;
+    cout << "\noptions:" << endl;
+    cout << " --help      prints this message." << endl;
+    cout << " --teamname  sets the team name. " << endl;
+    cout << " --unum      sets the uniform number." << endl;
+    cout << "\n";
 }
 
-void ReadOptions(int argc, char* argv[])
+void
+ReadOptions(int argc, char* argv[])
 {
-  for( int i = 0; i < argc; i++)
+    for( int i = 0; i < argc; i++)
     {
-      if( strcmp( argv[i], "--teamname" ) == 0 )
+        if( strcmp( argv[i], "--teamname" ) == 0 )
         {
-          if( i+1  < argc)
+            if( i+1  < argc)
             {
-              teamName = argv[i+1];
-              ++i;
-              Log("setting teamname to ");
-              Log(teamName.c_str());
-              Log("\n");
+                teamName = argv[i+1];
+                ++i;
+                Log("setting teamname to ");
+                Log(teamName.c_str());
+                Log("\n");
             }
         }
-      else if( strcmp( argv[i], "--help" ) == 0 )
+        else if( strcmp( argv[i], "--help" ) == 0 )
         {
-          printHelp();
-          exit(0);
+            printHelp();
+            exit(0);
         } else if (strcmp( argv[i], "--unum" ) == 0 )
-            {
-                teamUnum = atoi(argv[i+1]);
-                ++i;
-                stringstream ss;
-                ss << "setting uniform number to " << teamUnum << "\n";
-                Log(ss.str().c_str());
-            }
+        {
+            teamUnum = atoi(argv[i+1]);
+            ++i;
+            stringstream ss;
+            ss << "setting uniform number to " << teamUnum << "\n";
+            Log(ss.str().c_str());
+        }
     }
 }
 
-int Init(int argc, char* argv[], Zeitgeist& zg)
+int
+Init(int argc, char* argv[], Zeitgeist& zg)
 {
-  // register agent classes
-  zg.GetCore()->RegisterClassObject(new CLASS(CommServer), "");
-  zg.GetCore()->RegisterClassObject(new CLASS(WorldModel), "");
-  zg.GetCore()->RegisterClassObject(new CLASS(KickNRun), "");
+    // register agent classes
+    zg.GetCore()->RegisterClassObject(new CLASS(CommServer), "");
+    zg.GetCore()->RegisterClassObject(new CLASS(WorldModel), "");
+    zg.GetCore()->RegisterClassObject(new CLASS(KickNRun), "");
 
-  // run init script
-  zg.GetCore()->GetScriptServer()->RunInitScript("agenttest.rb", "app/agenttest");
+    // run init script
+    zg.GetCore()->GetScriptServer()->RunInitScript("agenttest.rb", "app/agenttest");
 
-  // read command line options
-  ReadOptions(argc,argv);
+    // read command line options
+    ReadOptions(argc,argv);
 
-  // register and create the CommServer
-  comm = shared_dynamic_cast<CommServer>
-      (zg.GetCore()->Get("/sys/CommServer"));
+    // register and create the CommServer
+    comm = shared_dynamic_cast<CommServer>
+        (zg.GetCore()->Get("/sys/CommServer"));
 
-  if (comm.get() == 0)
-      {
-          Log("cannot find CommServer\n");
-          return 1;
-      }
+    if (comm.get() == 0)
+    {
+        Log("cannot find CommServer\n");
+        return 1;
+    }
 
-  // register and create the WorldModel
-  wm = shared_dynamic_cast<WorldModel>
-      (zg.GetCore()->Get("/sys/WorldModel"));
+    // register and create the WorldModel
+    wm = shared_dynamic_cast<WorldModel>
+        (zg.GetCore()->Get("/sys/WorldModel"));
 
-  if (wm.get() == 0)
-      {
-          Log("cannot find WorldModel\n");
-          return 1;
-      }
+    if (wm.get() == 0)
+    {
+        Log("cannot find WorldModel\n");
+        return 1;
+    }
 
-  // get the installed behavior
-  behave = shared_dynamic_cast<Soccer>
-       (zg.GetCore()->Get("sys/Behavior"));
+    // get the installed behavior
+    behave = shared_dynamic_cast<Soccer>
+        (zg.GetCore()->Get("sys/Behavior"));
 
-  if (behave.get() == 0)
-      {
-          Log("cannot find Behavior\n");
-          return 1;
-      }
+    if (behave.get() == 0)
+    {
+        Log("cannot find Behavior\n");
+        return 1;
+    }
 
-  behave->SetTeamName(teamName);
-  behave->SetTeamUnum(teamUnum);
+    behave->SetTeamName(teamName);
+    behave->SetTeamUnum(teamUnum);
 
-  return 0;
+    return 0;
 }
 
-void Run()
+void
+Run()
 {
-  Log("AgentTest started\n");
+    Log("AgentTest started\n");
 
-  while (true)
+    while (true)
     {
-      if (! comm->GetInput())
+        if (! comm->GetInput())
         {
-          break;
+            break;
         }
 
-      wm->Parse(comm->GetMsg());
+        wm->Parse(comm->GetMsg());
 
-      switch(comm->GetMsg()[0])
+        switch(comm->GetMsg()[0])
         {
         case 'D' :
             behave->ProcessInitMessage();
@@ -185,33 +190,34 @@ void Run()
 
         case 'K' :
             behave->ProcessThinkTimeMessage();
-          break;
+            break;
 
         case 'S' :
             behave->Think();
-          break;
+            break;
 
         default:
-          Log("received unknown data: ");
-          Log(comm->GetMsg());
-          Log("\n");
-          break;
+            Log("received unknown data: ");
+            Log(comm->GetMsg());
+            Log("\n");
+            break;
         }
     }
 }
 
-int main(int argc, char* argv[])
+int
+main(int argc, char* argv[])
 {
-  //init zeitgeist and oxygen
-  Zeitgeist zg("." PACKAGE_NAME);
-  Oxygen oyxgen(zg);
+    //init zeitgeist and oxygen
+    Zeitgeist zg("." PACKAGE_NAME);
+    Oxygen oyxgen(zg);
 
-  int ret = Init(argc, argv, zg);
+    int ret = Init(argc, argv, zg);
 
-  if (ret > 0)
-      {
-          return ret;
-      }
+    if (ret > 0)
+    {
+        return ret;
+    }
 
-  Run();
+    Run();
 }
