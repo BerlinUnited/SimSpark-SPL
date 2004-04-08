80ccc66ff84a40aee5e100f1fe074fd3caec16b8
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-08 06:58:35 +0000

    - ws only

--- lib/kerosin/imageserver/imageserver.cpp
+++ lib/kerosin/imageserver/imageserver.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2004 RoboCup Soccer Server 3D Maintenance Group
-   $Id: imageserver.cpp,v 1.4 2004/03/22 18:11:07 fruit Exp $
+   $Id: imageserver.cpp,v 1.5 2004/04/08 06:58:35 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -38,62 +38,62 @@ shared_ptr<FileServer> gFileServer;
 //------------------------------------------------------------------------------------------------
 ILHANDLE ILAPIENTRY FSOpen(const ILstring inName)
 {
-        return gFileServer->Open(inName);
-        //return fopen(inName, "rb");
+    return gFileServer->Open(inName);
+    //return fopen(inName, "rb");
 }
 
 ILvoid ILAPIENTRY FSClose(ILHANDLE handle)
 {
-        //printf("CLOSE\n");
-        salt::RFile *file = (salt::RFile*)handle;
-        file->Close();
-        delete file;
-        //fclose((FILE*)handle);
+    //printf("CLOSE\n");
+    salt::RFile *file = (salt::RFile*)handle;
+    file->Close();
+    delete file;
+    //fclose((FILE*)handle);
 }
 
 ILboolean ILAPIENTRY FSEof(ILHANDLE handle)
 {
-        salt::RFile *file = (salt::RFile*)handle;
+    salt::RFile *file = (salt::RFile*)handle;
 
-        const ILboolean val= file->Eof();
-        //gFileServer->GetLog()->Debug().Printf("eof: %x\n", val);
-        return val;
+    const ILboolean val= file->Eof();
+    //gFileServer->GetLog()->Debug().Printf("eof: %x\n", val);
+    return val;
 }
 
 ILint ILAPIENTRY FSGetc(ILHANDLE handle)
 {
-        salt::RFile *file = (salt::RFile*)handle;
+    salt::RFile *file = (salt::RFile*)handle;
 
-        const int val = file->Getc();
-        //gFileServer->GetLog()->Debug().Printf("getc: %x\n", val);
-        return val;
+    const int val = file->Getc();
+    //gFileServer->GetLog()->Debug().Printf("getc: %x\n", val);
+    return val;
 }
 
 ILint ILAPIENTRY FSRead(void *buffer, ILuint size, ILuint count, ILHANDLE handle)
 {
-        salt::RFile *file = (salt::RFile*)handle;
+    salt::RFile *file = (salt::RFile*)handle;
 
-        const size_t readSize = file->Read(buffer, size, count);
-        //gFileServer->GetLog()->Debug().Printf("read(%d, %d): %d\n", size, count, readSize);
-        return readSize;
+    const size_t readSize = file->Read(buffer, size, count);
+    //gFileServer->GetLog()->Debug().Printf("read(%d, %d): %d\n", size, count, readSize);
+    return readSize;
 }
 
 ILint ILAPIENTRY FSSeek(ILHANDLE handle, ILint offset, ILint origin)
 {
-        salt::RFile *file = (salt::RFile*)handle;
+    salt::RFile *file = (salt::RFile*)handle;
 
-        const int val = file->Seek(offset, origin);
-        //gFileServer->GetLog()->Debug().Printf("seek: %d\n", val);
-        return val;
+    const int val = file->Seek(offset, origin);
+    //gFileServer->GetLog()->Debug().Printf("seek: %d\n", val);
+    return val;
 }
 
 ILint ILAPIENTRY FSTell(ILHANDLE handle)
 {
-        salt::RFile *file = (salt::RFile*)handle;
+    salt::RFile *file = (salt::RFile*)handle;
 
-        const int val = file->Tell();
-        //gFileServer->GetLog()->Debug().Printf("tell: %d\n", val);
-        return val;
+    const int val = file->Tell();
+    //gFileServer->GetLog()->Debug().Printf("tell: %d\n", val);
+    return val;
 }
 
 //------------------------------------------------------------------------------------------------
@@ -103,24 +103,24 @@ ILint ILAPIENTRY FSTell(ILHANDLE handle)
 // constructor
 ImageServer::ImageServer()
 {
-        // initialize DevIL
-        ilInit();
+    // initialize DevIL
+    ilInit();
 
-        // and setup the default behavior
-        // (this might come out of a config file at a later point)
-        ilEnable(IL_FILE_OVERWRITE);
-        ilEnable(IL_ORIGIN_SET);
-        ilOriginFunc(IL_ORIGIN_UPPER_LEFT);
+    // and setup the default behavior
+    // (this might come out of a config file at a later point)
+    ilEnable(IL_FILE_OVERWRITE);
+    ilEnable(IL_ORIGIN_SET);
+    ilOriginFunc(IL_ORIGIN_UPPER_LEFT);
 
-        // register FileServer hooks for DevIL
-        ilSetRead(
-                FSOpen,
-                FSClose,
-                FSEof,
-                FSGetc,
-                FSRead,
-                FSSeek,
-                FSTell);
+    // register FileServer hooks for DevIL
+    ilSetRead(
+              FSOpen,
+              FSClose,
+              FSEof,
+              FSGetc,
+              FSRead,
+              FSSeek,
+              FSTell);
 }
 
 //
@@ -130,52 +130,52 @@ ImageServer::ImageServer()
 //
 boost::shared_ptr<Image> ImageServer::Load(const char *inName, ILenum inType)
 {
-        // create a new image
-        boost::shared_ptr<Image> image(new Image());
+    // create a new image
+    boost::shared_ptr<Image> image(new Image());
 
-        // make it active with DevIL
-        image->Bind();
+    // make it active with DevIL
+    image->Bind();
 
-        // set the file server
-        gFileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
+    // set the file server
+    gFileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
 
-        // load the image
-        ilLoad(inType, (ILstring)inName);
+    // load the image
+    ilLoad(inType, (ILstring)inName);
 
-        // set the file server to 0 again
-        gFileServer.reset();
+    // set the file server to 0 again
+    gFileServer.reset();
 
-        // check for errors
-        if(HandleErrors() == true)
+    // check for errors
+    if(HandleErrors() == true)
         {
-                // release the image and return
-                return boost::shared_ptr<Image>();
+            // release the image and return
+            return boost::shared_ptr<Image>();
         }
 
-        return image;
+    return image;
 }
 
 bool ImageServer::Save(const boost::shared_ptr<Image> &inImage, const char *inName, ILenum inType)
 {
-        // make the image active
-        inImage->Bind();
+    // make the image active
+    inImage->Bind();
 
-        // set the file server
-        gFileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
+    // set the file server
+    gFileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
 
-        // save the image
-        ilSave(inType, (ILstring)inName);
+    // save the image
+    ilSave(inType, (ILstring)inName);
 
-        // set the file server to 0 again
-        gFileServer.reset();
+    // set the file server to 0 again
+    gFileServer.reset();
 
-        // check for errors
-        if(HandleErrors() == true)
+    // check for errors
+    if(HandleErrors() == true)
         {
-                return false;
+            return false;
         }
 
-        return true;
+    return true;
 }
 
 //
@@ -184,15 +184,15 @@ bool ImageServer::Save(const boost::shared_ptr<Image> &inImage, const char *inNa
 //
 bool ImageServer::HandleErrors()
 {
-        bool ret = false;
-        ILenum error;
+    bool ret = false;
+    ILenum error;
 
-        // check if we have any errors and log them accordingly
-        while ((error = ilGetError()) != IL_NO_ERROR)
+    // check if we have any errors and log them accordingly
+    while ((error = ilGetError()) != IL_NO_ERROR)
         {
-                ret = true;
-                GetLog()->Error().Printf("DevIL Error: %x\n", error);
+            ret = true;
+            GetLog()->Error().Printf("DevIL Error: %x\n", error);
         }
 
-        return ret;
+    return ret;
 }
