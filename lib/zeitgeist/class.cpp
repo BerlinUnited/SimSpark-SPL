78a7ab40b563811163dba3a9582d56f7b7dde00d
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-10 08:18:19 +0000

    - indentation fixes - moved zeitgeist CLASS into class_c.cpp

--- lib/zeitgeist/class.cpp
+++ lib/zeitgeist/class.cpp
@@ -1,10 +1,10 @@
-/* -*- mode: c++; c-basic-indent: 4; indent-tabs-mode: nil -*-
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
    this file is part of rcssserver3D
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: class.cpp,v 1.4 2003/08/27 22:39:24 fruit Exp $
+   $Id: class.cpp,v 1.5 2004/04/10 08:18:19 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,15 +22,13 @@
 #include "class.h"
 #include "leaf.h"
 #include "core.h"
-
 #include <iostream>
 
 using namespace boost;
 using namespace std;
 using namespace zeitgeist;
 
-Class::Class(const std::string &name) :
-Leaf(name)
+Class::Class(const std::string &name) : Leaf(name)
 {
 }
 
@@ -38,24 +36,19 @@ Class::~Class()
 {
     //cout << "~Class() '" << GetName() << "'\n";
 
-    /*if (GetName().compare("ClassClass") == 0)
-      {
-      FILE *f = fopen("class.log", "wb");
-      fclose(f);
-      }*/
-
     if (mInstances.size() > 0)
     {
         cout << "  Leaked " << mInstances.size() << " instances..." << endl;
-        for (TObjectList::iterator i = mInstances.begin(); i != mInstances.end(); ++i)
+        for (
+             TObjectList::iterator i = mInstances.begin();
+             i != mInstances.end(); ++i
+             )
         {
-            // weak_ptr doesn't support get anymore. The correct
-            // way to get the address is to use lock() to get
-            // a shared_ptr from the weak_ptr
             if (shared_ptr<Object> j = i->lock())
             {
                 cout << "    " << j.get() << endl;
-            } else {
+            } else
+            {
                 cout << "    " << "(expired)" << endl;
             }
         }
@@ -69,7 +62,8 @@ boost::shared_ptr<Object> Class::Create()
 
     if (obj.get())
     {
-        if (obj->Construct(obj, shared_static_cast<Class>(make_shared(GetSelf()))) == true)
+        if (obj->Construct(obj, shared_static_cast<Class>
+                           (make_shared(GetSelf()))) == true)
         {
             // successfully constructed
             AttachInstance(obj);
@@ -99,10 +93,12 @@ void Class::DetachInstance(const boost::weak_ptr<Object> &instance)
     // operator== is not implemented for weak_ptr
     TObjectList::iterator first = mInstances.begin();
     TObjectList::iterator last = mInstances.end();
+
     while (first != last)
     {
         TObjectList::iterator next = first;
         ++next;
+
         boost::shared_ptr<Object> i = first->lock();
         boost::shared_ptr<Object> j = instance.lock();
 
@@ -138,20 +134,28 @@ Class::TCmdProc Class::GetCmdProc(const std::string &functionName)
         return (*cmd).second;
     }
 
+    // ok, we don't have the requested function, so we'll try the base
+    // class objects
     shared_ptr<Leaf> classDir = GetCore()->Get("/classes");
-    // ok, we don't have the requested function, so we'll try the base class
-    // objects
-    for (TStringList::iterator baseClass = mBaseClasses.begin(); baseClass != mBaseClasses.end(); ++baseClass)
+
+    for (
+         TStringList::iterator baseClass = mBaseClasses.begin();
+         baseClass != mBaseClasses.end();
+         ++baseClass
+         )
     {
-        // this should get the base class object (it has to live on the same level of the hierarchy
-        // as this class object)
-        shared_ptr<Class> theClass = shared_static_cast<Class>(GetCore()->Get(*baseClass, classDir));
-        //printf("%x:'%s'.theClass: %s - %d\n", GetCore(), GetName().c_str(), (*baseClass).c_str(), theClass);
+        // this should get the base class object (it has to live on
+        // the same level of the hierarchy as this class object)
+        shared_ptr<Class> theClass = shared_static_cast<Class>
+            (GetCore()->Get(*baseClass, classDir));
+
         if (theClass)
         {
-            // now, we ask the class object, if it knows the command in question
+            // now, we ask the class object, if it knows the command
+            // in question
             TCmdProc theCmd = theClass->GetCmdProc(functionName);
             //printf("theCmd: %s - %d\n", functionName.c_str(), theCmd);
+
             if (theCmd != NULL)
             {
                 // here we have found the command and return it
@@ -170,29 +174,36 @@ const Class::TStringList& Class::GetBaseClasses() const
 
 bool Class::Supports(const std::string &name) const
 {
-    if (GetName().compare(name) == 0) return true;
+    if (GetName().compare(name) == 0)
+    {
+        return true;
+    }
 
     // check base-classes
     shared_ptr<Leaf> classDir = GetCore()->Get("/classes");
 
-    for (TStringList::const_iterator i = mBaseClasses.begin(); i != mBaseClasses.end(); ++i)
+    for (
+         TStringList::const_iterator i = mBaseClasses.begin();
+         i != mBaseClasses.end();
+         ++i
+         )
     {
-        shared_ptr<Class> theClass = shared_static_cast<Class>(GetCore()->Get(*i, classDir));
+        shared_ptr<Class> theClass = shared_static_cast<Class>
+            (GetCore()->Get(*i, classDir));
 
         if (theClass)
         {
-            if (theClass->Supports(name)) return true;
+            if (theClass->Supports(name))
+            {
+                return true;
+            }
         }
         else
         {
-            cout << "WARNING: Illegal BaseClass '" << (*i) << "' in Class '" << GetName() << "'" << endl;
+            cout << "(CLASS) WARNING: Illegal BaseClass '" << (*i)
+                 << "' in Class '" << GetName() << "'" << endl;
         }
     }
 
     return false;
-}
-
-void CLASS(Class)::DefineClass()
-{
-    DEFINE_BASECLASS(zeitgeist/Leaf);
 }
