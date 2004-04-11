1a5ce7b11cbd3e033efea4184fde21d29b9b5dd8
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-11 17:06:55 +0000

    - changed SceneImporter interface to accept a list of parameters

--- lib/oxygen/sceneserver/sceneimporter.h
+++ lib/oxygen/sceneserver/sceneimporter.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: sceneimporter.h,v 1.2 2004/04/10 09:19:44 rollmark Exp $
+   $Id: sceneimporter.h,v 1.3 2004/04/11 17:06:55 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -37,7 +37,8 @@ public:
     virtual ~SceneImporter() {};
 
     virtual bool ImportScene(const std::string& fileName,
-                             boost::shared_ptr<BaseNode> root) = 0;
+                             boost::shared_ptr<BaseNode> root,
+                             boost::shared_ptr<zeitgeist::ParameterList> parameter) = 0;
 };
 
 DECLARE_ABSTRACTCLASS(SceneImporter);
