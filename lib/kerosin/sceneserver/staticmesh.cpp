42c9f637681d4f33e0fbb5c946d308cbeb239b4c
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-18 16:26:25 +0000

    - some cleanup and ws changes - using shared_ptrs for mesh storage - removed unused lighting code

--- lib/kerosin/sceneserver/staticmesh.cpp
+++ lib/kerosin/sceneserver/staticmesh.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: staticmesh.cpp,v 1.7 2004/04/08 07:27:41 rollmark Exp $
+   $Id: staticmesh.cpp,v 1.8 2004/04/18 16:26:26 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,13 +20,14 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #include "staticmesh.h"
-#include "../openglserver/openglserver.h"
-#include "../materialserver/material.h"
-#include "../materialserver/materialserver.h"
+#include <zeitgeist/logserver/logserver.h>
 #include <zeitgeist/fileserver/fileserver.h>
 #include <zeitgeist/scriptserver/scriptserver.h>
-#include "helper/NVMeshMender.h"
+#include <kerosin/openglserver/openglserver.h>
+#include <kerosin/materialserver/material.h>
+#include <kerosin/materialserver/materialserver.h>
 #include <salt/fileclasses.h>
+#include "helper/NVMeshMender.h"
 
 using namespace boost;
 using namespace kerosin;
@@ -39,27 +40,61 @@ using namespace oxygen;
 StaticMesh::StaticMesh()
 {
     mVertexCount = 0;
-    mPos = 0;
-    mTexCoords = 0;
-    mBasisX = 0;
-    mBasisY = 0;
 }
 
 StaticMesh::~StaticMesh()
 {
-    if (mVertexCount)
+}
+
+void StaticMesh::ComputeBoundingBox()
+{
+}
+
+void StaticMesh::CalcBoundingBox()
+{
+    mLocalBoundingBox.Init();
+
+    const int n = mVertexCount * 3;
+
+    float* arPos = mPos.get();
+    if (arPos == 0)
         {
-            delete[] mPos;
-            delete[] mTexCoords;
-            delete[] mBasisX;
-            delete[] mBasisY;
+            return;
         }
+
+    for(int i = 0; i<n; ++i)
+        {
+            float* v = arPos + (i * 3);
+            mLocalBoundingBox.Encapsulate(v[0], v[1], v[2]);
+        }
+}
+
+void StaticMesh::RenderInternal()
+{
+    glVertexPointer(3, GL_FLOAT, 0, mPos.get());
+    glEnableClientState ( GL_VERTEX_ARRAY );
+
+    glClientActiveTextureARB ( GL_TEXTURE0_ARB );
+    glTexCoordPointer( 3, GL_FLOAT, 0, mTexCoords.get());
+    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
+
+    for (unsigned int i=0; i < mMaterials.size(); ++i)
+        {
+            mMaterials[i]->Bind();
+            glDrawElements(GL_TRIANGLES, mFaceCount[i]*3,
+                           GL_UNSIGNED_INT, mFaces[i].get());
+        }
+
+    glDisableClientState (GL_VERTEX_ARRAY);
+    glClientActiveTextureARB ( GL_TEXTURE0_ARB );
+    glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
 }
 
 bool StaticMesh::Load(const std::string& fileName)
 {
     // open file
-    shared_ptr<FileServer> fileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
+    shared_ptr<FileServer> fileServer = shared_static_cast<FileServer>
+        (GetCore()->Get("/sys/server/file"));
     shared_ptr<salt::RFile> file = fileServer->Open(fileName.c_str());
 
     if (file.get() == 0)
@@ -67,28 +102,28 @@ bool StaticMesh::Load(const std::string& fileName)
             return false;
         }
 
-    NVMeshMender                                    meshmender;
-    NVMeshMender::VAVector                  input, output;
-    NVMeshMender::VertexAttribute   att;
+    NVMeshMender meshmender;
+    NVMeshMender::VAVector input, output;
+    NVMeshMender::VertexAttribute  att;
 
     char buffer[1024];
     int temp;
     int i;
 
-    printf("Loading '%s'...\n", fileName.c_str());
+    GetLog()->Normal() << "Loading " << fileName << "\n";
     file->Gets(buffer, 1024);
     sscanf(buffer, "TotalTriCount: %d", &temp);
-    printf("  TotalTriCount: %d\n", temp);
+    GetLog()->Normal() << "  TotalTriCount: " << temp << "\n";
 
     int vertexCount;
     file->Gets(buffer, 1024);
     sscanf(buffer, "VertexCount:   %d", &vertexCount);
-    printf("  VertexCount:   %d\n", vertexCount);
+    GetLog()->Normal() << "  VertexCount:   " << vertexCount << "\n";
 
     int meshCount;
     file->Gets(buffer, 1024);
     sscanf(buffer, "MeshCount:     %d", &meshCount);
-    printf("  MeshCount:     %d\n", meshCount);
+    GetLog()->Normal() << "  MeshCount:     " << meshCount << "\n";
     file->Gets(buffer, 1024);
 
     // load position and texture coordinates of this object
@@ -110,6 +145,7 @@ bool StaticMesh::Load(const std::string& fileName)
             sscanf(buffer, "%f %f", &x, &y);
             tex0.floatVector_.push_back(x); tex0.floatVector_.push_back(y); tex0.floatVector_.push_back(1);
         }
+
     input.push_back(position);
     input.push_back(tex0);
 
@@ -202,52 +238,43 @@ bool StaticMesh::Load(const std::string& fileName)
             if (output[i].Name_ == "position")
                 {
                     mVertexCount = output[i].floatVector_.size()/3;
-                    mPos = new Float3[mVertexCount];
-                    for (int j = 0; j < mVertexCount; ++j)
+                    mPos = shared_array<float>(new float[mVertexCount * 3]);
+                    for (int j = 0; j < (mVertexCount*3); ++j)
                         {
-                            mPos[j].x = output[i].floatVector_[3*j];
-                            mPos[j].y = output[i].floatVector_[3*j + 1];
-                            mPos[j].z = output[i].floatVector_[3*j + 2];
+                            mPos[j] = output[i].floatVector_[j];
                         }
                 }
 
             if (output[i].Name_ == "tex0")
                 {
                     int size = output[i].floatVector_.size()/3;
-                    mTexCoords = new Float3[size];
+                    mTexCoords = shared_array<float>(new float[size * 3]);
 
-                    for (int j = 0; j < size; ++j)
+                    for (int j = 0; j < (size*3); ++j)
                         {
-                            mTexCoords[j].x = output[i].floatVector_[3*j];
-                            mTexCoords[j].y = output[i].floatVector_[3*j + 1];
-                            mTexCoords[j].z = output[i].floatVector_[3*j + 2];
+                            mTexCoords[j] = output[i].floatVector_[j];
                         }
                 }
 
             if (output[i].Name_ == "tangent")
                 {
                     int size = output[i].floatVector_.size()/3;
-                    mBasisX = new Float3[size];
+                    mBasisX = shared_array<float>(new float[size * 3]);
 
-                    for (int j = 0; j < size; ++j)
+                    for (int j = 0; j < (size*3); ++j)
                         {
-
-                            mBasisX[j].x = output[i].floatVector_[3*j];
-                            mBasisX[j].y = output[i].floatVector_[3*j + 1];
-                            mBasisX[j].z = output[i].floatVector_[3*j + 2];
+                            mBasisX[j] = output[i].floatVector_[j];
                         }
                 }
 
             if (output[i].Name_ == "binormal")
                 {
                     int size = output[i].floatVector_.size()/3;
-                    mBasisY = new Float3[size];
+                    mBasisY = shared_array<float>(new float[size * 3]);
 
-                    for (int j = 0; j < size; ++j)
+                    for (int j = 0; j < (size * 3); ++j)
                         {
-                            mBasisY[j].x = output[i].floatVector_[3*j];
-                            mBasisY[j].y = output[i].floatVector_[3*j + 1];
-                            mBasisY[j].z = output[i].floatVector_[3*j + 2];
+                            mBasisY[j] = output[i].floatVector_[j];
                         }
                 }
 
@@ -280,7 +307,9 @@ bool StaticMesh::Load(const std::string& fileName)
     for (i = 0; i<meshCount; ++i)
         {
             //printf("Resorting: %d %d\n", i, mFaceCount[i]);
-            mFaces[i] = new Face[mFaceCount[i]];
+            mFaces[i] = shared_array<unsigned int>
+                (new unsigned int[mFaceCount[i] * 3]);
+            unsigned int* faces = mFaces[i].get();
 
             int current = 0;
             for (int f = 0; f < totalFaceCount; ++f)
@@ -288,10 +317,13 @@ bool StaticMesh::Load(const std::string& fileName)
                     if (outMaterial.intVector_[outIndices.intVector_[3*f]*3]==i)
                         {
                             // add face
-                            mFaces[i][current].a = outIndices.intVector_[3*f];
-                            mFaces[i][current].b = outIndices.intVector_[3*f+1];
-                            mFaces[i][current].c = outIndices.intVector_[3*f+2];
-                            current++;
+                            unsigned int* currFace = faces + (current * 3);
+
+                            currFace[0] = outIndices.intVector_[3*f];
+                            currFace[1] = outIndices.intVector_[3*f+1];
+                            currFace[2] = outIndices.intVector_[3*f+2];
+                            ++current;
+
                             mFaceToMaterial.push_back(i);
                         }
                 }
@@ -304,272 +336,3 @@ bool StaticMesh::Load(const std::string& fileName)
     CalcBoundingBox();
     return true;
 }
-
-/*
-  void StaticMesh::PrepareCollision()
-  {
-  unsigned int i;
-
-  IndexBuffer indexBuffer;
-  for(i=0; i<mFaces.size(); ++i)
-  {
-  indexBuffer.Cache(mFaceCount[i]*3, (unsigned int*)mFaces[i]);
-  }
-
-  mCollisionModel = new OPCODE_Model();
-
-  OPCODECREATE OPCC;
-
-  //surface data
-  OPCC.NbTris     = indexBuffer.GetNumIndex()/3;
-  mOCFaces = new IndexedTriangle[indexBuffer.GetNumIndex()/3];
-
-  for (int l = 0; l < indexBuffer.GetNumIndex(); l+=3)
-  {
-  mOCFaces[l/3].mVRef[0] = indexBuffer.GetIndex()[l];
-  mOCFaces[l/3].mVRef[1] = indexBuffer.GetIndex()[l+1];
-  mOCFaces[l/3].mVRef[2] = indexBuffer.GetIndex()[l+2];
-  }
-
-  OPCC.NbVerts= mVertexCount;
-  OPCC.Tris       = indexBuffer.GetIndex();
-  OPCC.Verts  = (IceMaths::Point*)mPos;
-  OPCC.Rules      = SPLIT_COMPLETE|SPLIT_SPLATTERPOINTS|SPLIT_GEOMCENTER;
-  OPCC.NoLeaf     = true;
-  OPCC.Quantized = true;
-  bool status = mCollisionModel->Build(OPCC);
-
-  // create our spherecollider
-  mSphereCollider = new SphereCollider();
-  // we want all contacts
-  mSphereCollider->SetFirstContact(false);
-  mSphereCollider->SetTemporalCoherence(false);
-  mSphereCollider->SetPointers(mOCFaces, (IceMaths::Point*)mPos);
-
-  // create our planescollider
-  mPlanesCollider = new PlanesCollider();
-  mPlanesCollider->SetFirstContact(false);
-  mPlanesCollider->SetTemporalCoherence(false);
-  mPlanesCollider->SetPointers(mOCFaces, (IceMaths::Point*)mPos);
-
-  // create our raycollider
-  mRayCollider = new RayCollider();
-  mRayCollider->SetFirstContact(false);
-  mRayCollider->SetTemporalCoherence(false);
-  mRayCollider->SetClosestHit(true);
-  mRayCollider->SetCulling(true);
-  mRayCollider->SetPointers(mOCFaces, (IceMaths::Point*)mPos);
-  }
-*/
-
-/*
-  void StaticMesh::ConvertMatrix(IceMaths::Matrix4x4& matrix) const
-  {
-  for (int i=0; i<4; ++i)
-  for (int j=0; j<4; ++j)
-  matrix.m[j][i] = GetWorldTransform()(i, j);
-  }
-*/
-
-/*
-  void StaticMesh::FindInfluencedTriangles(Opcode::SphereCache &cache, const Meshmerizer::Sphere &sphere, std::vector<unsigned int> &triangles) const
-  {
-  Matrix4x4 worldMat;
-  ConvertMatrix(worldMat);
-  mSphereCollider->Collide(cache, sphere, mCollisionModel, 0, &worldMat);
-  const int numTriangles  = mSphereCollider->GetNbTouchedPrimitives();
-  const udword* colTriangles      = mSphereCollider->GetTouchedPrimitives();
-
-  triangles.clear();
-  triangles.reserve(numTriangles);
-
-  for (int i=0; i<numTriangles; ++i)
-  {
-  triangles.push_back(colTriangles[i]);
-  }
-  }
-*/
-
-/*
-  void StaticMesh::FindInfluencedTriangles(Opcode::PlanesCache &cache, const IceMaths::OPCPlane *planes, int numPlanes, std::vector<unsigned int> &triangles) const
-  {
-  Matrix4x4 worldMat;
-  ConvertMatrix(worldMat);
-  mPlanesCollider->Collide(cache, planes, numPlanes, mCollisionModel, &worldMat);
-  const int numTriangles  = mPlanesCollider->GetNbTouchedPrimitives();
-  const udword* colTriangles      = mPlanesCollider->GetTouchedPrimitives();
-
-  triangles.clear();
-  triangles.reserve(numTriangles);
-  //printf("PlaneCollider: %d\n", numTriangles);
-  for (int i=0; i<numTriangles; ++i)
-  {
-  triangles.push_back(colTriangles[i]);
-  }
-  }
-*/
-
-/*
-  void StaticMesh::FillIndexBuffers(TIndexBuffers &iBuffers, const std::vector<unsigned int> &triangles) const
-  {
-  if (mFaces.size() > iBuffers.size())
-  {
-  iBuffers.resize(mFaces.size());
-  }
-  const int n = triangles.size();
-  for (int i=0; i<n; ++i)
-  {
-  iBuffers[mFaceToMaterial[triangles[i]]].Cache(mOCFaces[triangles[i]].mVRef[0]);
-  iBuffers[mFaceToMaterial[triangles[i]]].Cache(mOCFaces[triangles[i]].mVRef[1]);
-  iBuffers[mFaceToMaterial[triangles[i]]].Cache(mOCFaces[triangles[i]].mVRef[2]);
-  }
-  }
-*/
-
-/*
-  void StaticMesh::Render(const TIndexBuffers &iBuffers) const
-  {
-  glPushMatrix();
-  glMultMatrixf(GetWorldTransform().m);
-
-  glVertexPointer(3, GL_FLOAT, 0, mPos);
-  glEnableClientState ( GL_VERTEX_ARRAY );
-
-  glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-  glTexCoordPointer( 3, GL_FLOAT, 0, mTexCoords);
-  glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-  glClientActiveTextureARB ( GL_TEXTURE1_ARB );
-  glTexCoordPointer( 3, GL_FLOAT, 0, mBasisX);
-  glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-  glClientActiveTextureARB ( GL_TEXTURE2_ARB );
-  glTexCoordPointer( 3, GL_FLOAT, 0, mBasisY);
-  glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-  for (unsigned int i=0; i < mMaterials.size(); ++i)
-  {
-  if (iBuffers[i].GetNumIndex()>0)
-  {
-  mMaterials[i]->Bind();
-  glDrawElements(GL_TRIANGLES, iBuffers[i].GetNumIndex(), GL_UNSIGNED_INT, iBuffers[i].GetIndex());
-  }
-  }
-
-  glDisableClientState (GL_VERTEX_ARRAY);
-  glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-  glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-  glClientActiveTextureARB ( GL_TEXTURE1_ARB );
-  glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-  glClientActiveTextureARB ( GL_TEXTURE2_ARB );
-  glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-  glPopMatrix();
-  }
-*/
-
-void StaticMesh::ComputeBoundingBox()
-{
-}
-
-void StaticMesh::CalcBoundingBox()
-{
-    mLocalBoundingBox.Init();
-
-    const int n = mVertexCount;
-
-    for(int i = 0; i<n; ++i)
-        {
-            Float3 &v = mPos[i];
-            mLocalBoundingBox.Encapsulate(v.x, v.y, v.z);
-        }
-}
-
-/*
-  bool StaticMesh::Intersect(const salt::Vector3f& start, const salt::Vector3f& end, salt::Vector3f& ret) const
-  {
-  CollisionFaces cf;
-
-  mRayCollider->SetDestination(&cf);
-  OPCRay ray;
-  //start.Dump();
-  ray.mOrig.Set(start.x(), start.y(), start.z());
-  Vector3f dir = end - start;
-
-  mRayCollider->SetMaxDist(dir.Length());
-  dir.Normalize();
-  //dir.Dump();
-  ray.mDir.Set(dir.x(), dir.y(), dir.z());
-
-  Matrix4x4 worldMat;
-  ConvertMatrix(worldMat);
-
-  if (mRayCollider->Collide(ray, mCollisionModel, &worldMat, NULL))
-  {
-  if (cf.GetNbFaces())
-  {
-  //printf("%d %f\n", cf.GetNbFaces(), cf.GetFaces()[0].mDistance);
-  ret = start + cf.GetFaces()[0].mDistance*dir;
-  return true;
-  }
-  }
-
-  return false;
-  }
-*/
-
-void StaticMesh::RenderInternal()
-{
-    // comment this method, when reenabling fancy lighting
-
-    glVertexPointer(3, GL_FLOAT, 0, mPos);
-    glEnableClientState ( GL_VERTEX_ARRAY );
-
-    glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-    glTexCoordPointer( 3, GL_FLOAT, 0, mTexCoords);
-    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-    glClientActiveTextureARB ( GL_TEXTURE1_ARB );
-    glTexCoordPointer( 3, GL_FLOAT, 0, mBasisX);
-    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-    glClientActiveTextureARB ( GL_TEXTURE2_ARB );
-    glTexCoordPointer( 3, GL_FLOAT, 0, mBasisY);
-    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-    for (unsigned int i=0; i < mMaterials.size(); ++i)
-        {
-            mMaterials[i]->Bind();
-            glDrawElements(GL_TRIANGLES, mFaceCount[i]*3, GL_UNSIGNED_INT, mFaces[i]);
-        }
-
-    glDisableClientState (GL_VERTEX_ARRAY);
-    glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-    glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-    glClientActiveTextureARB ( GL_TEXTURE1_ARB );
-    glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-    glClientActiveTextureARB ( GL_TEXTURE2_ARB );
-    glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-}
-
-/*
-  void StaticMesh::RenderAmbientInternal()
-  {
-  glVertexPointer(3, GL_FLOAT, 0, mPos);
-  glEnableClientState ( GL_VERTEX_ARRAY );
-
-  glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-  glTexCoordPointer( 3, GL_FLOAT, 0, mTexCoords);
-  glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-
-  for (unsigned int i=0; i < mMaterials.size(); ++i)
-  {
-  mMaterials[i]->BindAmbient();
-  glDrawElements(GL_TRIANGLES, mFaceCount[i]*3, GL_UNSIGNED_INT, mFaces[i]);
-  }
-
-  glDisableClientState (GL_VERTEX_ARRAY);
-  glClientActiveTextureARB ( GL_TEXTURE0_ARB );
-  glDisableClientState ( GL_TEXTURE_COORD_ARRAY );
-  }
-*/
--- lib/kerosin/sceneserver/staticmesh.h
+++ lib/kerosin/sceneserver/staticmesh.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: staticmesh.h,v 1.6 2003/11/14 14:05:52 fruit Exp $
+   $Id: staticmesh.h,v 1.7 2004/04/18 16:26:25 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,35 +22,10 @@
 #ifndef KEROSIN_STATICMESH_H
 #define KEROSIN_STATICMESH_H
 
-#include "../textureserver/textureserver.h"
+#include <kerosin/textureserver/textureserver.h>
 #include <oxygen/sceneserver/basenode.h>
 #include <oxygen/sceneserver/indexbuffer.h>
-
-// define the symbol BOOL to prevent opcode.h to typedef BOOL
-// #define BOOL BOOL
-// #include <opcode/Opcode.h>
-
-/* namespace Opcode */
-/* { */
-/* class OPCODE_Model; */
-/* class SphereCollider; */
-/* class PlanesCollider; */
-/* class RayCollider; */
-/* struct SphereCache; */
-/* struct PlanesCache; */
-/* } */
-
-namespace Meshmerizer
-{
-class IndexedTriangle;
-class Sphere;
-}
-
-namespace IceMaths
-{
-class OPCPlane;
-class Matrix4x4;
-}
+#include <boost/shared_array.hpp>
 
 namespace kerosin
 {
@@ -63,16 +38,7 @@ class StaticMesh : public oxygen::BaseNode
     // Types
     //
 public:
-    struct Float3
-    {
-        float x,y,z;
-    };
-
-    struct Face
-    {
-        unsigned int a, b, c;
-    };
-    typedef std::vector<oxygen::IndexBuffer>        TIndexBuffers;
+    typedef std::vector<oxygen::IndexBuffer> TIndexBuffers;
 
     //
     // Function
@@ -83,65 +49,31 @@ public:
 
     bool Load(const std::string& fileName);
 
-/*
-//! given a 3D sphere, this returns the potentially influenced triangles lying inside the spere
-void FindInfluencedTriangles(Opcode::SphereCache &cache, const Meshmerizer::Sphere &sphere, std::vector<unsigned int> &triangles) const;
-//! given a set of planes this returns the potentially influenced triangles lying in front of all planes
-void FindInfluencedTriangles(Opcode::PlanesCache &cache, const IceMaths::OPCPlane *planes, int numPlanes, std::vector<unsigned int> &triangles) const;
-
-
-void FillIndexBuffers(TIndexBuffers &iBuffers, const std::vector<unsigned int> &triangles) const;
-*/
-/*
-//! the render function, which is used by the dynamic point-based lights
-void Render(const TIndexBuffers &iBuffers) const;
-*/
-
     //! empty! this prevents the bounding box from being updated ... it cannot change!
-    virtual void    ComputeBoundingBox();
+    virtual void ComputeBoundingBox();
 
     //! calculates the local bounding box
-    void                    CalcBoundingBox();
+    void CalcBoundingBox();
 
-    //! given a ray, find closest intersection point
-    bool Intersect(const salt::Vector3f& start, const salt::Vector3f& end, salt::Vector3f& ret) const;
-private:
+protected:
     virtual void RenderInternal();
-    //   virtual void RenderAmbientInternal();
-
-/*
-//! initializes the collision system (Opcode)
-void PrepareCollision();
-
-//! converts internal matrix to an Opcode matrix
-void ConvertMatrix(IceMaths::Matrix4x4& matrix) const;
-*/
 
     //
     // Members
     //
 private:
-    int             mVertexCount;
-    Float3* mPos;
-    Float3* mTexCoords;
+    int mVertexCount;
+    boost::shared_array<float> mPos;
+    boost::shared_array<float> mTexCoords;
+
     // tangent space basis vectors
-    Float3* mBasisX;
-    Float3* mBasisY;
+    boost::shared_array<float> mBasisX;
+    boost::shared_array<float> mBasisY;
 
-    std::vector<boost::shared_ptr<Material> >       mMaterials;
-    std::vector<int>                                                        mFaceCount;
-    std::vector<Face*>                                                      mFaces;
-
-    // collision related
-    std::vector<unsigned int>               mFaceToMaterial;
-    Meshmerizer::IndexedTriangle    *mOCFaces;
-
-/*
-  Opcode::OPCODE_Model                    *mCollisionModel;
-  Opcode::SphereCollider                  *mSphereCollider;
-  Opcode::PlanesCollider                  *mPlanesCollider;
-  Opcode::RayCollider                     *mRayCollider;
-*/
+    std::vector<boost::shared_ptr<Material> >  mMaterials;
+    std::vector<int> mFaceCount;
+    std::vector<boost::shared_array<unsigned int> > mFaces;
+    std::vector<unsigned int> mFaceToMaterial;
 };
 
 DECLARE_CLASS(StaticMesh);
