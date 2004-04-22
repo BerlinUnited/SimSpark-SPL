8c6b820b34c082c9c194c5c9a474ce2046930978
Oliver Obst <oliverobst@gmail.com>
2004-04-22 11:31:08 +0000

    - added a hook that will be called once all the agents connected to a simulation

--- lib/oxygen/controlaspect/controlaspect.h
+++ lib/oxygen/controlaspect/controlaspect.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: controlaspect.h,v 1.4 2004/02/12 14:07:22 fruit Exp $
+   $Id: controlaspect.h,v 1.5 2004/04/22 11:31:08 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -41,6 +41,9 @@ public:
         seconds
      */
     virtual void Update(float deltaTime) = 0;
+
+    /** called after all the agents conntected to a simulation */
+    virtual void AgentsConnectedHook() {}
 
     /** queries the SceneServer for the currently active scene node */
     boost::shared_ptr<Scene> GetActiveScene();
