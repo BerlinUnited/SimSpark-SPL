6dfbb0165f6548d8ceb55275fa149fb4dc47ca2f
Oliver Obst <oliverobst@gmail.com>
2004-04-23 13:24:22 +0000

    - removed StartUpHook()

--- lib/oxygen/gamecontrolserver/gamecontrolserver.cpp
+++ lib/oxygen/gamecontrolserver/gamecontrolserver.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2004 RoboCup Soccer Server 3D Maintenance Group
-   $Id: gamecontrolserver.cpp,v 1.10 2004/04/22 14:20:11 fruit Exp $
+   $Id: gamecontrolserver.cpp,v 1.11 2004/04/23 13:24:22 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -308,23 +308,3 @@ GameControlServer::Update(float deltaTime)
     }
 }
 
-void
-GameControlServer::StartUpHook()
-{
-    // build list of ControlAspects, NOT searching recursively
-    TLeafList control;
-    GetChildrenSupportingClass("ControlAspect",control,false);
-
-    // update all ControlAspects found
-    for (
-        TLeafList::iterator iter = control.begin();
-        iter != control.end();
-        ++iter
-        )
-    {
-        shared_ptr<ControlAspect> aspect =
-            shared_static_cast<ControlAspect>(*iter);
-
-        aspect->StartUpHook();
-    }
-}
