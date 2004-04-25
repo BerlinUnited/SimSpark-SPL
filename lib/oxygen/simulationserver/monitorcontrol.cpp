93554bc21d5d14be513e90cd8ab99e5fc2e04920
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-25 16:48:03 +0000

    - added class MonitorControl that manages the communication with monitors in cooperation with the MonitorServer.

---
+++ lib/oxygen/simulationserver/monitorcontrol.cpp
@@ -0,0 +1,127 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: monitorcontrol.cpp,v 1.1 2004/04/25 16:48:03 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "monitorcontrol.h"
+#include "simulationserver.h"
+#include "netmessage.h"
+#include <zeitgeist/logserver/logserver.h>
+#include <oxygen/monitorserver/monitorserver.h>
+
+using namespace oxygen;
+using namespace zeitgeist;
+using namespace boost;
+using namespace std;
+
+MonitorControl::MonitorControl() : NetControl()
+{
+    mLocalAddr.setPort(3200);
+}
+
+MonitorControl::~MonitorControl()
+{
+}
+
+void MonitorControl::OnLink()
+{
+    NetControl::OnLink();
+    shared_ptr<SimulationServer> sim = GetSimulationServer();
+    if (sim.get() == 0)
+        {
+            GetLog()->Error()
+                << "(MonitorControl) ERROR: SimulationServer not found\n";
+            return;
+        }
+
+    mMonitorServer = sim->GetMonitorServer();
+}
+
+void MonitorControl::OnUnlink()
+{
+    NetControl::OnUnlink();
+    mMonitorServer.reset();
+}
+
+void MonitorControl::ClientConnect(shared_ptr<Client> client)
+{
+    if (mMonitorServer.get() == 0)
+        {
+            return;
+        }
+
+    SendMessage(client->addr,mMonitorServer->GetMonitorHeaderInfo());
+}
+
+void MonitorControl::EndCycle()
+{
+    if (
+        (mMonitorServer.get() == 0) ||
+        (mNetMessage.get() == 0)
+        )
+        {
+            return;
+        }
+
+    // send updates to all connected monitors
+    string info = mMonitorServer->GetMonitorInfo();
+    mNetMessage->PrepareToSend(info);
+
+    for (
+         TAddrMap::iterator iter = mClients.begin();
+         iter != mClients.end();
+         ++iter
+         )
+        {
+            SendMessage((*iter).second,info);
+        }
+}
+
+void MonitorControl::StartCycle()
+{
+    if (
+        (mMonitorServer.get() == 0) ||
+        (mNetMessage.get() == 0)
+        )
+        {
+            return;
+        }
+
+    // pass all received messages to the MonitorServer
+    for (
+         TBufferMap::iterator iter = mBuffers.begin();
+         iter != mBuffers.end();
+         ++iter
+         )
+        {
+            shared_ptr<NetBuffer>& netBuff = (*iter).second;
+            if (
+                (netBuff.get() == 0) ||
+                (netBuff->IsEmpty())
+                )
+                {
+                    continue;
+                }
+
+            string message;
+            while (mNetMessage->Extract(netBuff,message))
+                {
+                    mMonitorServer->ParseMonitorMessage(message);
+                }
+        }
+}
+
---
+++ lib/oxygen/simulationserver/monitorcontrol.h
@@ -0,0 +1,63 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: monitorcontrol.h,v 1.1 2004/04/25 16:48:03 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef OXYGEN_MONITORCONTROL_H
+#define OXYGEN_MONITORCONTROL_H
+
+#include "netcontrol.h"
+
+namespace oxygen
+{
+class MonitorServer;
+
+/** \class MonitorConrol is a NetControl node that manages the
+    communication with monitors in cooperation with the
+    MonitorServer.
+*/
+class MonitorControl : public NetControl
+{
+public:
+    MonitorControl();
+    virtual ~MonitorControl();
+
+    /** forwards all pending messages from all connected monitors to
+        the MonitorServer */
+    virtual void StartCycle();
+
+    /** updates all connected monitors at the end of each simulation
+        cycle */
+    virtual void EndCycle();
+
+    /** called when a new client connects */
+    virtual void ClientConnect(boost::shared_ptr<Client> client);
+
+protected:
+    virtual void OnLink();
+    virtual void OnUnlink();
+
+protected:
+    /** cached reference to the MonitorServer */
+    boost::shared_ptr<MonitorServer> mMonitorServer;
+};
+
+DECLARE_CLASS(MonitorControl);
+
+} // namespace oxygen
+
+#endif // OXYGEN_MONITORCONTROL_H
