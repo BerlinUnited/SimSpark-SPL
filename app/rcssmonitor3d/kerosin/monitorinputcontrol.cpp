286e3f58bab761b6592880b421e1c54e6d45011d
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-25 17:14:09 +0000

    - added class MonitorInputControl, an InputControl node that handles custom soccer monitor input events, like the KickOff command

---
+++ app/rcssmonitor3d/kerosin/monitorinputcontrol.cpp
@@ -0,0 +1,59 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: monitorinputcontrol.cpp,v 1.1 2004/04/25 17:14:09 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "monitorinputcontrol.h"
+
+using namespace zeitgeist;
+using namespace boost;
+using namespace std;
+
+void MonitorInputControl::OnLink()
+{
+    InputControl::OnLink();
+
+    mCommServer = shared_dynamic_cast<CommServer>
+        (GetCore()->Get("/sys/server/comm"));
+
+    if (mCommServer.get() == 0)
+        {
+            GetLog()->Error()
+                << "(MonitorInputControl) ERROR: CommServer not found\n";
+        }
+
+    // publish user defined command constants
+    shared_ptr<ScriptServer> script = GetCore()->GetScriptServer();
+    script->CreateVariable("Command.KickOff", CmdKickOff);
+}
+
+void MonitorInputControl::ProcessInput(kerosin::InputServer::Input& input)
+{
+    if (mCommServer.get() == 0)
+        {
+            return;
+        }
+
+    switch (input.id)
+        {
+        case CmdKickOff:
+            // kick off
+            GetLog()->Normal() <<"--- Kick Off\n";
+            mCommServer->SendKickOffCmd();
+            break;
+        }
+}
---
+++ app/rcssmonitor3d/kerosin/monitorinputcontrol.h
@@ -0,0 +1,44 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: monitorinputcontrol.h,v 1.1 2004/04/25 17:14:09 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include <kerosin/inputserver/inputcontrol.h>
+#include <commserver.h>
+
+/** \class MonitorInputControl is an InputControl node that handles
+    custom soccer monitor input events, like the KickOff command
+ */
+class MonitorInputControl : public kerosin::InputControl
+{
+public:
+    /** define user input constants */
+    static const int CmdKickOff = CmdUser+1;
+
+public:
+    /** handle user defined input constants */
+    virtual void ProcessInput(kerosin::InputServer::Input& input);
+
+protected:
+    virtual void OnLink();
+
+protected:
+    /** the cached CommServer instance */
+    boost::shared_ptr<CommServer> mCommServer;
+};
+
+DECLARE_CLASS(MonitorInputControl);
