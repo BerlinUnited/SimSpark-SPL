bcb2dad92caeb35bf69b18dbe5519b294e2543b2
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-25 16:43:51 +0000

    - added class SimControlNode that defines an interface for classes that are registered to the SimulationServer. The interface defines callbacks for the initial startup of the simulation, it's final shutdown and for different phases of each simulation cycle.

---
+++ lib/oxygen/simulationserver/simcontrolnode.cpp
@@ -0,0 +1,43 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: simcontrolnode.cpp,v 1.1 2004/04/25 16:43:51 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "simcontrolnode.h"
+#include "simulationserver.h"
+
+using namespace oxygen;
+using namespace zeitgeist;
+using namespace salt;
+using namespace std;
+using namespace boost;
+
+SimControlNode::SimControlNode() : Leaf()
+{
+}
+
+SimControlNode::~SimControlNode()
+{
+}
+
+shared_ptr<SimulationServer> SimControlNode::GetSimulationServer()
+{
+    return shared_static_cast<SimulationServer>
+        (make_shared(GetParent()));
+}
