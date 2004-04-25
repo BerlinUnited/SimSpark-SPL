b80a04bb7dd9079f0997153fb2fae4155cb64583
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-25 16:51:16 +0000

    - added class NetMessage that is responsible for the segmentation of raw received network data into meaningful messages, stripping any meta message information (e.g. msg length prefixes). It further is responsible to prepare a message to be sent over the network, i.e. to add any necessary meta information.

---
+++ lib/oxygen/simulationserver/netmessage.cpp
@@ -0,0 +1,72 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: netmessage.cpp,v 1.1 2004/04/25 16:51:16 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "netmessage.h"
+#include "netbuffer.h"
+#include <netinet/in.h>
+
+using namespace oxygen;
+using namespace zeitgeist;
+using namespace std;
+using namespace boost;
+
+NetMessage::NetMessage() : Leaf()
+{
+}
+
+NetMessage::~NetMessage()
+{
+}
+
+void NetMessage::PrepareToSend(std::string& msg)
+{
+    // prefix the message with it's payload length
+    unsigned int len = htonl(msg.size());
+    std::string prefix((const char*)&len,sizeof(len));
+    msg = prefix + msg;
+}
+
+bool NetMessage::Extract(shared_ptr<NetBuffer> buffer, std::string& msg)
+{
+    // a message is prefixed with it's payload length
+    const unsigned int preSz = sizeof(unsigned int);
+    string& data = buffer->GetData();
+
+    if (data.size() < preSz)
+        {
+            return false;
+        }
+
+    unsigned int msgLen = ntohl(*(unsigned int*)data.c_str());
+
+    if (data.size() < (msgLen + preSz))
+        {
+            // incomplete message
+            return false;
+        }
+
+    // copy to msg and cut from buffer
+    msg = string(data.c_str() + preSz, msgLen);
+    data.erase(0, preSz + msgLen);
+
+    // zero terminate received data
+    msg += '\0';
+
+    return true;
+}
---
+++ lib/oxygen/simulationserver/netmessage_c.cpp
@@ -0,0 +1,28 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2003 Koblenz University
+   $Id: netmessage_c.cpp,v 1.1 2004/04/25 16:51:16 rollmark Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "netmessage.h"
+
+using namespace oxygen;
+using namespace std;
+
+void CLASS(NetMessage)::DefineClass()
+{
+    DEFINE_BASECLASS(zeitgeist/Leaf);
+}
