e1485ffb71f2de1c3288799760f3b77bfbb6d6b3
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-25 17:06:26 +0000

    - setting up the SimulationServer - broke spark.rb into several utility functions that allow better customization of the spark lib (e.g. selective setup of rendering and server classes)

--- lib/spark/spark.rb
+++ lib/spark/spark.rb
@@ -3,66 +3,65 @@
 #
 
 #
-# import required plugins
-importBundle 'filesystemstd';
-
-#
-# scene and server path
-$scenePath = '/usr/scene/'
-$serverPath = '/sys/server/'
-
-#
-# setup the PhysicsServer
-new('oxygen/PhysicsServer', $serverPath+'physics')
-
-#
-# setup the SceneServer
-sceneServer = new('oxygen/SceneServer', $serverPath+'scene')
-sceneServer.createScene($scenePath)
-
-# use the ruby scene importer to import scenes
-importBundle 'rubysceneimporter'
-sceneServer.initSceneImporter("RubySceneImporter");
-
-#
-# setup the GeometryServer
-geometryServer = new('oxygen/GeometryServer', $serverPath+'geometry')
-importBundle 'voidmeshimporter'
-geometryServer.initMeshImporter("VoidMeshImporter");
-
-#
-# setup the kerosin render framework
-new('kerosin/OpenGLServer', $serverPath+'opengl');
-new('kerosin/MaterialServer', $serverPath+'material');
-new('kerosin/RenderServer', $serverPath+'render');
-new('kerosin/ImageServer', $serverPath+'image');
-new('kerosin/TextureServer', $serverPath+'texture');
-
-#
-# setup the InputServer
-
-# use the SDL input system and use a german keyboard layout
-importBundle 'inputsdl';
-inputServer = new('kerosin/InputServer', $serverPath+'input')
-inputServer.setScanCodeMapping('german.scan.rb');
-inputServer.init('InputSystemSDL')
-
-# add devices
-inputServer.createDevice('Timer')
-inputServer.createDevice('Keyboard')
-inputServer.createDevice('Mouse')
-
-# setup the FontServer
-new('kerosin/FontServer', $serverPath+'font');
-
-# create world and space aspects
-world = new('oxygen/World', $scenePath+'world')
-world.setGravity(0.0, 0.0, -9.81)
-new('oxygen/Space', $scenePath+'space')
-
-#
 # below is a set of utility functions for the user app
 #
+def sparkSetupServer
+  print "(spark.rb) sparkSetupServer\n"
+
+  # add the agent control node
+  simulationServer = get($serverPath+'simulation');
+  simulationServer.initControlNode('oxygen/AgentControl')  
+  simulationServer.initControlNode('oxygen/MonitorControl')
+end
+
+def sparkSetupRendering
+  print "(spark.rb) sparkSetupRendering\n"
+  #
+  # setup the GeometryServer
+  geometryServer = new('oxygen/GeometryServer', $serverPath+'geometry')
+  importBundle 'voidmeshimporter'
+  geometryServer.initMeshImporter("VoidMeshImporter");
+  
+  #
+  # setup the kerosin render framework
+  new('kerosin/OpenGLServer', $serverPath+'opengl');
+  new('kerosin/RenderServer', $serverPath+'render');
+  new('kerosin/ImageServer', $serverPath+'image');
+  new('kerosin/TextureServer', $serverPath+'texture');
+
+  #
+  # setup the InputServer
+
+  # use the SDL input system and use a german keyboard layout
+  importBundle 'inputsdl';
+  inputServer = new('kerosin/InputServer', $serverPath+'input')
+  inputServer.setScanCodeMapping('german.scan.rb');
+  inputServer.init('InputSystemSDL')
+
+  # add devices
+  inputServer.createDevice('Timer')
+  inputServer.createDevice('Keyboard')
+  inputServer.createDevice('Mouse')
+
+  # setup the FontServer
+  new('kerosin/FontServer', $serverPath+'font');
+
+  # 
+  # register render control node to the simulation server
+  simulationServer = get($serverPath+'simulation');
+  simulationServer.initControlNode('kerosin/RenderControl')
+end
+
+def sparkSetupInput()
+  print "(spark.rb) sparkSetupRendering\n"
+
+  # 
+  # register input control node to the simulation server
+  simulationServer = get($serverPath+'simulation');
+
+  # add the input control node
+  simulationServer.initControlNode('kerosin/InputControl')
+end
 
 # add a camera with an FPS Controller to the scene at path
 def sparkAddFPSCamera(
@@ -74,6 +73,8 @@ def sparkAddFPSCamera(
 		      addCollider = false,
 		      colliderRadius = 2.0
 		      )
+
+  print "(spark.rb) sparkAddFPSCamera\n"
 
   # add a camera. The camera movement is controlled using an
   # FPSController.
@@ -108,6 +109,58 @@ def sparkAddFPSCamera(
   end
 end
 
+#
+# setup spark
+#
+
+#
+# scene and server path
+$scenePath = '/usr/scene/'
+$serverPath = '/sys/server/'
+
+#
+# setup the PhysicsServer
+new('oxygen/PhysicsServer', $serverPath+'physics')
+
+#
+# setup the SceneServer
+sceneServer = new('oxygen/SceneServer', $serverPath+'scene')
+sceneServer.createScene($scenePath)
+
+#
+# setup the GeometryServer
+geometryServer = new('oxygen/GeometryServer', $serverPath+'geometry')
+importBundle 'voidmeshimporter'
+geometryServer.initMeshImporter("VoidMeshImporter");
+
+# use the ruby scene importer to import scenes
+importBundle 'rubysceneimporter'
+sceneServer.initSceneImporter("RubySceneImporter");
+
+# create world and space aspects
+world = new('oxygen/World', $scenePath+'world')
+world.setGravity(0.0, 0.0, -9.81)
+new('oxygen/Space', $scenePath+'space')
+
+#
+# setup the MaterialServer
+new('kerosin/MaterialServer', $serverPath+'material');
+
+#
+# setup the MonitorServer
+monitorServer = new('oxygen/MonitorServer', $serverPath+'monitor')
+
+#
+# setup the GameControlServer
+gameControlServer = new('oxygen/GameControlServer', $serverPath+'gamecontrol')
+
+# register the s-expression parser to the GameControlServer
+importBundle "sexpparser"
+gameControlServer.initParser('SexpParser')
+
+#
+# setup the SimulationServer
+simulationServer = new('oxygen/SimulationServer', $serverPath+'simulation')
 
 
 
