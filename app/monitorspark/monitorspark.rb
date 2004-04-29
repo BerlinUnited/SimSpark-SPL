c8c08c52267b11cf742110e5876e0df1e4a055cb
Markus Rollmann <rollmark@uni-koblenz.de>
2004-04-29 15:27:38 +0000

    - added app/MonitorSpark; a monitor intended to be used together with app/simspark

---
+++ app/monitorspark/monitorspark.rb
@@ -0,0 +1,46 @@
+#
+# simspark.rb
+#
+
+sparkSetupRendering()
+sparkSetupInput()
+sparkSetupMonitor()
+
+# let spark create a default camera
+sparkAddFPSCamera(
+		  $scenePath+'camera', 
+		  x = -5, 
+		  y = -40, 
+		  z = 2,
+		  maxSpeed = 15.0,
+		  accel = 400.0,
+		  drag = 4,
+		  addCollider = true
+		  )
+
+#
+# create materials 
+# material setup is not sent via the monitor protocol (yet)
+#
+material = new('kerosin/MaterialSolid', $serverPath+'material/matYellow');
+material.setDiffuse(1.0,1.0,0.0,1.0)
+
+material = new('kerosin/MaterialSolid', $serverPath+'material/matBlue');
+material.setDiffuse(0.0,0.0,1.0,1.0)
+
+material = new('kerosin/MaterialSolid', $serverPath+'material/matWhite');
+material.setDiffuse(1.0,1.0,1.0,1.0)
+
+material = new('kerosin/MaterialSolid', $serverPath+'material/matRed');
+material.setDiffuse(1.0,0.0,0.0,1.0)
+
+material = new('kerosin/Material2DTexture', $serverPath+'material/matRedGlow');
+material.setDiffuse(1.0,0.0,0.0,1.0)
+material.setEmission(0.5,0.0,0.0,1.0)
+
+material = new('kerosin/MaterialSolid', $serverPath+'material/matGreen');
+material.setDiffuse(0.1,0.5,0.1,1.0)
+
+material = new('kerosin/MaterialSolid', $serverPath+'material/matGrey');
+material.setDiffuse(0.1,0.1,0.1,1.0)
+
