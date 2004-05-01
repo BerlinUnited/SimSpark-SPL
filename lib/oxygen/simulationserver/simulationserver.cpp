57cb3c53d78953dae7894e7998b728a60d9353e2
Markus Rollmann <rollmark@uni-koblenz.de>
2004-05-01 14:20:33 +0000

    - added class NetClient, containing network client functions common to agents and monitor

--- lib/oxygen/simulationserver/simulationserver.cpp
+++ lib/oxygen/simulationserver/simulationserver.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: simulationserver.cpp,v 1.2 2004/04/28 14:27:13 rollmark Exp $
+   $Id: simulationserver.cpp,v 1.3 2004/05/01 14:20:33 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -91,6 +91,7 @@ void SimulationServer::OnLink()
             << "(SimulationServer) ERROR: SceneServer not found.\n";
     }
 }
+
 
 void SimulationServer::OnUnlink()
 {
