13deee0815fc6314b42d768ac440be48b8a3c76e
Oliver Obst <oliverobst@gmail.com>
2004-05-11 10:57:13 +0000

    - added line for using the lite-monitor as logplayer   (needs to be commented out; the CommServer has to be replaced by the LogfileServer)

--- app/rcssmonitor3d/lite/rcssmonitor3D-lite.rb
+++ app/rcssmonitor3d/lite/rcssmonitor3D-lite.rb
@@ -9,4 +9,5 @@ $serverPath = '/sys/server/'
 
 # setup the CommServer
 new('rcssmonitor3d/CommServer', $serverPath+'comm')
-
+## setup the LogfileServer instead of the CommServer for use as logplayer
+#new('rcssmonitor3d/LogfileServer', $serverPath+'comm')
