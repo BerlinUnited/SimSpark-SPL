7b9b0c891b27b3503da1d0de5ed0641645d12afc
Oliver Obst <oliverobst@gmail.com>
2004-05-14 16:00:46 +0000

    - added special case for Object::GetClass() error message   for the ClassClass object

--- lib/zeitgeist/object.cpp
+++ lib/zeitgeist/object.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: object.cpp,v 1.9 2004/04/29 12:28:24 rollmark Exp $
+   $Id: object.cpp,v 1.10 2004/05/14 16:00:46 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -54,21 +54,22 @@ bool Object::Construct(const boost::shared_ptr<Object>& self, const boost::share
 boost::shared_ptr<Class> Object::GetClass() const
 {
     if (mClass.get() == 0)
+    {
+        const Leaf* leaf = dynamic_cast<const Leaf*>(this);
+
+        if (leaf != 0)
         {
-          std::cout << "(Object) ERROR: failed to get Class object ";
-          const Leaf* leaf = dynamic_cast<const Leaf*>(this);
-
-          if (leaf != 0)
-              {
-                  std::cout << "for '" << leaf->GetName()
-                            << std::cout << "' installed at '"
-                            << leaf->GetFullPath()
-                            << "'";
-              }
-
-          std::cout << std::endl;
-
+            if (leaf->GetFullPath() != "ClassClass")
+            {
+                std::cerr << "(Object) ERROR: failed to get Class object "
+                          << "for '" << leaf->GetName()
+                          << "' installed at '"
+                          << leaf->GetFullPath() << "'\n";
+            }
+        } else {
+            std::cerr << "(Object) ERROR: failed to get Class object for unknown\n";
         }
+    }
     return mClass;
 }
 
