e2f0aa083412f84b43689c8ad154c2681bba12ff
Oliver Obst <oliverobst@gmail.com>
2004-05-14 16:54:31 +0000

    removed the CommServer setup (done in monitor.cpp now)

--- app/rcssmonitor3d/lite/rcssmonitor3D-lite.rb
+++ app/rcssmonitor3d/lite/rcssmonitor3D-lite.rb
@@ -5,9 +5,3 @@
 # import the sexpparser parser plugin
 importBundle "sexpparser"
 
-$serverPath = '/sys/server/'
-
-# setup the CommServer
-new('rcssmonitor3d/CommServer', $serverPath+'comm')
-## setup the LogfileServer instead of the CommServer for use as logplayer
-#new('rcssmonitor3d/LogfileServer', $serverPath+'comm')
