29407f825ad5b9879fe46853033eb82b217554bb
Oliver Obst <oliverobst@gmail.com>
2004-06-06 11:10:54 +0000

    - removed methods that are implemented as empty in the base class now

--- app/rcssmonitor3d/lib/logfileserver.h
+++ app/rcssmonitor3d/lib/logfileserver.h
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: logfileserver.h,v 1.1 2004/05/11 10:25:24 fruit Exp $
+   $Id: logfileserver.h,v 1.2 2004/06/06 11:10:54 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -39,12 +39,8 @@ public:
     virtual bool ReadMessage(std::string& msg);
     virtual boost::shared_ptr<oxygen::PredicateList> GetPredicates() const;
 
-    virtual void SendKickOffCmd();
-    virtual void SendTrainerCmd(const std::string& cmd);
     virtual void SendPauseCmd();
     virtual void SendRunCmd();
-    virtual void SendDisconnectCmd();
-    virtual void SendToWorldModel(const std::string& msg);
 
 protected:
     void Parse(const std::string& message);
