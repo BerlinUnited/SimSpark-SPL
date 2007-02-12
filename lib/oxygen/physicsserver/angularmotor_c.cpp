96e68c855bd4fe63619706d701428684aed1c836
Markus Rollmann <rollmark@uni-koblenz.de>
2007-02-12 19:22:54 +0000

    - added missing cast

--- lib/oxygen/physicsserver/angularmotor_c.cpp
+++ lib/oxygen/physicsserver/angularmotor_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: angularmotor_c.cpp,v 1.1 2004/04/20 14:11:30 rollmark Exp $
+   $Id: angularmotor_c.cpp,v 1.2 2007/02/12 19:27:01 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -115,7 +115,7 @@ FUNCTION(AngularMotor,setAxisAngle)
             return false;
         }
 
-    obj->SetAxisAngle(static_cast<Joint::EAxisIndex>(inAxisIdx),inDegAngle);
+    obj->SetAxisAngle(static_cast<Joint::EAxisIndex>(inAxisIdx),static_cast<float>(inDegAngle));
     return true;
 }
 
