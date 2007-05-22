6733dbb5a07c6a94c830876d7d87ab6f2420a568
Oliver Obst <oliverobst@gmail.com>
2007-05-22 01:53:09 +0000

    config.h -> sparkconfig.h

--- utility/rcssnet/exception.cpp
+++ utility/rcssnet/exception.cpp
@@ -19,7 +19,7 @@
  ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include <sparkconfig.h>
 #endif
 
 #include "exception.hpp"
