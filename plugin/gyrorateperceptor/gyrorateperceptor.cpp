a4274d1a383e87b32e797364ce0cbe8796fd161d
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2007-05-29 21:11:54 +0000

    Added 'rt' in the rate output

--- plugin/gyrorateperceptor/gyrorateperceptor.cpp
+++ plugin/gyrorateperceptor/gyrorateperceptor.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: gyrorateperceptor.cpp,v 1.3 2007/05/26 20:48:19 hedayat Exp $
+   $Id: gyrorateperceptor.cpp,v 1.4 2007/05/29 21:11:54 hedayat Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -59,7 +59,7 @@ GyroRatePerceptor::Percept(boost::shared_ptr<PredicateList> predList)
     Vector3f rate = body->GetAngularVelocity();
 
     ParameterList & ratesElement = predicate.parameter.AddList();    
-//    ratesElement.AddValue(std::string("rt"));
+    ratesElement.AddValue(std::string("rt"));
     ratesElement.AddValue(gRadToDeg(rate.x()));
     ratesElement.AddValue(gRadToDeg(rate.y()));
     ratesElement.AddValue(gRadToDeg(rate.z()));
