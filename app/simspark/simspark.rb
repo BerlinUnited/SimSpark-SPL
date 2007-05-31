8b0b55e6dbc216377119d108053f102ac8794e35
Joschka Boedecker <joschka.boedecker@gmail.com>
2007-05-31 08:18:51 +0000

    - load all the materials from materials.rb now

--- app/simspark/simspark.rb
+++ app/simspark/simspark.rb
@@ -30,51 +30,8 @@ sparkAddFPSCamera(
 # setup default input bindings
 run "bindings.rb"
 
-# create custom materials
-material = new('kerosin/MaterialSolid', $serverPath+'material/matYellow');
-material.setDiffuse(1.0,1.0,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matOrange');
-material.setDiffuse(1.0,0.3,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matBlue');
-material.setDiffuse(0.0,0.0,1.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matWhite');
-material.setDiffuse(1.0,1.0,1.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matRed');
-material.setDiffuse(1.0,0.0,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matRedGlow');
-material.setDiffuse(1.0,0.0,0.0,1.0)
-material.setEmission(0.5,0.0,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matGreen');
-material.setDiffuse(0.1,0.6,0.1,1.0)
-material.setAmbient(0.1,0.3,0.1,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matGrey');
-material.setDiffuse(0.2,0.2,0.2,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matDarkGrey');
-material.setDiffuse(0.05,0.05,0.05,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matLightBlue');
-material.setDiffuse(0.0,0.75,1.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matMagenta');
-material.setDiffuse(1.0,0.0,0.75,1.0)
-
-material = new('kerosin/Material2DTexture', $serverPath+'material/matBall');
-material.setDiffuse(1.0,1.0,1.0,1.0)
-material.setAmbient(0.5,0.5,0.5,1.0)
-material.setDiffuseTexture('./soccerball.tga')
-
-material = new('kerosin/Material2DTexture', $serverPath+'material/matGrass');
-material.setDiffuse(1.0,1.0,1.0,1.0)
-material.setAmbient(0.5,0.5,0.5,1.0)
-material.setDiffuseTexture('./grass.tga')
+# setup materials
+run "materials.rb"
 
 #
 # uncomment for logging setup (see spark.rb for reference)
