0d7a6fdfe0a2803de79b8b64628b94c1974a3755
Joschka Boedecker <joschka.boedecker@gmail.com>
2007-06-02 10:20:38 +0000

    - using the new methods of the BeamAction class - rotate all of the agents bodies according to   the given angle in the x-y-plane

--- plugin/soccer/beameffector/beameffector.cpp
+++ plugin/soccer/beameffector/beameffector.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: beameffector.cpp,v 1.11 2007/05/30 18:40:34 jboedeck Exp $
+   $Id: beameffector.cpp,v 1.12 2007/06/02 10:20:38 jboedeck Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -71,13 +71,16 @@ BeamEffector::Realize(boost::shared_ptr<ActionObject> action)
     // the beam effector only has an effect in PM_BeforeKickOff
     if (mGameState->GetPlayMode() == PM_BeforeKickOff)
     {
-        Vector3f pos = beamAction->GetPosition();
+        Vector3f pos;
+        pos[0] = beamAction->GetPosX();
+        pos[1] = beamAction->GetPosY();
+
+        float angle = beamAction->GetXYAngle();
 
         // reject nan or infinite numbers in the beam position
         if (
             (! isfinite(pos[0])) ||
-            (! isfinite(pos[1])) ||
-            (! isfinite(pos[2]))
+            (! isfinite(pos[1]))
             )
             {
                 return false;
@@ -93,6 +96,7 @@ BeamEffector::Realize(boost::shared_ptr<ActionObject> action)
         pos[1] = std::max<float>(minY,pos[1]);
         pos[1] = std::min<float>(maxY,pos[1]);
 
+        // fix z coordinate
         pos[2] = mAgentRadius;
 
         // swap x and y coordinates accordingly for the current
@@ -105,19 +109,16 @@ BeamEffector::Realize(boost::shared_ptr<ActionObject> action)
                 );
 
 	Vector3f bodyPos = mBody->GetPosition();
-        //mBody->SetPosition(pos);
-        //mBody->SetVelocity(Vector3f(0,0,0));
-        //mBody->SetAngularVelocity(Vector3f(0,0,0));
 
 	shared_ptr<Transform> parent = shared_dynamic_cast<Transform>
-	    (GetParent().lock());
-	parent = shared_dynamic_cast<Transform>
-	    (parent->GetParent().lock());
-
+	    (((GetParent().lock())->GetParent().lock())->GetParent().lock());
+	        
 	if (parent.get()==0)
-	{
-    	    return false;
-	}
+            {
+                GetLog()->Error() << "(BeamEffector) ERROR: can't get "
+                                  << "parent node.\n";
+                return false;
+            }
 
         Leaf::TLeafList leafList;
 
@@ -132,6 +133,9 @@ BeamEffector::Realize(boost::shared_ptr<ActionObject> action)
 
             return false;
         }
+
+        Matrix mat;
+        mat.RotationZ(gDegToRad(angle));        
 
         Leaf::TLeafList::iterator iter = leafList.begin();
        
@@ -149,6 +153,15 @@ BeamEffector::Realize(boost::shared_ptr<ActionObject> action)
     	    childBody->SetPosition(pos + (childPos-bodyPos));
     	    childBody->SetVelocity(Vector3f(0,0,0));
     	    childBody->SetAngularVelocity(Vector3f(0,0,0));
+
+            // EXPERIMENTAL
+            // We are setting an absolute rotation here! This will reset
+            // any orientation to the current rotation matrix which only has
+            // entries for a rotation around Z, which means that any other
+            // rotation will be lost. I assume this is okay, since we probably
+            // don't care about any current pose of the robot if we use beam...
+            childBody->SetRotation(mat);
+            
     	}
     }
     
@@ -165,15 +178,33 @@ BeamEffector::GetActionObject(const Predicate& predicate)
       return shared_ptr<ActionObject>();
     }
 
-  Vector3f pos;
-  if (! predicate.GetValue(predicate.begin(), pos))
+  Predicate::Iterator iter = predicate.begin();
+
+  float posX;
+  if (! predicate.AdvanceValue(iter, posX))
   {
       GetLog()->Error()
-          << "ERROR: (BeamEffector) Vector3f parameter expected\n";
+          << "ERROR: (BeamEffector) float expected for parameter1\n";
       return shared_ptr<ActionObject>(new ActionObject(GetPredicate()));
   }
 
-  return shared_ptr<ActionObject>(new BeamAction(GetPredicate(),pos));
+  float posY;
+  if (! predicate.AdvanceValue(iter, posY))
+  {
+      GetLog()->Error()
+          << "ERROR: (BeamEffector) float expected for parameter2\n";
+      return shared_ptr<ActionObject>(new ActionObject(GetPredicate()));
+  }
+
+  float angle;
+  if (! predicate.AdvanceValue(iter, angle))
+  {
+      GetLog()->Error()
+          << "ERROR: (BeamEffector) float expected for parameter3\n";
+      return shared_ptr<ActionObject>(new ActionObject(GetPredicate()));
+  }
+
+  return shared_ptr<ActionObject>(new BeamAction(GetPredicate(), posX, posY, angle));
 }
 
 void
