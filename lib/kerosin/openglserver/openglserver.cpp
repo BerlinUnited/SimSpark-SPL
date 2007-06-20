bf45581007e63e37348caa790eb94b0b2bc927dc
Oliver Obst <oliverobst@gmail.com>
2007-06-20 00:31:09 +0000

    - changes from simspark CVS (OpenGLServer using a GL subsystem)

--- lib/kerosin/openglserver/openglserver.cpp
+++ lib/kerosin/openglserver/openglserver.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: openglserver.cpp,v 1.16 2007/05/29 09:45:38 jboedeck Exp $
+   $Id: openglserver.cpp,v 1.17 2007/06/20 00:31:09 fruit Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,14 +19,34 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-
 #include "openglserver.h"
-#include <SDL.h>
+#include "openglwrapper.h"
+#include "openglsystem.h"
 #include <zeitgeist/scriptserver/scriptserver.h>
 #include <zeitgeist/fileserver/fileserver.h>
 #include <zeitgeist/logserver/logserver.h>
+#include <sstream>
 
 using namespace std;
+
+#ifdef __APPLE__
+// code below from http://developer.apple.com/qa/qa2001/qa1188.html
+#import <mach-o/dyld.h>
+void *NSGLGetProcAddress(const char *name)
+{
+    NSSymbol symbol;
+    char *symbolName;
+    // Prepend a '_' for the Unix C symbol mangling convention
+    symbolName = (char*) malloc (strlen (name) + 2);
+    strcpy(symbolName + 1, name);
+    symbolName[0] = '_';
+    symbol = NULL;
+    if (NSIsSymbolNameDefined (symbolName))
+        symbol = NSLookupAndBindSymbol (symbolName);
+    free (symbolName);
+    return symbol ? NSAddressOfSymbol (symbol) : NULL;
+}
+#endif
 
 namespace kerosin
 {
@@ -54,11 +74,9 @@ OpenGLServer::OpenGLServer() : Leaf(),
 
 OpenGLServer::~OpenGLServer()
 {
-    SDL_Quit();
 }
 
-void 
-OpenGLServer::Quit()
+void OpenGLServer::Quit()
 {
     mWantsToQuit = true;
 }
@@ -72,231 +90,53 @@ OpenGLServer::WantsToQuit() const
 void 
 OpenGLServer::Update()
 {
-    // Our SDL event placeholder.
-    SDL_Event event;
-
-    // Grab all the events off the queue.
-    while( SDL_PollEvent( &event ) )
+    if (mGLSystem.get() == 0)
     {
+        return;
     }
+
+    mGLSystem->Update();
 }
 
 void 
 OpenGLServer::SwapBuffers() const
 {
-    SDL_GL_SwapBuffers();
-}
-
-unsigned int 
-OpenGLServer::LoadARBProgram(GLenum /*target*/, const char* /*fileName*/)
-{
-#if 0
-    // only try to load stuff if the extension is supported
-    if (!mExtensionReg->Has_GL_ARB_vertex_program())
+    if (mGLSystem.get() == 0)
     {
-        return 0;
+        return;
     }
 
-    // before actually loading, try the cache
-    MapHolder::TProgramCache::iterator entry = mHolder->mPrograms.find(fileName);
-
-    if (entry != mHolder->mPrograms.end())
-    {
-        // we already have a match
-        return (*entry).second;
-    }
-
-    unsigned int id = 0;
-
-    // open file
-    shared_ptr<FileServer> fileServer = shared_static_cast<FileServer>(GetCore()->Get("/sys/server/file"));
-    salt::RFile *file = fileServer->Open(fileName);
-
-    if (!file) return 0;
-
-    unsigned char *buffer = new unsigned char[file->Size()+1];
-    file->Read(buffer, file->Size());
-
-    glGenProgramsARB(1, &id);
-    glBindProgramARB(target, id);
-
-    // try to load the actual program
-    glProgramStringARB(target, GL_PROGRAM_FORMAT_ASCII_ARB, file->Size(), buffer);
-
-    // free memory
-    delete file;
-    delete []buffer;
-
-    const unsigned char* error = glGetString(GL_PROGRAM_ERROR_STRING_ARB);
-
-    // if an error occured, display error message
-    if (error[0] != 0)
-    {
-        int i;
-        glDeleteProgramsARB(1, &id);
-        glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &i);
-        GetCore()->GetLogServer()->Error() << "ERROR: Loading ARB program (Pos: " << i << ")..." << endl;
-        GetCore()->GetLogServer()->Error() << "  => " << error << endl;
-        return 0;
-    }
-
-    // enter program into the cache
-    mHolder->mPrograms[fileName] = id;
-
-    return id;
-#else
-    return 0;
-#endif
+    mGLSystem->SwapBuffers();
 }
 
-unsigned int 
-OpenGLServer::LoadARBVertexProgram(const char* fileName)
-{
-#if 0
-    // only try to load stuff if the extension is supported
-    if (!mExtensionReg->Has_GL_ARB_vertex_program())
-        {
-            return 0;
-        }
-#endif
-    return LoadARBProgram(GL_VERTEX_PROGRAM_ARB, fileName);
-}
-
-unsigned int 
-OpenGLServer::LoadARBFragmentProgram(const char* /*fileName*/)
-{
-    // only try to load stuff if the extension is supported
-    //      if (!mExtensionReg->Has_GL_ARB_fragment_program())
-    //      {
-    //              return 0;
-    //      }
-
-    //      return LoadARBProgram(GL_FRAGMENT_PROGRAM_ARB, fileName);
-    return 0;
-}
-
-/*!
-  Set up the OpenGL viewport, initialize extension registry
-*/
 bool 
-OpenGLServer::ConstructInternal()
+OpenGLServer::Init(const string& openGLSysName)
 {
-    int result = SDL_Init(SDL_INIT_VIDEO);
+    GetLog()->Normal() << "(OpenGLServer) Init " << openGLSysName << "\n";
+    mGLSystem.reset();
 
-    if ( result < 0 )
+    if (! openGLSysName.empty())
     {
-        GetLog()->Error() << "ERROR: (OpenGLServer) Could not init SDL."
-                          << "SDL_Init returned error "
-                          << result << "\n";
-        return false;
+        // create the OpenGLSystem
+        mGLSystem = shared_dynamic_cast<OpenGLSystem>
+            (GetCore()->New(openGLSysName));
+
+        if (mGLSystem.get() == 0)
+        {
+            // could not create OpenGLSystem
+            GetLog()->Error() << "(OpenGLServer) ERROR: unable to create "
+                              << openGLSysName << "\n";
+            return false;
+        }
+
+        if (mGLSystem->Init() == false)
+        {
+            GetLog()->Error() << "(InputServer) ERROR: unable to initialize "
+                              << openGLSysName << "\n";
+            return false;
+        }
     }
-
-    const SDL_VideoInfo* info = SDL_GetVideoInfo();
-
-    int redBits;
-    int greenBits;
-    int blueBits;
-    int alphaBits;
-    int depthBits;
-    int stencilBits;
-    bool doubleBuffer;
-    bool fullScreen;
-    int xRes, yRes;
-    string title;
-
-    bool getConfig =
-        (
-         GetScript()->GetVariable("Viewport.RedBits", redBits) &&
-         GetScript()->GetVariable("Viewport.GreenBits", greenBits) &&
-         GetScript()->GetVariable("Viewport.BlueBits", blueBits) &&
-         GetScript()->GetVariable("Viewport.AlphaBits", alphaBits) &&
-         GetScript()->GetVariable("Viewport.DepthBits", depthBits) &&
-         GetScript()->GetVariable("Viewport.StencilBits", stencilBits) &&
-         GetScript()->GetVariable("Viewport.DoubleBuffer", doubleBuffer) &&
-         GetScript()->GetVariable("Viewport.FullScreen", fullScreen) &&
-         GetScript()->GetVariable("Viewport.XRes", xRes) &&
-         GetScript()->GetVariable("Viewport.YRes", yRes) &&
-         GetScript()->GetVariable("Application.Title", title)
-         );
-
-    if (! getConfig)
-    {
-        GetLog()->Error() << "(OpenGLServer) error reading config from ScriptServer\n";
-        return false;
-    }
-
-    GetLog()->Normal() << "(OpenGLServer) bits per channel (RGB): "
-                       << redBits << " "
-                       << greenBits << " "
-                       << blueBits << "\n";
-
-    GetLog()->Normal() << "(OpenGLServer)"
-                       << " depth bits= " << depthBits
-                       << " alpha depth= " << alphaBits
-                       << " stencil depth= " << stencilBits
-                       << "\n";
-
-    GetLog()->Normal() << "(OpenGLServer)"
-                       << " doubleBuffer= " << doubleBuffer
-                       << " fullScreen= " << fullScreen
-                       << "\n";
-
-    GetLog()->Normal() << "(OpenGLServer)"
-                       << " xRes = " << xRes
-                       << " yRes = " << yRes
-                       << "\n";
-
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, redBits);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, greenBits);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, blueBits);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, alphaBits);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, depthBits);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, stencilBits);
-    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, doubleBuffer ? 1:0);
-
-    int flags = SDL_OPENGL;
-    if (fullScreen)
-    {
-        flags |= SDL_FULLSCREEN;
-    }
-
-    SDL_Surface *screen = SDL_SetVideoMode
-        (xRes, yRes, info->vfmt->BitsPerPixel, flags);
-
-    if (screen == 0)
-    {
-        GetLog()->Error()
-            << "ERROR: (OpenGLServer) SDL_SetVideoMode() failed\n";
-        return false;
-    }
-
-    SDL_WarpMouse(xRes/2,yRes/2);
-    SDL_WM_SetCaption(title.c_str(), NULL);
-
-    //    mExtensionReg->Init();
-
-    // check if fancy lighting is supported
-    mSupportsFancyLighting = true;
-
-    //      if (!mExtensionReg->Has_GL_ARB_vertex_program() || !mExtensionReg->Has_GL_ARB_fragment_program())
-    //      {
-    // GetLog()->Normal() << "WARNING: GL_ARB_vertex_program not supported. "
-    //                   << "disabling fancy lighting\n" << endl;
     mSupportsFancyLighting = false;
-    //      }
-
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    GetLog()->Normal() << "(OpenGLServer) Initialized OpenGL Window\n";
-
-    const unsigned char* glRenderer = glGetString(GL_RENDERER);
-    const unsigned char* glVersion = glGetString(GL_VERSION);
-    const unsigned char* glExtensions = glGetString(GL_EXTENSIONS);
-
-    GetLog()->Normal() << "(OpenGLServer) GL_RENDERER:   " << glRenderer   << "\n";
-    GetLog()->Normal() << "(OpenGLServer) GL_VERSION:    " << glVersion    << "\n";
-    GetLog()->Normal() << "(OpenGLServer) GL_EXTENSIONS: " << glExtensions << "\n";
-    GetLog()->Normal() << "(OpenGLServer) GL_MAX_LIGHTS: " << GL_MAX_LIGHTS << "\n";
 
     // prepare the set of available lights
     for (int i=0;i<GL_MAX_LIGHTS;++i)
@@ -307,13 +147,12 @@ OpenGLServer::ConstructInternal()
     return true;
 }
 
-void 
-OpenGLServer::ToggleFancyLighting()
+/*!
+  Set up the OpenGL viewport, initialize extension registry
+*/
+bool OpenGLServer::ConstructInternal()
 {
-    if (mSupportsFancyLighting)
-        mSupportsFancyLighting = false;
-    else
-        mSupportsFancyLighting = true;
+    return true;
 }
 
 int
@@ -336,4 +175,49 @@ OpenGLServer::PutLight(int l)
 {
     glDisable(l);
     mAvailableLights.insert(l);
+}
+
+// Maybe see also http://rainwarrior.thenoos.net/dragon/sdl_glsl.html
+// and http://www.evl.uic.edu/arao/cs594/sdlglsl.html to rework the stuff here more platform independently
+// The code below uses stuff from the apple developer website (hidden in openglwrapper.h)
+void* OpenGLServer::GetExtension(const char* name)
+{
+#ifdef WIN32
+    return wglGetProcAddress(name);
+#elif defined(__APPLE__)
+    return NSGLGetProcAddress(name);
+#else
+    return glXGetProcAddressARB((unsigned char*)name);
+#endif
+}
+
+#define PROC_ADDRESS(_ptr, _function)\
+    static _ptr proc = (_ptr) GetExtension(#_function);
+
+void 
+OpenGLServer::glActiveTextureARB(unsigned int texture)
+{
+#ifdef __APPLE__
+    // this is ugly, but I don't know where are the function prototypes in mac os X?
+    return ::glActiveTextureARB(texture);
+#else
+    PROC_ADDRESS(PFNGLACTIVETEXTUREARBPROC, "glActiveTextureARB");
+
+    if (! proc)
+        {
+            return;
+        }
+
+    (proc)(static_cast<GLenum>(texture));
+#endif
+}
+
+long int
+OpenGLServer::GetWindowHandle() const
+{
+    if (mGLSystem.get() == 0)
+    {
+        return 0;
+    }
+    return mGLSystem->GetWindowHandle();
 }
