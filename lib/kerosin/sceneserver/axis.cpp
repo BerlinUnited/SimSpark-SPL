4bda19698891f31236e349f42971368e4f8d1db8
Oliver Obst <oliverobst@gmail.com>
2007-06-20 00:38:24 +0000

    include openglwrapper / remove include openglserver ws changes

--- lib/kerosin/sceneserver/axis.cpp
+++ lib/kerosin/sceneserver/axis.cpp
@@ -1,26 +1,26 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-this file is part of rcssserver3D
-Fri May 9 2003
-Copyright (C) 2002,2003 Koblenz University
-Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-$Id: axis.cpp,v 1.5 2007/02/12 19:44:34 rollmark Exp $
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: axis.cpp,v 1.6 2007/06/20 00:38:24 fruit Exp $
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #include "axis.h"
-#include "../openglserver/openglserver.h"
+#include "../openglserver/openglwrapper.h"
 #include <kerosin/openglserver/glbase.h>
 
 using namespace kerosin;
