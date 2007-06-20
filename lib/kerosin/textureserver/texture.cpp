30148f7f6502dbce2964e62e57b8e87dbbff0199
Oliver Obst <oliverobst@gmail.com>
2007-06-20 00:36:57 +0000

    removed include openglserver added copyright / ws changes

--- lib/kerosin/textureserver/texture.cpp
+++ lib/kerosin/textureserver/texture.cpp
@@ -1,5 +1,25 @@
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+   this file is part of rcssserver3D
+   Fri May 9 2003
+   Copyright (C) 2002,2003 Koblenz University
+   Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
+   $Id: texture.cpp,v 1.2 2007/06/20 00:36:57 fruit Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #include "texture.h"
-#include "../openglserver/openglserver.h"
 
 using namespace boost;
 using namespace kerosin;
@@ -25,13 +45,13 @@ void Texture::Reset()
 
 void Texture::Acquire()
 {
-	Reset();
-	glGenTextures(1, &mTexID);
+    Reset();
+    glGenTextures(1, &mTexID);
 }
 
 unsigned int Texture::GetWidth() const
 {
-	return mWidth;
+    return mWidth;
 }
 
 unsigned int Texture::GetHeight() const
