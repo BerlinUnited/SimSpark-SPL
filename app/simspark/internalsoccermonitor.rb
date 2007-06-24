91171eb6fedf12f365a4430a0dcfafe7b945ccc0
Joschka Boedecker <joschka.boedecker@gmail.com>
2007-06-24 17:28:06 +0000

    - setting up the internal soccer input plug-in

--- app/simspark/internalsoccermonitor.rb
+++ app/simspark/internalsoccermonitor.rb
@@ -2,6 +2,13 @@
 fontServer = get($serverPath+'font')
 fontServer.getFont('VeraMono.ttf')
 
-# register the soccer render plugin to draw the game state on the
+# register the internal soccer render plugin to draw the game state on the
 # screen
 sparkRegisterCustomRender 'InternalSoccerRender'
+
+# register the internal soccer input plugin to process soccer specific key
+# presses
+sparkRegisterCustomInput 'InternalSoccerInput'
+
+# bind keys to soccer commands
+run "internalsoccerbindings.rb"
