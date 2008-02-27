53c1f4c320045bbeb6e7c73575b0af930fe2ac9f
Markus Rollmann <rollmark@uni-koblenz.de>
2008-02-27 09:13:08 +0000

    - move common default materials to materials-default.rb and load them from   materials.rb and materials-textures.rb - update Makefile.am

--- app/simspark/materials.rb
+++ app/simspark/materials.rb
@@ -2,49 +2,7 @@
 # materials.rb -- create custom materials for simspark
 #
 
-#----------------------------------------------------------------
-# general colored materials
-#----------------------------------------------------------------
-
-# rainbow color
-material = new('kerosin/MaterialSolid', $serverPath+'material/matYellow');
-material.setDiffuse(1.0,1.0,0.0,1.0)
-material.setAmbient(0.2,0.2,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matOrange');
-material.setDiffuse(1.0,0.3,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matRed');
-material.setDiffuse(1.0,0.0,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matRedGlow');
-material.setDiffuse(1.0,0.0,0.0,1.0)
-material.setEmission(0.5,0.0,0.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matMagenta');
-material.setDiffuse(1.0,0.0,0.75,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matBlue');
-material.setDiffuse(0.0,0.0,1.0,1.0)
-material.setAmbient(0.0,0.0,0.2,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matLightBlue');
-material.setDiffuse(0.0,0.75,1.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matGreen');
-material.setDiffuse(0.1,0.6,0.1,1.0)
-material.setAmbient(0.1,0.3,0.1,1.0)
-
-# shades of grey
-material = new('kerosin/MaterialSolid', $serverPath+'material/matWhite');
-material.setDiffuse(1.0,1.0,1.0,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matGrey');
-material.setDiffuse(0.2,0.2,0.2,1.0)
-
-material = new('kerosin/MaterialSolid', $serverPath+'material/matDarkGrey');
-material.setDiffuse(0.05,0.05,0.05,1.0)
-
+run "materials-default.rb"
 
 #----------------------------------------------------------------
 # special purpose materials
