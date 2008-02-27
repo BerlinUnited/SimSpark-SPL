935d0cb9f94915ab6940b65cff41a38ee468fdd1
Markus Rollmann <rollmark@uni-koblenz.de>
2008-02-27 20:13:33 +0000

    - use FileServer::LocateResource to expand the given image name

--- lib/kerosin/imageserver/imageserver.cpp
+++ lib/kerosin/imageserver/imageserver.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2004 RoboCup Soccer Server 3D Maintenance Group
-   $Id: imageserver.cpp,v 1.11 2008/02/24 10:17:30 rollmark Exp $
+   $Id: imageserver.cpp,v 1.12 2008/02/27 20:13:33 rollmark Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -42,7 +42,13 @@ shared_ptr<FileServer> gFileServer;
 ILHANDLE
 ILAPIENTRY FSOpen(const ILstring inName)
 {
-    return (ILHANDLE)(gFileServer->Register(inName));
+    string fname;
+    if (! gFileServer->LocateResource(inName, fname))
+        {
+            return 0;
+        }
+
+    return (ILHANDLE)(gFileServer->Register(fname));
 }
 
 ILvoid
