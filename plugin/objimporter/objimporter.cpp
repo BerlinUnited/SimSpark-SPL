c339bde2a70c9225193cf435a4587c633bf4ce6f
Markus Rollmann <rollmark@uni-koblenz.de>
2008-05-12 08:58:40 +0000

    - sanity check for currentmat

--- plugin/objimporter/objimporter.cpp
+++ plugin/objimporter/objimporter.cpp
@@ -133,8 +133,8 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
                 {
                     str >> x >> y;
                     // ...
-                    //tex.push_back(Vector3f(0.5 + (0.5 - (x - 0.25)), 0.75 - (0.5 - y), 0.0)); 
-                    tex.push_back(Vector3f(1.0 - (1.0 - x), 1.0 - y, 0.0)); 
+                    //tex.push_back(Vector3f(0.5 + (0.5 - (x - 0.25)), 0.75 - (0.5 - y), 0.0));
+                    tex.push_back(Vector3f(1.0 - (1.0 - x), 1.0 - y, 0.0));
                 }
             else if (buffer == "vn") // vertex normal
                 {
@@ -170,25 +170,25 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
                     string del("/");
                     vector<string> exploded;
                     ExplodeString(f1, exploded, del);
-                                                
+
                     assert(exploded.size() == 3);
-                                                
+
                     idx1.v  = atoi(exploded[0].c_str());
                     idx1.vt = atoi(exploded[1].c_str());
                     idx1.vn = atoi(exploded[2].c_str());
 
                     exploded.clear();
                     ExplodeString(f2, exploded, del);
-                                        
+
                     assert(exploded.size() == 3);
-                                        
+
                     idx2.v  = atoi(exploded[0].c_str());
                     idx2.vt = atoi(exploded[1].c_str());
                     idx2.vn = atoi(exploded[2].c_str());
 
                     exploded.clear();
                     ExplodeString(f3, exploded, del);
-                                        
+
                     assert(exploded.size() == 3);
 
                     idx3.v  = atoi(exploded[0].c_str());
@@ -196,20 +196,20 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
                     idx3.vn = atoi(exploded[2].c_str());
 
                     exploded.clear();
-                                        
+
                     tempFace.vertex[0] = idx1;
                     tempFace.vertex[1] = idx2;
                     tempFace.vertex[2] = idx3;
-                                        
+
                     if (mat.size() != 0)
                         {
                             matFaceListMap[mat].push_back(tempFace);
-                                                
+
                             ++totalFaceCount;
                         }
                     else
                         {
-                            GetLog()->Error() 
+                            GetLog()->Error()
                                 << "(ObjImporter) WARNING: no material defined for use with "
                                 << "this face index, so I'll ignore it and go on.\n";
                         }
@@ -224,24 +224,24 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
     // shared arrays for the respective TriMesh methods. We need to use the
     // information in the face list to put the right information to the
     // correct indeces in the arrays.
-    // 
-    // We store 9 values (for 3 vertices) per face for positions, tex coords, 
+    //
+    // We store 9 values (for 3 vertices) per face for positions, tex coords,
     // and normals.
-    // 
+    //
     // We need to have separate index buffers for each material; tex coords and
     // normals don't care about the material, however.
 
     // for one face: 3 x <x,y,z>
     shared_array<float> posArray(new float[totalFaceCount * 9]);
-        
+
     // for one face: 3 x <u,v,0.0> (last value ignored)
     shared_array<float> texArray(new float[totalFaceCount * 9]);
 
     // for one face: 3 x <x,y,z>
     shared_array<float> normArray(new float[totalFaceCount * 9]);
-        
+
     shared_ptr<TriMesh> triMesh(new TriMesh());
-        
+
     unsigned int faceCount = 0;
 
     for (
@@ -273,15 +273,15 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
                             posArray[faceCount * 9 + i * 3]      = pos[j].x();
                             posArray[faceCount * 9 + i * 3 + 1]  = pos[j].y();
                             posArray[faceCount * 9 + i * 3 + 2]  = pos[j].z();
-                                
+
                             // cache this vertex index
                             indeces->Cache(faceCount * 3 + i);
-                                
+
                             if (tex.size() != 0)
                                 {
                                     // indices in the OBJ file start with 1 so subtract 1 here
                                     j = faceIter->vertex[i].vt - 1;
-                                        
+
                                     // for every face, we have 9 values (3 x <x,y,z>)
                                     texArray[faceCount * 9 + i * 3]      = tex[j].x();
                                     texArray[faceCount * 9 + i * 3 + 1]  = tex[j].y();
@@ -290,7 +290,7 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
 
                             // indices in the OBJ file start with 1 so subtract 1 here
                             j = faceIter->vertex[i].vn - 1;
-                                
+
                             // for every face, we have 9 values (3 x <x,y,z>)
                             normArray[faceCount * 9 + i * 3]     = norm[j].x();
                             normArray[faceCount * 9 + i * 3 + 1] = norm[j].y();
@@ -306,12 +306,12 @@ shared_ptr<TriMesh> ObjImporter::ImportMesh
         }
 
     triMesh->SetPos(posArray, faceCount * 3);
-        
+
     if (tex.size() != 0)
         {
             triMesh->SetTexCoords(texArray);
         }
-        
+
     triMesh->SetNormals(normArray);
 
     GetLog()->Normal() << "(ObjImporter) Done importing .obj file "
@@ -366,7 +366,7 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
 
     string matName, texName;
     float x1,x2,x3;
-    ObjMatValues * currentMat;
+    ObjMatValues* currentMat = 0;
 
     while(! ifs.eof())
         {
@@ -379,7 +379,7 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
             if (buffer == "newmtl")
                 {
                     // whenever we find the "newmtl" keyword, we create a
-                    // new struct and store it in the vector; we fill in 
+                    // new struct and store it in the vector; we fill in
                     // the values as we read the following lines
                     str >> matName;
                     ObjMatValues temp;
@@ -387,7 +387,13 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
                     matVector.push_back(temp);
                     currentMat = &(matVector.back());
                 }
-            else if (buffer == "Ka")
+
+            if (currentMat == 0)
+                {
+                    continue;
+                }
+
+            if (buffer == "Ka")
                 {
                     str >> x1 >> x2 >> x3;
                     currentMat->ambient[0] = x1;
@@ -418,21 +424,21 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
                     str >> x1;
                     currentMat->alpha = x1;
                 }
-           
+
             else if (buffer == "map_Kd")
                 {
                     str >> texName;
-                    
+
                     if (! GetFile()->LocateResource("textures/" + texName, currentMat->diffuseTexture))
                         {
                             GetLog()->Error()
-                                << "(ObjImporter) ERROR: cannot locate texture file '" 
+                                << "(ObjImporter) ERROR: cannot locate texture file '"
                                 <<  "textures/" << texName << "'\n";
                         }
-                }  
+                }
         }
 
-    GetLog()->Debug() << "(ObjImporter) read " << matVector.size() 
+    GetLog()->Debug() << "(ObjImporter) read " << matVector.size()
                       << " material definitions.\n";
 
     // now we read all the materials values, so create the materials and
@@ -460,37 +466,37 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
         {
             // decide whether we need to setup a Material2DTexture or whether
             // a MaterialSolid is sufficient (we only support diffuse textures for now)
-            if (iter->diffuseTexture.size()) 
+            if (iter->diffuseTexture.size())
                 {
                     material = shared_dynamic_cast<Material2DTexture>
-                        (GetCore()->New("/kerosin/Material2DTexture"));  
-                                                
+                        (GetCore()->New("/kerosin/Material2DTexture"));
+
                     if (material.get() == 0)
                         {
                             GetLog()->Error()
                                 << "(ObjImporter) ERROR: could not create new Material2DTexture\n";
                             return false;
                         }
-                                                          
-                    GetLog()->Debug() << "(ObjImporter) defining Material2DTexture " 
+
+                    GetLog()->Debug() << "(ObjImporter) defining Material2DTexture "
                                       << iter->name << "\n";
-                                                
-                    //we only support diffuse textures for now      
+
+                    //we only support diffuse textures for now
                     (shared_dynamic_cast<Material2DTexture>(material))->SetDiffuseTexture(iter->diffuseTexture);
                 }
             else
                 {
                     material = shared_dynamic_cast<MaterialSolid>
                         (GetCore()->New("/kerosin/MaterialSolid"));
-                                                
+
                     if (material.get() == 0)
                         {
                             GetLog()->Error()
                                 << "(ObjImporter) ERROR: could not create new MaterialSolid\n";
                             return false;
                         }
-                                        
-                    GetLog()->Debug() << "(ObjImporter) defining MaterialSolid " 
+
+                    GetLog()->Debug() << "(ObjImporter) defining MaterialSolid "
                                       << iter->name << "\n";
                 }
 
@@ -499,40 +505,40 @@ bool ObjImporter::SetupMaterials(string & matLibName, TObjMatValueVector & matVe
             rgba = RGBA(
                         iter->ambient[0],
                         iter->ambient[1],
-                        iter->ambient[2],       
+                        iter->ambient[2],
                         iter->alpha
                         );
 
             material->SetAmbient(rgba);
-                        
+
             rgba = RGBA(
                         iter->diffuse[0],
                         iter->diffuse[1],
-                        iter->diffuse[2],       
+                        iter->diffuse[2],
                         iter->alpha
                         );
-                        
+
             material->SetDiffuse(rgba);
-                        
+
             rgba = RGBA(
                         iter->specular[0],
                         iter->specular[1],
-                        iter->specular[2],      
+                        iter->specular[2],
                         iter->alpha
                         );
-                        
+
             material->SetSpecular(rgba);
-                        
+
             material->SetShininess(iter->shininess);
 
             materialServer->RegisterMaterial(material);
-                        
-            GetLog()->Debug() << "(ObjImporter) Material " 
-                              << iter->name << " set up and successfully registered.\n";              
+
+            GetLog()->Debug() << "(ObjImporter) Material "
+                              << iter->name << " set up and successfully registered.\n";
         }
 
-    GetLog()->Debug() << "(ObjImporter) Material setup finished for material library " 
-                      << matLibName << ".\n"; 
+    GetLog()->Debug() << "(ObjImporter) Material setup finished for material library "
+                      << matLibName << ".\n";
 
     return true;
 }
