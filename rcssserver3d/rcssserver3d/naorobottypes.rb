7c5ebc7fa33558b189ea03b0bf71291b35c23ca8
Klaus Dorer <klaus.dorer@hs-offenburg.de>
2013-04-12 09:04:57 +0000

    adjusted rsg files to be able to pass heterogeneous type information introduced the possibility to specify different joint max speeds added two heterogeneous types: longer leg and faster foot pitch

--- rcssserver3d/rcssserver3d/naorobottypes.rb
+++ rcssserver3d/rcssserver3d/naorobottypes.rb
@@ -1,6 +1,8 @@
 NaoType = [
 # Type 0 (Standard Nao)
-{'tl' => 0.1},
+{'ThighRelHip2_Z' => -0.04, 'lj5_max_abs_speed' => 6.1395, 'lj6_max_abs_speed' => 6.1395},
 # Type 1
-{'tl' => 0.12}
+{'ThighRelHip2_Z' => -0.08, 'lj5_max_abs_speed' => 6.1395, 'lj6_max_abs_speed' => 6.1395},
+# Type 2
+{'ThighRelHip2_Z' => -0.04, 'lj5_max_abs_speed' => 9.21,   'lj6_max_abs_speed' => 4.605}
 ]
