c737e7f57df396df3a973d22cb47c326ce9ec3d5
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2013-06-12 14:58:36 +0000

    Add Id svn keyword to all .cpp/.c/.h files

--- spark/lib/kerosin/sceneserver/capsule_c.cpp
+++ spark/lib/kerosin/sceneserver/capsule_c.cpp
@@ -4,7 +4,7 @@
    Fri May 9 2003
    Copyright (C) 2002,2003 Koblenz University
    Copyright (C) 2003 RoboCup Soccer Server 3D Maintenance Group
-   $Id: capsule_c.cpp 3 2008-11-21 02:38:08Z hedayat $
+   $Id$
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
